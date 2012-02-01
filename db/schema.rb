a3470df66620a714231127dd2e0eff9063266774
obrok <pawel.obrok@gmail.com>
2012-02-01 11:06:00 +0100

    Added a migration to initialize measurement counts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120127120519) do
+ActiveRecord::Schema.define(:version => 20120201084112) do
 
   create_table "measurements", :force => true do |t|
     t.float    "value"
