55b1c3d85ab2401a2eac566dace018c7ff8b2a07
obrok <pawel.obrok@gmail.com>
2012-02-20 14:23:34 +0100

    Note times will also be fixed

--- db/migrate/20120220090451_fix_missing_timezones.rb
+++ db/migrate/20120220090451_fix_missing_timezones.rb
@@ -8,16 +8,23 @@ class FixMissingTimezones < ActiveRecord::Migration
         unless session.measurements.empty?
           # Guess the timezone from the location.
           timezone_offset = (session.measurements.first.longitude / 15).to_i * MINUTES_IN_HOUR
+          seconds = timezone_offset * SECONDS_IN_MINUTE
 
           session.measurements.each do |measurement|
-            time = measurement.time - timezone_offset * SECONDS_IN_MINUTE if measurement.time
+            time = measurement.time - seconds if measurement.time
 
             measurement.update_column(:time, time)
             measurement.update_column(:timezone_offset, timezone_offset)
           end
 
-          start_time = session.start_time - timezone_offset * SECONDS_IN_MINUTE if session.start_time
-          end_time = session.end_time - timezone_offset * SECONDS_IN_MINUTE if session.end_time
+          session.notes.each do |note|
+            date = note.date - seconds if note.date
+
+            note.update_column(:date, date)
+          end
+
+          start_time = session.start_time - seconds if session.start_time
+          end_time = session.end_time - seconds if session.end_time
 
           session.update_column(:start_time, start_time)
           session.update_column(:end_time, end_time)
