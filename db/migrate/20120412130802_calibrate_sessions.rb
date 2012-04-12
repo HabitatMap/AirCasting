9ebf3117d756658828aa8d9b2b24b13a3ac45e7e
Andrzej Grzesik <andrzej.grzesik@gmail.com>
2012-04-12 15:45:08 +0200

    calibrated measurements

---
+++ db/migrate/20120412130802_calibrate_sessions.rb
@@ -0,0 +1,15 @@
+class CalibrateSessions < ActiveRecord::Migration
+  CALIBRATE = "(value + (calibration - 60 + offset_60_db)) / (calibration - 60 + offset_60_db) * (calibration - 60) + 60"
+
+  def up
+
+  	sql = <<-SQL
+ 			update measurements m
+ 			LEFT JOIN sessions s on s.id = m.session_id
+ 			set value = #{CALIBRATE}
+ 		SQL
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120220090451) do
+ActiveRecord::Schema.define(:version => 20120412130802) do
 
   create_table "measurements", :force => true do |t|
     t.float    "value"
