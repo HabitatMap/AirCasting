7b5101f1b02cee7228713ef3c8932fca6f287ae8
Andrzej Grzesik <andrzej.grzesik@gmail.com>
2012-04-18 00:44:55 +0200

    scripts for CI

---
+++ script/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -i
+
+rvm 1.9.2
+rvm gemset list | grep aircasting || rvm gemset create aircasting
+rvm 1.9.2@aircasting
+gem list | grep bundler || gem install bundler
+
+bundle install
+bundle update
+
+export DISPLAY=:92
+
+script/xvfb start
+
+export RAILS_ENV=test 
+
+rake db:migrate
+rspec
+rake jasmine:ci
+
+script/xvfb stop
+
+echo "Done."
