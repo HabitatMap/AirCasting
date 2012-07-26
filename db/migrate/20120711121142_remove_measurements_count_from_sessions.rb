69c06b2d598745cade764c9aebb6cc384b581863
Michal Wrobel <sparrovv@gmail.com>
2012-07-26 16:51:41 +0200

    Revert "Remove unnecessary measurements_count column from sessions"
    It's used, so I need it to stay
    
    This reverts commit a7b5cbdbac0efeef276f7f311b41b81961103edc.

--- db/migrate/20120711121142_remove_measurements_count_from_sessions.rb
+++
@@ -1,9 +0,0 @@
-class RemoveMeasurementsCountFromSessions < ActiveRecord::Migration
-  def up
-    remove_column :sessions, :measurements_count
-  end
-
-  def down
-    add_column :sessions, :measurements_count, :Integer
-  end
-end
