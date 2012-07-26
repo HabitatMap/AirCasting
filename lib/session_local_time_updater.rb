05d9ed2305a176ad3ae54044ac6119ea4b01a45d
Michal Wrobel <sparrovv@gmail.com>
2012-07-26 13:55:12 +0200

    Takes local times from measurements

--- lib/session_local_time_updater.rb
+++ lib/session_local_time_updater.rb
@@ -1,23 +1,7 @@
 class SessionLocalTimeUpdater
   def self.update(session)
-    timezone_offset = session.timezone_offset
-    unless timezone_offset
-      timezone_offset = session.measurements.first.timezone_offset
-    end
-
-    unless timezone_offset
-      timezone_offset = 0
-    end
-
-    session.local_start_time = session.start_time
-    session.local_end_time = session.end_time
-
-    begin
-      session.local_start_time += timezone_offset.minutes
-      session.local_end_time += timezone_offset.minutes
-    rescue => e
-      p e.inspect
-    end
+    session.local_start_time = session.measurements.minimum('time')
+    session.local_end_time = session.measurements.maximum('time')
 
     session.save
   end
--- lib/tasks/fix.rake
+++ lib/tasks/fix.rake
@@ -1,10 +1,6 @@
 require 'session_local_time_updater'
 
 namespace :fix do
-  task :ensure_start_time_end_time_in_sessions => [:environment] do
-
-  end
-
   desc "Add local time to sessions"
   task :add_local_time_to_session => [:environment] do
     Session.find_in_batches do |group|
