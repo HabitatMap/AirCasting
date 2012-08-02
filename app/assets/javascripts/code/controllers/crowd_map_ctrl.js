91ee42eeb615f259175bc85971a9f30cc37be0ff
Bartosz Knapik <bartosz.knapik@llp.pl>
2012-08-03 00:12:05 +0200

    angular part3

--- app/assets/javascripts/code/controllers/crowd_map_ctrl.js
+++ app/assets/javascripts/code/controllers/crowd_map_ctrl.js
@@ -1,51 +1,63 @@
-function CrowdMapCtrl($scope, $routeParams, timeFormats) {
-  $scope.expandables = {};
-  $scope.master = {};
-  $scope.local = {};
-
+function CrowdMapCtrl($scope, $routeParams, $http) {
+  $scope.sensors = [];
   $scope.minResolution = 10;
   $scope.maxResolution = 50;
-  $scope.gridResolution = 25;
-
   $scope.minTime = 0;
   $scope.maxTime = 24 * 60 - 1;
-  $scope.minDay = 1
-  $scope.maxDay = 365
+  $scope.minDay = 1;
+  $scope.maxDay = 365;
 
-  $scope.timeFrom = $scope.minTime
-  $scope.timeTo = $scope.maxTime
-  $scope.dayFrom = $scope.minDay
-  $scope.dayTo = $scope.maxDay
+  $scope.master = {
+    time: {
+      timeFrom : $scope.minTime,
+      timeTo : $scope.maxTime,
+      dayFrom : $scope.minDay,
+      dayTo : $scope.maxDay
+    },
+    gridResolution : 25
+  };
 
+  $scope.data = angular.copy($scope.master);
+
+  $scope.expandables = {};
   $scope.toggle = function(name) {
     $scope.expandables[name] = $scope.expandables[name] ? undefined : "expanded";
   }
-
+  //show some expandables
   _.each(['sensor', 'location', 'usernames'], function(name) {
     $scope.toggle(name);
   })
 
-  $scope.update = function(local) {
-    $scope.master = angular.copy(local);
+  $scope.update = function(formData) {
+    $scope.master = angular.copy(formData);
   };
 
   $scope.reset = function(name) {
-    $scope.local[name] = angular.copy($scope.master[name]);
+    $scope.data[name] = angular.copy($scope.master[name]);
   };
 
   $scope.onResolutionSlide = function(event, ui) {
-    $scope.gridResolution = ui.value;
+    $scope.data.gridResolution = ui.value;
     $scope.$digest();
   }
   $scope.onMonthDaySlide = function(event, ui) {
-    $scope.dayFrom = ui.values[0];
-    $scope.dayTo = ui.values[1];
+    $scope.data.time.dayFrom = _.min(ui.values);
+    $scope.data.time.dayTo = _.max(ui.values);
     $scope.$digest();
   }
   $scope.onTimeSlide = function(event, ui) {
-    $scope.timeFrom = ui.values[0];
-    $scope.timeTo = ui.values[1];
+    $scope.data.time.timeFrom = _.min(ui.values);
+    $scope.data.time.timeTo = _.max(ui.values);
     $scope.$digest();
   }
+  $scope.onSensorsFetch = function(data, status, headers, config) {
+    _(data).each(function(sensor){
+      sensor.id = sensor.measurement_type + "-" + sensor.sensor_name;
+      sensor.label = sensor.measurement_type + " - " + sensor.sensor_name;
+    });
+    $scope.sensors = data;
+  }
+
+  $http.get('/api/sensors').success($scope.onSensorsFetch);
 }
-CrowdMapCtrl.$inject = ['$scope', '$routeParams'];
+CrowdMapCtrl.$inject = ['$scope', '$routeParams', '$http'];
