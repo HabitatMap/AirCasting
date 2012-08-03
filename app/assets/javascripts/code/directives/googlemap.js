64909dbab0a0fbb8c04997458db44231add4a322
Bartosz Knapik <bartosz.knapik@llp.pl>
2012-08-04 00:27:53 +0200

    added cookies and google map

---
+++ app/assets/javascripts/code/directives/googlemap.js
@@ -0,0 +1,34 @@
+angular.module("aircasting").directive('googlemap', function($cookies){
+  return {
+    link: function(scope, element, attrs, controller) {
+      var lat = $cookies.vp_lat || 38.693861956002024;
+      var lng = $cookies.vp_lng || -87.5;
+      var latlng = new google.maps.LatLng(lat, lng);
+      var zoom = $cookies.vp_zoom || 5;
+      var minZoom = 3;
+      var options = {
+        zoom: parseInt(zoom),
+        minZoom: minZoom,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+          position: google.maps.ControlPosition.TOP_CENTER,
+          mapTypeIds: [
+            google.maps.MapTypeId.ROADMAP,
+            google.maps.MapTypeId.SATELLITE,
+            google.maps.MapTypeId.TERRAIN,
+            google.maps.MapTypeId.HYBRID
+          ]
+        },
+        zoomControl: false,
+        panControl: false,
+        streetViewControl: true,
+        streetViewControlOptions: {
+          position: google.maps.ControlPosition.TOP_CENTER
+        }
+      };
+      var map = new google.maps.Map(element[0], options);
+    }
+  };
+});
