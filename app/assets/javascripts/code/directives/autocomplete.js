c270b2e74f93dd35a484344a6d789ffa008cc6d6
Bartosz Knapik <bartosz.knapik@llp.pl>
2012-08-13 01:13:14 +0200

    fixed autocomplete

--- app/assets/javascripts/code/directives/autocomplete.js
+++ app/assets/javascripts/code/directives/autocomplete.js
@@ -1,7 +1,51 @@
 angular.module("aircasting").directive('autocomplete', function (){
   return {
     link: function(scope, element, attrs, controller) {
-      $(element).autocomplete(attrs.autocomplete, {multiple: true, delay: 100});
+      function split( val ) {
+        return val.split( /,\s*/ );
+      }
+      function extractLast( term ) {
+        return split( term ).pop();
+      }
+      // don't navigate away from the field on tab when selecting an item
+      $(element).bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).data( "autocomplete" ).menu.active ) {
+          event.preventDefault();
+        }
+      }).autocomplete({
+        source: function( request, response ) {
+          $.getJSON( attrs.autocomplete, {
+            q: extractLast( request.term ), limit: 10
+          }, response );
+        },
+        search: function() {
+          // custom minLength
+          var term = extractLast( this.value );
+          if ( term.length < 2 ) {
+            return false;
+          }
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( ", " );
+          return false;
+        }
+      });
     }
   };
 });
+
+
+
+
--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -1,19 +1,19 @@
 # AirCasting - Share your Air!
 # Copyright (C) 2011-2012 HabitatMap, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 # You can contact the authors by email at <info@habitatmap.org>
 
 class AutocompleteController < ApplicationController
@@ -26,9 +26,9 @@ class AutocompleteController < ApplicationController
       sql_query = ActiveRecord::Base.send(:sanitize_sql_array, [FINDER_SQL, "#{q}%", params[:limit].to_i])
       tag_names = ActiveRecord::Base.connection.execute(sql_query).to_a.flatten
 
-      render :text => tag_names.join("\n")
+      render :json => tag_names
     else
-      render :text => ""
+      render :json => []
     end
   end
 
@@ -37,9 +37,9 @@ class AutocompleteController < ApplicationController
 
     if q.present?
       names = User.select("username").where("username LIKE ?", "#{q}%").order(:username).map(&:username)
-      render :text => names.join("\n")
+      render :json => names
     else
-      render :text => ""
+      render :json => []
     end
   end
 end
