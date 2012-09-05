038479723f015db62fd839b06ed8270339c3b16f
Marcin Kulik <marcin.kulik@gmail.com>
2012-09-05 14:08:29 +0200

    Fix calculating of min/max_latitude/longitude

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -58,7 +58,7 @@ class Stream < ActiveRecord::Base
       stream = create!(data)
       stream.build_measurements!(measurements)
 
-      if stream.measurements.size > 0
+      if stream.measurements.count > 0
         stream.calc_bounding_box! if stream.min_latitude.nil?
         stream.calc_average_value! if stream.average_value.nil?
       end
