2ca37ee596ad108009773e188bd9e79734412d64
Marcin Kulik <marcin.kulik@gmail.com>
2012-09-06 14:55:08 +0200

    Fix regions_controller_spec

--- spec/controllers/api/regions_controller_spec.rb
+++ spec/controllers/api/regions_controller_spec.rb
@@ -4,10 +4,12 @@ describe Api::RegionsController do
   describe "GET 'show'" do
     it "should delegate to RegionInfo" do
       RegionInfo.should_receive(:new).
-        with(:north => 10, :south => 11, :east => 10.5, :west => 10.9).
+        with(:north => 10, :south => 11, :east => 10.5, :west => 10.9,
+             :sensor_name => 'foo').
         and_return("some" => "data")
 
-      get :show, :north => "10", :east => "10.5", :west => "10.9", :south => "11", :format => :json
+      get :show, :north => "10", :east => "10.5", :west => "10.9",
+                 :south => "11", :sensor_name => 'foo', :format => :json
 
       json_response.should == { "some" => "data" }
     end
