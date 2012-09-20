f10fc23e065bf1a690b83a9c708c7e4af6000b69
Michal Wrobel <sparrovv@gmail.com>
2012-09-20 14:49:53 +0200

    Fix #415092

--- app/assets/javascripts/code/controllers/note_ctrl.js
+++ app/assets/javascripts/code/controllers/note_ctrl.js
@@ -1,15 +1,7 @@
 function NoteCtrl($scope, note, sessions) {
   $scope.note = note.data;
   var notes = sessions.find(note.data.session_id).notes;
-  $scope.total = _(notes).size();
-  $scope.idx = note.idx;
-  $scope.next = function(){
-    $scope.idx = _([$scope.idx + 1, $scope.total - 1]).min();
-    $scope.note = notes[$scope.idx];
-  };
-  $scope.prev = function(){
-    $scope.idx = _([$scope.idx - 1, 0]).max();
-    $scope.note = notes[$scope.idx];
-  };
+  var total = _(notes).size();
+  $scope.number = note.idx + '/' + total;
 }
 NoteCtrl.$inject = ['$scope', 'note', 'sessions'];
