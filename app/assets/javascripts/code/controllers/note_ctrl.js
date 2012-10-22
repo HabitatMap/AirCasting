26f2898cd9d77344bfee700e5d5a6c5198fee8fb
Michal Wrobel <sparrovv@gmail.com>
2012-10-22 16:50:56 +0200

    Fix notes numbering

--- app/assets/javascripts/code/controllers/note_ctrl.js
+++ app/assets/javascripts/code/controllers/note_ctrl.js
@@ -2,6 +2,7 @@ function NoteCtrl($scope, note, sessions) {
   $scope.note = note.data;
   var notes = sessions.find(note.data.session_id).notes;
   var total = _(notes).size();
-  $scope.number = note.idx + '/' + total;
+  var noteNumber = parseInt(note.idx, 10) + 1;
+  $scope.number = noteNumber + '/' + total;
 }
 NoteCtrl.$inject = ['$scope', 'note', 'sessions'];
