36a71201d38e5cb4aba92a7c42c93b995520bfbf
kustosz <marcin.kostrzewa@llp.pl>
2014-07-17 10:57:18 +0200

    fix params parsing for Regressions Controller

--- app/controllers/api/regressions_controller.rb
+++ app/controllers/api/regressions_controller.rb
@@ -3,9 +3,9 @@ module Api
     before_filter :authenticate_user!, only: :create
 
     def create
-      session = Session.find_by_uuid!(params[:session_uuid])
-      ref = session.streams.where(params[:reference])
-      target = session.streams.where(params[:target])
+      session = current_user.sessions.find_by_uuid!(params[:session_uuid])
+      ref = session.streams.where(JSON.parse(params[:reference]))
+      target = session.streams.where(JSON.parse(params[:target]))
       respond_with Regression.create_for_streams(target, ref)
     end
 
