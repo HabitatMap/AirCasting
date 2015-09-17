e2a1eefed248080f4267003429ca75c84cc5e58b
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-09-17 12:59:56 +0200

    log elasticsearch queries

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'elasticsearch/rails/instrumentation'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
