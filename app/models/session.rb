f14fa5ddf8908d08c71ed18d6ef6944efa7e5ba8
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-09-26 22:12:31 +0200

    allow to pass array for session_ids

--- app/models/session.rb
+++ app/models/session.rb
@@ -69,7 +69,7 @@ class Session < ActiveRecord::Base
 
   def self.filter(data={})
     sessions = order("sessions.created_at DESC").
-    where("contribute = true OR sessions.id = ?", data[:session_ids]).
+    where("contribute = true OR sessions.id IN (?)", data[:session_ids]).
     day_range(data[:day_from], data[:day_to]).
     joins(:user)
 
