dcdcaf16665842338fb40fe17886711477b6f9d4
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-09-29 12:33:44 +0200

    display more info in production log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ AirCasting::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :info
+  config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
