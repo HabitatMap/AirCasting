3206c226d220fd7d5f784feb8ea2645ece7eafc6
Marek Ciupak <marek.ciupak@lunarlogic.io>
2016-03-25 10:30:10 +0100

    Fix time in sessions notes
    To use creator's local time.

--- app/assets/javascripts/code/controllers/note_ctrl.js
+++ app/assets/javascripts/code/controllers/note_ctrl.js
@@ -1,5 +1,6 @@
 function NoteCtrl($scope, note, sessions) {
   $scope.note = note.data;
+  $scope.note.dateLocalTime = moment($scope.note.date, "YYYY-MM-DDTHH:mm:ss").format('MM/DD/YYYY, HH:mm');
   var notes = sessions.find(note.data.session_id).notes;
   var total = _(notes).size();
   var noteNumber = parseInt(note.idx, 10) + 1;
