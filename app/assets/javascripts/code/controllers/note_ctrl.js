e1ebed2d81a30ac186e7a974e1d1a7991ac52860
Marek Ciupak <marek.ciupak@lunarlogic.io>
2016-03-28 18:09:31 +0200

    Display seconds in notes datetime

--- app/assets/javascripts/code/controllers/note_ctrl.js
+++ app/assets/javascripts/code/controllers/note_ctrl.js
@@ -1,6 +1,6 @@
 function NoteCtrl($scope, note, sessions) {
   $scope.note = note.data;
-  $scope.note.dateLocalTime = moment($scope.note.date, "YYYY-MM-DDTHH:mm:ss").format('MM/DD/YYYY, HH:mm');
+  $scope.note.dateLocalTime = moment($scope.note.date, "YYYY-MM-DDTHH:mm:ss").format('MM/DD/YYYY, HH:mm:ss');
   var notes = sessions.find(note.data.session_id).notes;
   var total = _(notes).size();
   var noteNumber = parseInt(note.idx, 10) + 1;
