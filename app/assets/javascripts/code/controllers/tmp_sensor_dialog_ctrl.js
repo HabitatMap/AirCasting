8991604e81f165241da805ebc2ec4b9d8a4c8e7e
Marek Ciupak <marek.ciupak@lunarlogic.io>
2016-05-17 17:34:45 +0200

    Fix displaying sensors in sensor dialog

--- app/assets/javascripts/code/controllers/tmp_sensor_dialog_ctrl.js
+++ app/assets/javascripts/code/controllers/tmp_sensor_dialog_ctrl.js
@@ -1,5 +1,9 @@
 function TmpSensorDialogCtrl($scope, sensors, params) {
   $scope.sensors = sensors;
   $scope.params = params;
+
+  $scope.availSensors = function() {
+    return singleSession.availSensors();
+  };
 }
 TmpSensorDialogCtrl.$inject = ['$scope', 'sensors', 'params'];
