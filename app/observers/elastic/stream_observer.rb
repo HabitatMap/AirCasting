6a569e7679b35ab92702bf5df0304b8206ae31b2
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2016-09-26 15:18:15 +0200

    Don't index fixed sessions measurements
    We don't want fixed sessions data on the crowdmap as of now,
    so because of that we do not want to index them. Also, this will help
    with sidekiq performance

--- app/observers/elastic/stream_observer.rb
+++ app/observers/elastic/stream_observer.rb
@@ -3,15 +3,23 @@ module Elastic
     observe :stream
 
     def after_create(record)
-      Elastic::StreamIndexer.perform_async(record.id, 'index_document')
+      indexer_callback(record, 'index_document')
     end
 
     def after_update(record)
-      Elastic::StreamIndexer.perform_async(record.id, 'update_document')
+      indexer_callback(record, 'update_document')
     end
 
     def after_destroy(record)
-      Elastic::StreamIndexer.perform_async(record.id, 'delete_document')
+      indexer_callback(record, 'delete_document')
+    end
+
+    private
+
+    def indexer_callback(stream, action)
+      return if stream.session.type == 'FixedSession'
+
+      Elastic::StreamIndexer.perform_async(stream.id, action)
     end
   end
 end
