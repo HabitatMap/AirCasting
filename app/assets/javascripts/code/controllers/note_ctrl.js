76b9851c5e2c50fab317d37fc2787f2333972923
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2016-10-20 13:02:13 +0200

    Fix mobile sessions notes display

--- app/assets/javascripts/code/controllers/note_ctrl.js
+++ app/assets/javascripts/code/controllers/note_ctrl.js
@@ -1,11 +1,11 @@
-function NoteCtrl($scope, note) {
-  sessions = $scope.sessions;
+function NoteCtrl($scope, note, singleMobileSession) {
+  var session = singleMobileSession.get();
 
   $scope.note = note.data;
   $scope.note.dateLocalTime = moment($scope.note.date, "YYYY-MM-DDTHH:mm:ss").format('MM/DD/YYYY, HH:mm:ss');
-  var notes = sessions.find(note.data.session_id).notes;
+  var notes = session.notes;
   var total = _(notes).size();
   var noteNumber = parseInt(note.idx, 10) + 1;
   $scope.number = noteNumber + '/' + total;
 }
-NoteCtrl.$inject = ['$scope', 'note'];
+NoteCtrl.$inject = ['$scope', 'note', 'singleMobileSession'];
