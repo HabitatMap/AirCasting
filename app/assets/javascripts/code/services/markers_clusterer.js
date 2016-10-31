4f5715d36022e8a5570850b51b9ac44866ba215d
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2016-10-31 10:50:15 +0100

    Raduce marker clusterer grid size

--- app/assets/javascripts/code/services/markers_clusterer.js
+++ app/assets/javascripts/code/services/markers_clusterer.js
@@ -1,12 +1,12 @@
 angular.module("aircasting").factory('markersClusterer', ['map', function(map) {
 
   var MarkersClusterer = function() {
-    this.markerCluster = new MarkerClusterer(map.get(), [], {imagePath: '/assets/'});
-  }
+    this.markerCluster = new MarkerClusterer(map.get(), [], {gridSize: 40, imagePath: '/assets/'});
+  };
 
   MarkersClusterer.prototype = {
     draw: function(map, markers) {
-      this.markerCluster = new MarkerClusterer(map, markers, {imagePath: '/assets/'});
+      this.markerCluster = new MarkerClusterer(map, markers, {gridSize: 40, imagePath: '/assets/'});
     },
 
     clear: function() {
