a084c5d52ef1835f7ce1c24042fa572a36c332a0
Tomasz Giereś <tomasz.gieres@lunarlogic.io>
2018-06-29 16:20:02 +0200

    fix cannot read property 'max_latitude'
    When in fixed map, loading some sessions was causing js to throw
    "cannot read property 'max_latitude' of undefined"

--- app/assets/javascripts/code/services/bounds_calculator.js
+++ app/assets/javascripts/code/services/bounds_calculator.js
@@ -1,38 +1,27 @@
 angular.module('aircasting').factory('boundsCalculator',
        ['sensors', function(sensors) {
   var calculate = function(sessions) {
-    var north,  east, south, west;
     var maxLat = [], minLat = [], maxLong = [], minLong = [];
     var sensor = sensors.anySelected();
-    if(!sensor) {
-     return;
-    }
-    var streams = allStreams(sessions, sensor.sensor_name);
+    if(!sensor) return;
 
-    if(!_.isEmpty(streams)){
-      _(streams).each(function(s){
-        maxLat.push(s.max_latitude);
-        minLat.push(s.min_latitude);
-        maxLong.push(s.max_longitude);
-        minLong.push(s.min_longitude);
-      });
+    sessions.forEach(function(session){
+      var stream = session.streams[sensor.sensor_name];
+      if(!stream) return;
 
-      north = Math.max.apply(null, maxLat);
-      south = Math.min.apply(null, minLat);
-      west = Math.min.apply(null, minLong);
-      east = Math.max.apply(null, maxLong);
-    }
-
-    if(!north){
-      return;
-    }
-    return {north: north, east: east, south : south, west: west};
-  };
-
-  var allStreams = function(sessions, sensor_name){
-    return _(sessions).map(function(session){
-      return session.streams[sensor_name];
+      maxLat.push(stream.max_latitude);
+      minLat.push(stream.min_latitude);
+      maxLong.push(stream.max_longitude);
+      minLong.push(stream.min_longitude);
     });
+
+    var north = Math.max.apply(null, maxLat);
+    var south = Math.min.apply(null, minLat);
+    var west = Math.min.apply(null, minLong);
+    var east = Math.max.apply(null, maxLong);
+
+    if(!north){ return; }
+    return {north: north, east: east, south : south, west: west};
   };
 
   return calculate;
