10ea1e63f5ad4158de6d33c4f37128a8ca77c3ec
3v0k4 <riccardo.odone@gmail.com>
2018-07-06 12:27:37 +0200

    deploy script

---
+++ bin/deploy_staging.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+git fetch --all
+git checkout -b deploy
+git cherry-pick cap3
+git push --set-upstream origin deploy
+bundle exec cap staging deploy BRANCH=deploy
+git push origin :deploy
+git checkout -
+git branch -D deploy
