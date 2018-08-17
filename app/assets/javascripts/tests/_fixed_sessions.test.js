f05a65b9199faed8fe872ba9cf6d6b7485553c0f
3v0k4 <riccardo.odone@gmail.com>
2018-08-17 16:21:19 +0200

    tests(fixed): better naming

--- app/assets/javascripts/tests/_fixed_sessions.test.js
+++ app/assets/javascripts/tests/_fixed_sessions.test.js
@@ -38,105 +38,105 @@ test('fetch with empty address does not call goToAddress', t => {
 });
 
 test('fetch with no sessions ids in params passes empty array to sessionsDownloader', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData();
   const sessionIds = [];
-  const fixedSessionsService = _fixedSessions({ args, data, sessionIds });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls , data, sessionIds });
 
   fixedSessionsService._fetch();
 
-  t.deepEqual(args[0].session_ids, sessionIds);
+  t.deepEqual(sessionsDownloaderCalls[0].session_ids, sessionIds);
 
   t.end();
 });
 
 test('fetch with sessions ids in params passes them to sessionsDownloader', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData();
   const sessionIds = [1, 2, 3];
-  const fixedSessionsService = _fixedSessions({ args, data, sessionIds });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data, sessionIds });
 
   fixedSessionsService._fetch();
 
-  t.deepEqual(args[0].session_ids, sessionIds);
+  t.deepEqual(sessionsDownloaderCalls[0].session_ids, sessionIds);
 
   t.end();
 });
 
 test('fetch with time params passes them to sessionsDownloader after subtracting an offset from utils', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ time: { timeFrom: 1, timeTo: 2 } });
   const utils = { timeOffset: 1 };
-  const fixedSessionsService = _fixedSessions({ args, data, utils });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data, utils });
 
   fixedSessionsService._fetch();
 
-  t.deepEqual(args[0].time_from, 0);
-  t.deepEqual(args[0].time_to, 1);
+  t.deepEqual(sessionsDownloaderCalls[0].time_from, 0);
+  t.deepEqual(sessionsDownloaderCalls[0].time_to, 1);
 
   t.end();
 });
 
 test('fetch with day and year params passes them to sessionsDownloader', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ time: { dayFrom: 3, dayTo: 4, yearFrom: 5, yearTo: 6 } });
-  const fixedSessionsService = _fixedSessions({ args, data });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data });
 
   fixedSessionsService._fetch();
 
-  t.deepEqual(args[0].day_from, 3);
-  t.deepEqual(args[0].day_to, 4);
-  t.deepEqual(args[0].year_from, 5);
-  t.deepEqual(args[0].year_to, 6);
+  t.deepEqual(sessionsDownloaderCalls[0].day_from, 3);
+  t.deepEqual(sessionsDownloaderCalls[0].day_to, 4);
+  t.deepEqual(sessionsDownloaderCalls[0].year_from, 5);
+  t.deepEqual(sessionsDownloaderCalls[0].year_to, 6);
 
   t.end();
 });
 
 test('fetch with tags and usernames params passes them to sessionsDownloader', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ tags: "tag1, tag2", usernames: "will123, agata" });
-  const fixedSessionsService = _fixedSessions({ args, data });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data });
 
   fixedSessionsService._fetch();
 
-  t.deepEqual(args[0].tags, "tag1, tag2");
-  t.deepEqual(args[0].usernames, "will123, agata");
+  t.deepEqual(sessionsDownloaderCalls[0].tags, "tag1, tag2");
+  t.deepEqual(sessionsDownloaderCalls[0].usernames, "will123, agata");
 
   t.end();
 });
 
 test('fetch with outdoorOnly set to true passes is_indoor false to sessionsDownloader', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ location: { outdoorOnly: true, address: '' } });
-  const fixedSessionsService = _fixedSessions({ args, data });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data });
 
   fixedSessionsService._fetch();
 
-  t.deepEqual(args[0].is_indoor, false);
+  t.deepEqual(sessionsDownloaderCalls[0].is_indoor, false);
 
   t.end();
 });
 
 test('fetch with outdoorOnly set to false does not pass is_indoor to sessionsDownloader', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ location: { outdoorOnly: false, address: '' } });
-  const fixedSessionsService = _fixedSessions({ args, data });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data });
 
   fixedSessionsService._fetch();
 
-  t.deepEqual(args[0].is_indoor, undefined);
+  t.deepEqual(sessionsDownloaderCalls[0].is_indoor, undefined);
 
   t.end();
 });
 
 test('fetch with no time in params does not call downloadSessions', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ time: undefined });
-  const fixedSessionsService = _fixedSessions({ args, data });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data });
 
   fixedSessionsService._fetch();
 
-  t.true(args.length === 0);
+  t.true(sessionsDownloaderCalls.length === 0);
 
   t.end();
 });
@@ -154,33 +154,33 @@ test('fetch with time calls drawSession.clear', t => {
 });
 
 test('fetch with time calls downloadSessions', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ time: {} });
-  const fixedSessionsService = _fixedSessions({ args, data });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data });
 
   fixedSessionsService._fetch();
 
-  t.true(args.length > 0);
+  t.true(sessionsDownloaderCalls.length > 0);
 
   t.end();
 });
 
 test('fetch when on a different route than fixed map does not call downloadSessions', t => {
-  const args = [];
+  const sessionsDownloaderCalls = [];
   const data = buildData({ time: {} });
   const $window = { location: { href: '/other_route' } };
-  const fixedSessionsService = _fixedSessions({ args, data, $window });
+  const fixedSessionsService = _fixedSessions({ sessionsDownloaderCalls, data, $window });
 
   fixedSessionsService._fetch();
 
-  t.true(args.length === 0);
+  t.true(sessionsDownloaderCalls.length === 0);
 
   t.end();
 });
 
 const buildData = obj => ({ time: {}, location: {}, ...obj });
 
-const _fixedSessions = ({ args = [], map, data, drawSession, utils, sessionIds = [], $window = { location: { href: '/map_fixed_sessions' } } }) => {
+const _fixedSessions = ({ sessionsDownloaderCalls = [], map, data, drawSession, utils, sessionIds = [], $window = { location: { href: '/map_fixed_sessions' } } }) => {
   const $rootScope = { $new: () => ({}) };
   const params = {
     get: what => {
@@ -197,7 +197,7 @@ const _fixedSessions = ({ args = [], map, data, drawSession, utils, sessionIds =
   const _utils = utils || {};
   const sensors = { selected: () => {} };
   const _drawSession = drawSession || { clear: () => {} };
-  const sessionsDownloader = (_, arg) => { args.push(arg) };
+  const sessionsDownloader = (_, arg) => { sessionsDownloaderCalls.push(arg) };
 
   return fixedSessions(params, null, _map, sensors, $rootScope, _utils, sessionsDownloader, _drawSession, null, null, null, null, $window);
 };
