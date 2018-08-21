dbcca00d88fb6107de8e2ff5d7ab66908134760d
3v0k4 <riccardo.odone@gmail.com>
2018-08-21 17:50:50 +0200

    fix(prev commit)

--- config/initializers/geocoder.rb
+++
@@ -1,7 +0,0 @@
-Geocoder.configure(
-  api_key:              Rails.application.config.secrets.fetch('GOOGLE_MAPS_API_KEY'),
-  use_https:            true,
-  logger:               :kernel,
-  kernel_logger_level:  ::Logger::INFO,
-  units: :mi
-)
