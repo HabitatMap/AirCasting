ac2159fe21fe363f5fe43512aea7f397377dec0e
3v0k4 <riccardo.odone@gmail.com>
2018-09-04 12:43:04 +0200

    WIP(mobile): hide all filters but heat when session selected

--- app/assets/javascripts/code/services/expandables.js
+++ app/assets/javascripts/code/services/expandables.js
@@ -1,5 +1,6 @@
 angular.module("aircasting").factory('expandables', function() {
   var Expandables = function() {
+    this.prev = {}
     this.allHidden = false;
     this.sections = {};
   };
@@ -15,7 +16,18 @@ angular.module("aircasting").factory('expandables', function() {
     },
     visible: function(name){
       return !!this.sections[name];
-    }
+    },
+    justHeat: function() {
+      this.prev = this.sections
+      this.sections = { heat: "expanded" };
+   },
+   isJustHeat: function() {
+     return Object.keys(this.sections).length === 1 &&
+       Object.keys(this.sections)[0] === "heat";
+   },
+   unjustHeat: function() {
+     this.sections = this.prev;
+   }
   };
   return new Expandables();
 });
