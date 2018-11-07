1d4a3d8c1f9a5c07318c47fe785d2811297e126f
3v0k4 <riccardo.odone@gmail.com>
2018-11-07 10:56:52 +0100

    refactor(services/single_mobile_session)

--- app/assets/javascripts/code/services/single_mobile_session.js
+++ app/assets/javascripts/code/services/single_mobile_session.js
@@ -1,74 +1,86 @@
-angular.module("aircasting").factory('singleMobileSession',
-       ['mobileSessions', 'map','sensors', 'storage', 'drawSession', 'heat', 'utils',
-        function(mobileSessions, map, sensors, storage, drawSession, heat, utils) {
-  var SingleMobileSession = function() {
-  };
+angular.module("aircasting").factory('singleMobileSession', [
+  'mobileSessions',
+  'sensors',
+  'storage',
+  'drawSession',
+  'heat',
+  'utils',
+  function(
+    mobileSessions,
+    sensors,
+    storage,
+    drawSession,
+    heat,
+    utils
+  ) {
+    var SingleMobileSession = function() {
+    };
 
-  SingleMobileSession.prototype = {
-    isSingle: function() {
-      return this.noOfSelectedSessions() == 1;
-    },
+    SingleMobileSession.prototype = {
+      isSingle: function() {
+        return this.noOfSelectedSessions() == 1;
+      },
 
-    noOfSelectedSessions : function() {
-      return mobileSessions.allSelected().length;
-    },
+      noOfSelectedSessions : function() {
+        return mobileSessions.allSelected().length;
+      },
 
-    get: function() {
-      return _(mobileSessions.allSelected()).first();
-    },
+      get: function() {
+        return _(mobileSessions.allSelected()).first();
+      },
 
-    id: function(onlySingle) {
-      if(onlySingle && !this.isSingle()){
-        return;
+      id: function(onlySingle) {
+        if(onlySingle && !this.isSingle()){
+          return;
+        }
+        var el = this.get();
+        return el && el.id;
+      },
+
+      availSensors: function() {
+        if(!this.get()){
+          return [];
+        }
+        var ids = _(this.get().streams).map(function(sensor){
+          return sensors.buildSensorId(sensor)
+        });
+
+        return _(sensors.sensors).select(function(sensor){
+          return _(ids).include(sensor.id);
+        });
+      },
+
+      withSelectedSensor: function(){
+        return !!this.get().streams[sensors.anySelected().sensor_name];
+      },
+
+      measurements: function(){
+        return  drawSession.measurements(this.get());
+      },
+
+      measurementsToTime: function(){
+        var currentOffset = moment.duration(utils.timeOffset, "minutes").asMilliseconds();
+        var x;
+        var result = {};
+        _(this.measurements()).each(function(measurement){
+          x = moment(measurement.time,"YYYY-MM-DDTHH:mm:ss").valueOf() - currentOffset;
+          result[x + ""] = {x: x,
+            y: measurement.value,
+            latitude: measurement.latitude,
+            longitude: measurement.longitude};
+        });
+        return result;
+      },
+
+      updateHeat: function() {
+        var data = heat.toSensoredList(this.get().streams[sensors.anySelected().sensor_name]);
+        storage.updateDefaults({heat: heat.parse(data)});
+        storage.reset("heat");
+      },
+
+      isFixed: function() {
+        return false;
       }
-      var el = this.get();
-      return el && el.id;
-    },
-
-    availSensors: function() {
-      if(!this.get()){
-        return [];
-      }
-      var ids = _(this.get().streams).map(function(sensor){
-        return sensors.buildSensorId(sensor)
-      });
-
-      return _(sensors.sensors).select(function(sensor){
-        return _(ids).include(sensor.id);
-      });
-    },
-
-    withSelectedSensor: function(){
-      return !!this.get().streams[sensors.anySelected().sensor_name];
-    },
-
-    measurements: function(){
-      return  drawSession.measurements(this.get());
-    },
-
-    measurementsToTime: function(){
-      var currentOffset = moment.duration(utils.timeOffset, "minutes").asMilliseconds();
-      var x;
-      var result = {};
-      _(this.measurements()).each(function(measurement){
-        x = moment(measurement.time,"YYYY-MM-DDTHH:mm:ss").valueOf() - currentOffset;
-        result[x + ""] = {x: x,
-                y: measurement.value,
-                latitude: measurement.latitude,
-                longitude: measurement.longitude};
-      });
-      return result;
-    },
-
-    updateHeat: function() {
-      var data = heat.toSensoredList(this.get().streams[sensors.anySelected().sensor_name]);
-      storage.updateDefaults({heat: heat.parse(data)});
-      storage.reset("heat");
-    },
-
-    isFixed: function() {
-      return false;
-    }
-  };
-  return new SingleMobileSession();
-}]);
+    };
+    return new SingleMobileSession();
+  }]);
