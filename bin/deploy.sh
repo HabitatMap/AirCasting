b1042e360466b60d7706c5abc36a36f80790fe2d
3v0k4 <riccardo.odone@gmail.com>
2018-11-13 11:44:54 +0100

    travis: do not build PRs

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+  echo "This is a pull request build so no deploy"
+  exit 0
+fi
+
 if [ "$TRAVIS_BRANCH" == "staging" ]; then
   bundle exec cap staging deploy
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
