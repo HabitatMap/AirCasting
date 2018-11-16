c6a33d8d442ddee541430f0ec9b7a968729739e1
3v0k4 <riccardo.odone@gmail.com>
2018-11-16 16:59:17 +0100

    feat(outliers): if above 5% do not filter

--- app/services/outliers/filter_measurements_farther_than.rb
+++ app/services/outliers/filter_measurements_farther_than.rb
@@ -1,16 +1,33 @@
 class Outliers::FilterMeasurementsFartherThan
   def initialize(
     max_distance,
+    threshold,
     calculate_distance = Outliers::CalculateDistance.new
   )
     @max_distance = max_distance
     @calculate_distance = calculate_distance
+    @threshold = threshold
   end
 
   def call(centroid, measurements)
+    filtered = filter(centroid, measurements)
+    filtered_or_all(filtered, measurements)
+  end
+
+  private
+
+  def filter(centroid, measurements)
     measurements.select do |measurement|
       lng_lat = [measurement.longitude, measurement.latitude]
       @calculate_distance.call(lng_lat, centroid) <= @max_distance
     end
+  end
+
+  def filtered_or_all(filtered, measurements)
+    percentage = 1 - filtered.size.to_f / measurements.size.to_f
+
+    percentage > @threshold ?
+      measurements :
+      filtered
   end
 end
--- spec/services/outliers/filter_measurements_farther_than_spec.rb
+++ spec/services/outliers/filter_measurements_farther_than_spec.rb
@@ -2,22 +2,43 @@ require "spec_helper"
 
 describe Outliers::FilterMeasurementsFartherThan do
   it "filters all measurement that are farther than max_distance from centroid" do
-    m1 = m2 = Measurement.new(
+    m1 = Measurement.new(
       longitude: one,
       latitude: one,
     )
-    m3 = Measurement.new(
+    m2 = Measurement.new(
       longitude: two,
       latitude: two,
     )
-    measurements = [m1, m2, m3]
+    measurements = [m1, m2]
     centroid = [one, one]
     max_distance = 0.001
-    subject = Outliers::FilterMeasurementsFartherThan.new(max_distance)
+    subject = Outliers::FilterMeasurementsFartherThan.new(max_distance, 100)
 
     actual = subject.call(centroid, measurements)
 
-    expected = [m1, m2]
+    expected = [m1]
+    expect(actual).to eq(expected)
+  end
+
+  it "when outliers found are above the threshold then measurements are not filtered" do
+    m1 = Measurement.new(
+      longitude: one,
+      latitude: one,
+    )
+    m2 = Measurement.new(
+      longitude: two,
+      latitude: two,
+    )
+    measurements = [m1, m2]
+    centroid = [one, one]
+    max_distance = 0.001
+    threshold = 0.49
+    subject = Outliers::FilterMeasurementsFartherThan.new(max_distance, threshold)
+
+    actual = subject.call(centroid, measurements)
+
+    expected = measurements
     expect(actual).to eq(expected)
   end
 
