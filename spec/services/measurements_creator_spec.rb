2673f5dc3e6bb98d45e869d74633db119b5b3434
AkkarinForest <alexander.suminski@lunarlogic.io>
2018-11-19 14:12:27 +0100

    improve test coverage of MeasurementCreator.call

--- spec/services/measurements_creator_spec.rb
+++ spec/services/measurements_creator_spec.rb
@@ -1,43 +1,59 @@
 require "spec_helper"
+require 'sidekiq/testing'
 
 describe MeasurementsCreator do
-  it "creates a measurement" do
-    session = create_session!
-    stream = create_stream!(session: session)
+  describe "#self.call" do
+    context "when there is only one measurement" do
+      it "creates a measurement" do
+        session = create_session!
+        stream = create_stream!(session: session)
 
-    MeasurementsCreator.call(stream, measurements_attributes)
+        MeasurementsCreator.call(stream, [measurement_attributes])
 
-    expect(Measurement.count).to eq(1)
-  end
+        expect(Measurement.count).to eq(1)
+      end
 
-  it "when sessions fixed creates a measurement with utc time" do
-    session = create_session!(type: "FixedSession")
-    stream = create_stream!(session: session)
+      it "when sessions fixed creates a measurement with utc time" do
+        session = create_session!(type: "FixedSession")
+        stream = create_stream!(session: session)
 
-    MeasurementsCreator.call(stream, measurements_attributes)
+        MeasurementsCreator.call(stream, [measurement_attributes])
 
-    expect(Measurement.first.utc_time).to be_within(1.second).of Time.now
-  end
+        expect(Measurement.first.arrival_utc_time).to be_within(1.second).of Time.current
+      end
 
-  it "when session is moblie creates a measurement without utc time" do
-    session = create_session!(type: "MoblieSession")
-    stream = create_stream!(session: session)
+      it "when session is moblie creates a measurement without utc time" do
+        session = create_session!(type: "MoblieSession")
+        stream = create_stream!(session: session)
 
-    MeasurementsCreator.call(stream, measurements_attributes)
+        MeasurementsCreator.call(stream, [measurement_attributes])
 
-    expect(Measurement.first.utc_time).to be_nil
+        expect(Measurement.first.arrival_utc_time).to be_nil
+      end
+    end
+
+    context "when there are more than one measurements" do
+      it "schedules the creation of measurements" do
+        session = create_session!
+        stream = create_stream!(session: session)
+
+        MeasurementsCreator.call(stream, [measurement_attributes, measurement_attributes])
+
+        assert_equal 1, AsyncMeasurementsCreator.jobs.size
+      end
+    end
   end
 
   private
 
-  def measurements_attributes
-    [{ longitude: 25.4356212,
+  def measurement_attributes
+    { longitude: 25.4356212,
       latitude: 56.4523456,
       time: "2016-05-11T17:09:02",
       milliseconds: 925,
       measured_value: 59.15683475380729,
       value: 59.15683475380729,
-    }]
+    }
   end
 
   def create_stream!(attributes)
