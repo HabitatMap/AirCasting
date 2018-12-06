HabitatMap/AirCasting

commit e6670c03c8dcdda1871662ce01550d6fa4f4e8f4

rename(mobile crowd) buildQueryParamsForAverages to buildQueryParamsForCrowdMapLayer

app/assets/javascripts/code/services/build_query_params_for_crowd_map_layer.js
@@ -1,0 +1,10 @@
+import { buildQueryParamsForCrowdMapLayer } from './_build_query_params_for_averages';
+
+angular.module("aircasting").factory('buildQueryParamsForCrowdMapLayer', [
+  'map',
+  'sensors',
+  'params',
+  'utils',
+  'flash',
+  buildQueryParamsForCrowdMapLayer
+]);

app/assets/javascripts/code/services/_build_query_params_for_crowd_map_layer.js
@@ -1,4 +1,4 @@
-export const buildQueryParamsForAverages = (
+export const buildQueryParamsForCrowdMapLayer = (
   sensors,
   params,
   utils,

app/assets/javascripts/code/services/_update_crowd_map_layer.js
@@ -5,7 +5,7 @@ export const updateCrowdMapLayer = (
   storage,
   map,
   $http,
-  buildQueryParamsForAverages,
+  buildQueryParamsForCrowdMapLayer,
   flash,
   $location,
   params,
@@ -18,10 +18,10 @@ export const updateCrowdMapLayer = (
     if (!storage.isCrowdMapLayerOn()) return;
 
     const bounds = map.getBounds();
-    const q = buildQueryParamsForAverages.call(sessionIds, bounds);
+    const q = buildQueryParamsForCrowdMapLayer.call(sessionIds, bounds);
     if (!q) return;
 
-    const _onRectangleClick = onRectangleClick(infoWindow, rectangles, sessionIds, buildQueryParamsForAverages);
+    const _onRectangleClick = onRectangleClick(infoWindow, rectangles, sessionIds, buildQueryParamsForCrowdMapLayer);
 
     $http.get('/api/averages2', { cache: true, params: { q }})
       .error(onError(flash))
@@ -37,6 +37,6 @@ const onAveragesFetch = ($location, map, params, utils, _onRectangleClick) => da
   map.drawRectangles(data, heats, _onRectangleClick);
 };
 
-const onRectangleClick = (infoWindow, rectangles, sessionIds, buildQueryParamsForAverages) => rectangleData => {
-  infoWindow.show("/api/region", buildQueryParamsForAverages.call(sessionIds, rectangleData), rectangles.position(rectangleData));
+const onRectangleClick = (infoWindow, rectangles, sessionIds, buildQueryParamsForCrowdMapLayer) => rectangleData => {
+  infoWindow.show("/api/region", buildQueryParamsForCrowdMapLayer.call(sessionIds, rectangleData), rectangles.position(rectangleData));
 };

app/assets/javascripts/code/services/build_query_params_for_averages.js
@@ -1,10 +1,0 @@
-import { buildQueryParamsForAverages } from './_build_query_params_for_averages';
-
-angular.module("aircasting").factory('buildQueryParamsForAverages', [
-  'map',
-  'sensors',
-  'params',
-  'utils',
-  'flash',
-  buildQueryParamsForAverages
-]);

app/assets/javascripts/code/services/update_crowd_map_layer.js
@@ -4,7 +4,7 @@ angular.module("aircasting").factory('updateCrowdMapLayer', [
   'storage',
   'map',
   '$http',
-  'buildQueryParamsForAverages',
+  'buildQueryParamsForCrowdMapLayer',
   'flash',
   '$location',
   'params',

app/assets/javascripts/tests/_build_query_params_for_crowd_map_layer.test.js
@@ -1,13 +1,13 @@
 import test from 'blue-tape';
 import { mock } from './helpers';
-import { buildQueryParamsForAverages } from '../code/services/_build_query_params_for_averages';
+import { buildQueryParamsForCrowdMapLayer } from '../code/services/_build_query_params_for_crowd_map_layer';
 
 test('when no sensor is selected it flashes a message and returns false', t => {
   const sensors = {
     selected: () => false
   };
   const flash = mock('set');
-  const service = _buildQueryParamsForAverages({ sensors, flash });
+  const service = _buildQueryParamsForCrowdMapLayer({ sensors, flash });
 
   const actual = service.call();
 
@@ -19,7 +19,7 @@ test('when no sensor is selected it flashes a message and returns false', t => {
 
 test('when one coordinate is missing in bounds it returns false', t => {
   const bounds = { west: null, east: 2, north: 3, south: 4 }
-  const service = _buildQueryParamsForAverages({});
+  const service = _buildQueryParamsForCrowdMapLayer({});
 
   const actual = service.call(null, bounds);
 
@@ -35,7 +35,7 @@ test('when time is missing in params it returns false', t => {
       gridResolution: 123
     })
   };
-  const service = _buildQueryParamsForAverages({ params });
+  const service = _buildQueryParamsForCrowdMapLayer({ params });
 
   const actual = service.call(null, { west: 1, east: 2, north: 3, south: 4 });
 
@@ -51,7 +51,7 @@ test('when heat is missing in params it returns false', t => {
       gridResolution: 123
     })
   };
-  const service = _buildQueryParamsForAverages({ params });
+  const service = _buildQueryParamsForCrowdMapLayer({ params });
 
   const actual = service.call(null, { west: 1, east: 2, north: 3, south: 4 });
 
@@ -67,7 +67,7 @@ test('when gridResolution is missing in params it returns false', t => {
       heat: {}
     })
   };
-  const service = _buildQueryParamsForAverages({ params });
+  const service = _buildQueryParamsForCrowdMapLayer({ params });
 
   const actual = service.call(null, { west: 1, east: 2, north: 3, south: 4 });
 
@@ -110,7 +110,7 @@ test('when everything is present it returns params for averages', t => {
     normalizeTime: x => x,
     gridSizeX: x => grid_size_x
   };
-  const service = buildQueryParamsForAverages(sensors, params, utils);
+  const service = buildQueryParamsForCrowdMapLayer(sensors, params, utils);
   const sessionIds = [3, 4];
   const bounds = { west, east, north, south };
 
@@ -143,7 +143,7 @@ test('when everything is present it returns params for averages', t => {
   t.end();
 });
 
-const _buildQueryParamsForAverages = ({ sensors, params, flash }) => {
+const _buildQueryParamsForCrowdMapLayer = ({ sensors, params, flash }) => {
   const sensor_name = "sensor_name";
   const measurement_type = "measurement_type";
   const unit_symbol = "unit_symbol";
@@ -181,5 +181,5 @@ const _buildQueryParamsForAverages = ({ sensors, params, flash }) => {
   };
   const _flash = flash || {};
 
-  return buildQueryParamsForAverages(_sensors, _params, utils, _flash);
+  return buildQueryParamsForCrowdMapLayer(_sensors, _params, utils, _flash);
 };

app/assets/javascripts/tests/_update_crowd_map_layer.test.js
@@ -28,16 +28,16 @@ test('when the request for the averages fails it flashes an error', t => {
 
 test('it delegates building the query param to a service passing it the session ids', t => {
   const calls = [];
-  const buildQueryParamsForAverages = {
+  const buildQueryParamsForCrowdMapLayer = {
     call: arg => { calls.push(arg); return false },
     wasCalledWith: arg => calls[calls.length - 1] === arg
   };
-  const service = _updateCrowdMapLayer({ buildQueryParamsForAverages });
+  const service = _updateCrowdMapLayer({ buildQueryParamsForCrowdMapLayer });
   const sessionIds = [1, 2];
 
   service.call(sessionIds);
 
-  t.true(buildQueryParamsForAverages.wasCalledWith(sessionIds));
+  t.true(buildQueryParamsForCrowdMapLayer.wasCalledWith(sessionIds));
 
   t.end();
 });
@@ -74,10 +74,10 @@ test('when the request for the averages succeeds and the app is on the mobile ta
   t.end();
 });
 
-test('when buildQueryParamsForAverages return false it does not request the averages', t => {
-  const buildQueryParamsForAverages = { call: () => false };
+test('when buildQueryParamsForCrowdMapLayer return false it does not request the averages', t => {
+  const buildQueryParamsForCrowdMapLayer = { call: () => false };
   const $http = mock('get');
-  const service = _updateCrowdMapLayer({ $http, buildQueryParamsForAverages });
+  const service = _updateCrowdMapLayer({ $http, buildQueryParamsForCrowdMapLayer });
 
   service.call();
 
@@ -115,7 +115,10 @@ test('when the request for the averages succeeds and the app is on the mobile ta
   const $location = { path: () => "/map_sessions" };
   const infoWindow = mock('show');
   const queryParamsForAverages = {};
-  const service = _updateCrowdMapLayer({ map, $http, $location, infoWindow });
+  const buildQueryParamsForCrowdMapLayer = {
+    call: () => queryParamsForAverages
+  };
+  const service = _updateCrowdMapLayer({ map, $http, buildQueryParamsForCrowdMapLayer, $location, infoWindow });
 
   service.call();
 
@@ -139,7 +142,7 @@ const mockHttp = ({ shouldFail }) => {
   };
 };
 
-const _updateCrowdMapLayer = ({ storage, map, $http, flash, buildQueryParamsForAverages, $location, infoWindow, rectangles }) => {
+const _updateCrowdMapLayer = ({ storage, map, $http, flash, buildQueryParamsForCrowdMapLayer, $location, infoWindow, rectangles }) => {
   const _storage = {
     isCrowdMapLayerOn: () => true,
     ...storage
@@ -149,9 +152,9 @@ const _updateCrowdMapLayer = ({ storage, map, $http, flash, buildQueryParamsForA
     getBounds: () => {},
     ...map
   };
-  const _buildQueryParamsForAverages = {
+  const _buildQueryParamsForCrowdMapLayer = {
     call: () => ({}),
-    ...buildQueryParamsForAverages
+    ...buildQueryParamsForCrowdMapLayer
   };
   const params = {
     get: () => ({})
@@ -163,5 +166,5 @@ const _updateCrowdMapLayer = ({ storage, map, $http, flash, buildQueryParamsForA
     position: () => {}
   };
 
-  return updateCrowdMapLayer(_storage, _map, $http, _buildQueryParamsForAverages, flash, $location, params, utils, infoWindow, _rectangles);
+  return updateCrowdMapLayer(_storage, _map, $http, _buildQueryParamsForCrowdMapLayer, flash, $location, params, utils, infoWindow, _rectangles);
 };

commit da591a8490dd0ef28edc9d356ba08ade349b15df

fix(mobile crowd): wrong file name
Changed the name of _build_query_params_for_averages in _build_query_params_for_crowd_map_layer
Broken in: e6670c03c8dcdda1871662ce01550d6fa4f4e8f4

app/assets/javascripts/code/services/build_query_params_for_crowd_map_layer.js
@@ -1,4 +1,4 @@
-import { buildQueryParamsForCrowdMapLayer } from './_build_query_params_for_averages';
+import { buildQueryParamsForCrowdMapLayer } from './_build_query_params_for_crowd_map_layer';
 
 angular.module("aircasting").factory('buildQueryParamsForCrowdMapLayer', [
   'map',