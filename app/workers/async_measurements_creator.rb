ad84f5d6aca1c448a0980885b0345b6f1b8d7e1f
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-01-16 12:47:07 +0100

    fix(sidekiq): add new measurements only when stream exists

--- app/workers/async_measurements_creator.rb
+++ app/workers/async_measurements_creator.rb
@@ -2,8 +2,10 @@ class AsyncMeasurementsCreator
   include Sidekiq::Worker
 
   def perform(stream_id, measurements_attributes)
-    stream = streams_repository.find(stream_id)
-    measurements_creator.call(stream, measurements_attributes)
+    stream = streams_repository.find_by_id(stream_id)
+    if stream
+      measurements_creator.call(stream, measurements_attributes)
+    end
   end
 
   private
