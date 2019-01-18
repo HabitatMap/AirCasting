4b2b1395569c7a2fa0c74e5550b4ebe1ba2dd9b7
3v0k4 <riccardo.odone@gmail.com>
2019-01-18 11:00:22 +0100

    keep secret_token too

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+AirCasting::Application.config.secret_token = 'fd39e34292e6363f1640e5bc495a9268076b27eed4ccd544e22039ae8510a99f3a05a72c6c06cbbe4127a98bc9c255945d126138e7cbb0d846c95c58d9d607e4'
 AirCasting::Application.config.secret_key_base = 'fd39e34292e6363f1640e5bc495a9268076b27eed4ccd544e22039ae8510a99f3a05a72c6c06cbbe4127a98bc9c255945d126138e7cbb0d846c95c58d9d607e4'
