d84d40841efb3478fc8b7ed9911312c0fe7f1e3f
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-01-24 12:35:59 +0100

    feat(new-markers): add session class to values

--- app/assets/javascripts/code/values/session.js
+++ app/assets/javascripts/code/values/session.js
@@ -9,3 +9,27 @@ export const formatSessionForList = session => ({
   timeframe: session.timeframe,
   shortTypes: session.shortTypes
 });
+
+export class Session {
+  constructor(session) {
+    this.session = session;
+  };
+
+  roundedAverage() {
+    return Math.round(this.session.average)
+  };
+
+  average() {
+    return this.session.average
+  };
+
+  selectedSensorUnit(selectedSensor) {
+    this.session.streams[selectedSensor]["unit_symbol"];
+  };
+
+  startingLatLng(selectedSensor) {
+    const latitude = this.session.streams[selectedSensor]["start_latitude"];
+    const longitude = this.session.streams[selectedSensor]["start_longitude"];
+    return new google.maps.LatLng(latitude, longitude);
+  };
+}
