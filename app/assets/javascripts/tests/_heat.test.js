eecb377a772d6931b2809a10e73b759309972b57
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-01-25 14:26:50 +0100

    feat(new-markers): add tests for custom markers

--- app/assets/javascripts/tests/_heat.test.js
+++ app/assets/javascripts/tests/_heat.test.js
@@ -22,8 +22,58 @@ test('outsideOfScope return false when value inside scope', t => {
   t.end()
 });
 
+test('levelName returns low when heat level between lowest and low' , t => {
+  const heatStub = _heat();
+
+  const actual = heatStub.levelName(1)
+
+  t.equal(actual, "low")
+
+  t.end()
+});
+
+test('levelName returns mid when heat level between low and mid' , t => {
+  const heatStub = _heat();
+
+  const actual = heatStub.levelName(11)
+
+  t.equal(actual, "mid")
+
+  t.end()
+});
+
+test('levelName returns high when heat level between mid and high' , t => {
+  const heatStub = _heat();
+
+  const actual = heatStub.levelName(51)
+
+  t.equal(actual, "high")
+
+  t.end()
+});
+
+test('levelName returns highest when heat level between high and highest' , t => {
+  const heatStub = _heat();
+
+  const actual = heatStub.levelName(101)
+
+  t.equal(actual, "highest")
+
+  t.end()
+});
+
+test('levelName returns null when heat level outside of scope' , t => {
+  const heatStub = _heat();
+
+  const actual = heatStub.levelName(151)
+
+  t.equal(actual, null)
+
+  t.end()
+});
+
 const _heat = () => {
-  const params = { get: () => ({ heat: { lowest: 0, highest: 150 }})};
+  const params = { get: () => ({ heat: { lowest: 0, low: 10, mid: 50, high: 100, highest: 150 }})};
   const $rootScope = { $new: () => ({ $watch: () => {} }) };
 
   return heat($rootScope, params);
