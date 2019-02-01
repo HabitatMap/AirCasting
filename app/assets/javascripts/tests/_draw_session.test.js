6bfb969dace77269ec5acbe8e5a6a79de5237ef2
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-02-01 13:03:05 +0100

    add tests

--- app/assets/javascripts/tests/_draw_session.test.js
+++ app/assets/javascripts/tests/_draw_session.test.js
@@ -16,9 +16,46 @@ test('clearOtherSessions removes all markers expect the selected one', t => {
   t.end();
 });
 
+test('drawMobileSession draws a session when session is loaded and sensor is selected', t => {
+  const map = mock('drawMarker');
+  const drawSessionStub = _drawSession({ map, sensors: selectedSensor });
+
+  drawSessionStub.drawMobileSession(loadedSession);
+
+  t.true(map.wasCalled());
+  t.true(loadedSession.drawed);
+
+  t.end();
+});
+
+test('undoDraw removes all session elements from the map', t => {
+  const map = mock('removeMarker');
+  const session = { drawed: true, markers: [1], lines: [1], noteDrawings: [1] }
+  const drawSessionStub = _drawSession({ map });
+
+  drawSessionStub.undoDraw(session)
+
+  t.true(map.wasCalledNTimes(3));
+  t.deepEqual(session.markers, [])
+  t.deepEqual(session.lines , [])
+  t.deepEqual(session.noteDrawings, [])
+  t.false(session.drawed)
+
+  t.end();
+});
+
+const measurement = { value: 1, latitude: 2, longitude: 3 };
+const selectedSensor = { anySelected: () => ({ sensor_name: "sensorName" })};
+const loadedSession = {
+  loaded: true,
+  markers: [],
+  lines: [],
+  streams: { sensorName: { unit_symbol: "unit", measurements: [measurement]}}
+};
+
 const _drawSession = ({ map, sensors, heat }) => {
-  const _map = { drawMarker: () => ({}), ...map };
-  const _heat = { getLevel: () => {}, ...heat };
+  const _map = { drawMarker: () => ({}), drawLine: () => ({}), ...map };
+  const _heat = { getLevel: () => {}, outsideOfScope: () => false, ...heat };
   const _sensors = { ...sensors };
   return drawSession(_sensors, _map, _heat);
 };
