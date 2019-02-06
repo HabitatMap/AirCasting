45c33f36161494776288f6febf40f060659fc5ec
3v0k4 <riccardo.odone@gmail.com>
2019-02-06 11:37:19 +0100

    feat(measurements creator): label measurements when passing to async

--- app/services/measurements_creator.rb
+++ app/services/measurements_creator.rb
@@ -1,5 +1,6 @@
 class MeasurementsCreator
   SLICE_SIZE = 500
+  AMOUNT_THRESHOLD = 20_000
 
   def initialize(
     sync_measurements_creator: SyncMeasurementsCreator.new,
@@ -12,8 +13,10 @@ class MeasurementsCreator
   def call(stream, measurements_attributes)
     if measurements_attributes.one?
       create_one_sync(stream, measurements_attributes)
+    elsif measurements_attributes.size < AMOUNT_THRESHOLD
+      create_several_async(stream.id, measurements_attributes)
     else
-      create_multiple_async(stream.id, measurements_attributes)
+      create_many_async(stream.id, measurements_attributes)
     end
   end
 
@@ -23,9 +26,17 @@ class MeasurementsCreator
     @sync_measurements_creator.call(stream, measurements_attributes)
   end
 
-  def create_multiple_async(stream_id, measurements_attributes)
+  def create_several_async(stream_id, measurements_attributes)
+    create_multiple_async(stream_id, measurements_attributes, :several)
+  end
+
+  def create_many_async(stream_id, measurements_attributes)
+    create_multiple_async(stream_id, measurements_attributes, :many)
+  end
+
+  def create_multiple_async(stream_id, measurements_attributes, amount)
     measurements_attributes.each_slice(SLICE_SIZE) do |attributes|
-      @async_measurements_creator.perform_async(stream_id: stream_id, measurements_attributes: attributes)
+      @async_measurements_creator.perform_async(stream_id: stream_id, measurements_attributes: attributes, amount: amount)
     end
   end
 end
--- spec/services/measurements_creator_spec.rb
+++ spec/services/measurements_creator_spec.rb
@@ -12,7 +12,7 @@ describe MeasurementsCreator do
     subject.call(stream, measurement_attributes)
 	end
 
-  it "with more than 1 measurement it delegates to async_measurements_creator" do
+  it "with more than 1 measurement it delegates to async_measurements_creator and labeling them as several" do
     async_measurements_creator = class_double(AsyncMeasurementsCreator)
     subject = MeasurementsCreator.new(async_measurements_creator: async_measurements_creator)
     stream_id = 1
@@ -22,13 +22,13 @@ describe MeasurementsCreator do
 
     expect(async_measurements_creator)
       .to receive(:perform_async)
-      .with(stream_id: stream_id, measurements_attributes: measurement_attributes)
+      .with(stream_id: stream_id, measurements_attributes: measurement_attributes, amount: :several)
       .once
 
     subject.call(stream, measurement_attributes)
 	end
 
-  it "with 501 measurements it delegates to async_measurements_creator by batching in groups of 500" do
+  it "with 501 measurements it delegates to async_measurements_creator by batching in groups of 500 and labeling them as several" do
     async_measurements_creator = class_double(AsyncMeasurementsCreator)
     subject = MeasurementsCreator.new(async_measurements_creator: async_measurements_creator)
     stream_id = 1
@@ -38,12 +38,29 @@ describe MeasurementsCreator do
 
     expect(async_measurements_creator)
       .to receive(:perform_async)
-      .with(stream_id: stream_id, measurements_attributes: [{}] * 500)
+      .with(stream_id: stream_id, measurements_attributes: [{}] * 500, amount: :several)
       .once
     expect(async_measurements_creator)
       .to receive(:perform_async)
-      .with(stream_id: stream_id, measurements_attributes: [{}])
+      .with(stream_id: stream_id, measurements_attributes: [{}], amount: :several)
       .once
+
+    subject.call(stream, measurement_attributes)
+	end
+
+  it "with 20_000 measurements it delegates to async_measurements_creator by batching in groups of 500 and labeling them as many" do
+    async_measurements_creator = class_double(AsyncMeasurementsCreator)
+    subject = MeasurementsCreator.new(async_measurements_creator: async_measurements_creator)
+    stream_id = 1
+    stream = Stream.new
+    stream.id = stream_id
+    measurement_attributes = [{}] * 20_000
+
+    expect(async_measurements_creator)
+      .to receive(:perform_async)
+      .with(stream_id: stream_id, measurements_attributes: [{}] * 500, amount: :many)
+      .exactly(40)
+      .times
 
     subject.call(stream, measurement_attributes)
 	end
