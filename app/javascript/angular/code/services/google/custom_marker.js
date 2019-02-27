HabitatMap/AirCasting

commit 0bf0676e08dc91aa99110731ee28f3a917ed188e

fix(markers): notes and info window on top of custom markers

app/javascript/angular/code/services/google/custom_marker.js
@@ -19,7 +19,7 @@ export function buildCustomMarker(latLng, content, colorClass, callback, type) {
   CustomMarker.prototype = Object.create(google.maps.OverlayView.prototype);
 
   CustomMarker.prototype.onAdd = function() {
-    this.getPanes().floatPane.appendChild(this.markerContainer);
+    this.getPanes().markerLayer.appendChild(this.markerContainer);
   };
 
   CustomMarker.prototype.onRemove = function() {

commit c4026014e383232713f28beec0e52ed627fb2afe

fix(marker): click on session marker selects session
Broken in 0bf0676e08dc91aa99110731ee28f3a917ed188e. This commit
unfortunately puts the note marker behind the starting session marker

app/javascript/angular/code/services/google/custom_marker.js
@@ -14,12 +14,11 @@ export function buildCustomMarker(latLng, content, colorClass, callback, type) {
 
     this.stopEventPropagation();
   };
-    // NOTE: google.maps.OverlayView is only defined once the Maps API has
-    // loaded. That is why CustomMarker is defined inside initMap().
+
   CustomMarker.prototype = Object.create(google.maps.OverlayView.prototype);
 
   CustomMarker.prototype.onAdd = function() {
-    this.getPanes().markerLayer.appendChild(this.markerContainer);
+    this.getPanes().overlayMouseTarget.appendChild(this.markerContainer);
   };
 
   CustomMarker.prototype.onRemove = function() {