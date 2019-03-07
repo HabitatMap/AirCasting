3a2111b30d7f2b267ae87fc0ff709f638dc1592f
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-03-07 14:06:06 +0100

    feat(time-filter): add TimeRange data structure to elm

---
+++ app/javascript/elm/tests/FixedSessionsFiltersTests.elm
@@ -0,0 +1,48 @@
+module FixedSessionsFiltersTests exposing (timeFilter)
+
+import Expect
+import FixedSessionFilters exposing (..)
+import Fuzz exposing (int)
+import Json.Encode as Encode
+import Ports
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector as Slc
+import TimeRange
+
+
+timeFilter : Test
+timeFilter =
+    describe "Time filter tests: "
+        [ test "Time filter is present" <|
+            \_ ->
+                defaultModel
+                    |> view
+                    |> Query.fromHtml
+                    |> Query.has [ Slc.id "test-time-filter" ]
+        , fuzz2 int int "UpdateTimeRange returns the updated model" <|
+            \timeFrom timeTo ->
+                let
+                    value =
+                        Encode.object [ ( "timeFrom", Encode.int timeFrom ), ( "timeTo", Encode.int timeTo ) ]
+
+                    expected =
+                        { defaultModel
+                            | timeRange = TimeRange.update defaultModel.timeRange value
+                        }
+                in
+                defaultModel
+                    |> update (UpdateTimeRange value)
+                    |> Tuple.first
+                    |> Expect.equal expected
+        , fuzz2 int int "UpdateTimeRange triggers updateTimeRange port" <|
+            \timeFrom timeTo ->
+                let
+                    value =
+                        Encode.object [ ( "timeFrom", Encode.int timeFrom ), ( "timeTo", Encode.int timeTo ) ]
+                in
+                defaultModel
+                    |> update (UpdateTimeRange value)
+                    |> Tuple.second
+                    |> Expect.equal (Ports.updateTimeRange value)
+        ]
