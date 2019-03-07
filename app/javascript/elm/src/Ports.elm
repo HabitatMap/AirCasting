7fbbcb179242fb18d4d0e9e81c921d46774b30b8
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-03-07 14:06:06 +0100

    feat(time-filter): add TimeRange data structure to elm

--- app/javascript/elm/src/Ports.elm
+++ app/javascript/elm/src/Ports.elm
@@ -1,4 +1,6 @@
-port module Ports exposing (profileNameSelected, tagSelected, toggleCrowdMap, updateProfiles, updateResolutionPort, updateTags)
+port module Ports exposing (profileNameSelected, tagSelected, timeRangeSelected, toggleCrowdMap, updateProfiles, updateResolution, updateTags, updateTimeRange)
+
+import Json.Encode as Encode
 
 
 port tagSelected : (String -> msg) -> Sub msg
@@ -7,13 +9,19 @@ port tagSelected : (String -> msg) -> Sub msg
 port profileNameSelected : (String -> msg) -> Sub msg
 
 
+port timeRangeSelected : (Encode.Value -> msg) -> Sub msg
+
+
 port toggleCrowdMap : () -> Cmd a
 
 
-port updateResolutionPort : Int -> Cmd a
+port updateResolution : Int -> Cmd a
 
 
 port updateTags : List String -> Cmd a
 
 
 port updateProfiles : List String -> Cmd a
+
+
+port updateTimeRange : Encode.Value -> Cmd a
