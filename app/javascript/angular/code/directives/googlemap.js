e6b63a47d462fbf2492d7d7aa3dca33ca561831c
Paulina Materna <paulina.materna@lunarlogic.io>
2019-03-08 13:24:44 +0100

    New map styles

--- app/javascript/angular/code/directives/googlemap.js
+++ app/javascript/angular/code/directives/googlemap.js
@@ -14,12 +14,13 @@ angular.module('aircasting').directive('googlemap', function() {
       var lng = params.lng || map.getMapCookie('vp_lng') || point.lng;
       var latlng = new google.maps.LatLng(lat, lng);
       var zoom = params.zoom || map.getMapCookie('vp_zoom') || point.zoom;
-      var mapType = params.mapType || map.getMapCookie('vp_mapType') || google.maps.MapTypeId.TERRAIN;
+      var mapType = params.mapType || map.getMapCookie('vp_mapType') || google.maps.MapTypeId.roadmap;
       const minZoom = 3;
       var options = {
         zoom: parseInt(zoom, 10),
         minZoom,
         center: latlng,
+        styles: [],
         mapTypeId: mapType,
         mapTypeControl: true,
         mapTypeControlOptions: {
