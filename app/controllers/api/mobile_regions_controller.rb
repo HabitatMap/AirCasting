7ef604b6ee159220da11c80a2648bfe2856c5d04
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-03-14 10:43:09 +0100

    feat(fixed-clustering): split regions into separate endpoints for mobile and fixed

---
+++ app/controllers/api/mobile_regions_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  class MobileRegionsController < BaseController
+    FLOAT_Q_ATTRS = [:north, :south, :east, :west]
+    INT_Q_ATTRS = [
+      :time_from, :time_to,
+      :grid_size_x, :grid_size_y
+    ]
+
+    def show
+      data = ActiveSupport::JSON.decode(params[:q]).symbolize_keys
+      data[:session_ids] ||= []
+
+      FLOAT_Q_ATTRS.each { |key| data[key] = data[key].to_f if data.key?(key) }
+      INT_Q_ATTRS.each { |key| data[key] = data[key].to_i if data.key?(key) }
+
+      data[:time_from] = data[:time_from] || Time.new(2010).to_i
+      data[:time_to] = data[:time_to] || Time.new(2100).end_of_year.to_i
+
+      respond_with MobileRegionInfo.new(data)
+    end
+  end
+end
