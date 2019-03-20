74eaf883293bec9af0fc8309999cd3b0a07a71cc
3v0k4 <riccardo.odone@gmail.com>
2019-03-20 12:22:41 +0100

    wip: do not clear on reselect

--- app/javascript/angular/code/services/_mobile_sessions.js
+++ app/javascript/angular/code/services/_mobile_sessions.js
@@ -103,7 +103,7 @@ export const mobileSessions = (
     reSelectSession: function(id) {
       // this is called when refreshing a page with selected session
       setTimeout(() => {
-        drawSession.clear(this.sessions);
+        //drawSession.clear(this.sessions);
         const callback = (session, allSelected) => (data) => {
           const drawSessionStartingMarker = (session, sensorName) => this.drawSessionWithLabel(session, sensorName);
           const draw = () => drawSession.drawMobileSession(session, drawSessionStartingMarker);
