46bcf35be4210a2a355e9b0bf44119f6e924518e
3v0k4 <riccardo.odone@gmail.com>
2019-03-20 11:09:01 +0100

    add console.log to debug

--- app/javascript/angular/code/services/_draw_session.js
+++ app/javascript/angular/code/services/_draw_session.js
@@ -23,6 +23,7 @@ export const drawSession = (
 
       this.measurements(session).forEach(function(measurement, idx){
         const marker = createMeasurementMarker(measurement, idx, heat, map, suffix);
+	console.log('after createMeasurementMarker');
 
         session.markers.push(marker);
         points.push(measurement);
