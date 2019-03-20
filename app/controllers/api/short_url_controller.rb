9845ddf6fe3885887f50c71e25390463b783b0e4
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-03-20 11:09:08 +0100

    refactor(copy-link): use fetch instead of $http

--- app/controllers/api/short_url_controller.rb
+++ app/controllers/api/short_url_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class ShortUrlController < BaseController
     def index
-      render plain: UrlShortener.new.call(params["longUrl"])
+      render json: { short_url: UrlShortener.new.call(params["longUrl"]) }
     end
   end
 end
