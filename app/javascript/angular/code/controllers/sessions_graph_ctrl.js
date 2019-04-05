5f61b594c1c0ff5137b9efba2e9e0491693fca1e
3v0k4 <riccardo.odone@gmail.com>
2019-04-05 11:47:04 +0200

    feat(graph): load selected session from url

--- app/javascript/angular/code/controllers/sessions_graph_ctrl.js
+++ app/javascript/angular/code/controllers/sessions_graph_ctrl.js
@@ -1,36 +1,27 @@
-function SessionsGraphCtrl($scope, map, graph, flash, heat, sensors,
+function SessionsGraphCtrl($scope, map, graph, heat, sensors,
                            graphHighlight, $window, $timeout) {
   $scope.graph = graph;
   $scope.$window = $window;
-  $scope.expanded = false;
+  $scope.isSingleSessionSelected = false;
   $scope.heat = heat;
   $scope.sensors = sensors;
   var singleSession = $scope.singleSession;
 
-  function updateExpanded() {
-    $scope.expanded = singleSession.isSingle() && !_.isEmpty(sensors.anySelected());
-  }
-
-  $scope.$watch('sensors.anySelected()', function() {
-    console.log('watch - sensors.anySelected()');
-    updateExpanded();
-  });
-
   $scope.$watch('singleSession.isSingle()', function() {
     console.log('watch - singleSession.isSingle()');
-    updateExpanded();
+    $scope.isSingleSessionSelected = singleSession.isSingle();
   });
 
   $scope.$watch('singleSession.get().loaded', function() {
     console.log('watch - singleSession.get().loaded');
-    if ($scope.expanded && !_.isEmpty(singleSession.measurements())) {
+    if ($scope.isSingleSessionSelected && !_.isEmpty(singleSession.measurements())) {
       $scope.redraw();
     }
   });
 
-  $scope.$watch("expanded", function(expanded) {
-    console.log("watch - expanded");
-    if(!expanded){
+  $scope.$watch("isSingleSessionSelected", function(isSingleSessionSelected) {
+    console.log("watch - isSingleSessionSelected");
+    if(!isSingleSessionSelected){
       graphHighlight.hide();
     } else {
       $timeout(function(){
@@ -40,23 +31,8 @@ function SessionsGraphCtrl($scope, map, graph, flash, heat, sensors,
     }
   });
 
-  $scope.toggle = function(){
-    var sessionsSize = singleSession.noOfSelectedSessions();
-    if(sessionsSize === 0) {
-      flash.set("Please select one session to view the graph");
-      return;
-    } else if(sessionsSize > 1) {
-      flash.set("You can have only one session selected to view the graph. Currently you have " + sessionsSize);
-      return;
-    } else if(!singleSession.get().loaded) {
-      flash.set("You need to wait till session be loaded");
-      return;
-    }
-    $scope.expanded = !$scope.expanded;
-  };
-
   $scope.shouldRedraw = function() {
-    return singleSession.isSingle() && !!sensors.anySelected() && !!singleSession.get().loaded;
+    return singleSession.isSingle() && !!singleSession.get().loaded;
   };
 
   $scope.$watch("heat.getValues()", function() {
@@ -76,6 +52,6 @@ function SessionsGraphCtrl($scope, map, graph, flash, heat, sensors,
       graph.draw(singleSession.measurementsToTime(), false);
   };
 }
-SessionsGraphCtrl.$inject = ['$scope', 'map',  'graph', 'flash', 'heat', 'sensors',
+SessionsGraphCtrl.$inject = ['$scope', 'map',  'graph', 'heat', 'sensors',
   'graphHighlight', '$window', '$timeout'];
 angular.module('aircasting').controller('SessionsGraphCtrl', SessionsGraphCtrl);
