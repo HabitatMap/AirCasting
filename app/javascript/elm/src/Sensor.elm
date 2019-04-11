45e52ffa8bfea3c7842fb496d9baeee9769a7ae0
3v0k4 <riccardo.odone@gmail.com>
2019-04-11 11:58:15 +0200

    feat(sensors): select default when parameter changes

--- app/javascript/elm/src/Sensor.elm
+++ app/javascript/elm/src/Sensor.elm
@@ -14,6 +14,49 @@ import Json.Encode as Encode
 import Set
 
 
+defaultSensorId : String
+defaultSensorId =
+    "Particulate Matter-airbeam2-pm2.5 (µg/m³)"
+
+
+defaultSensorIdByParameter : Dict.Dict String String
+defaultSensorIdByParameter =
+    Dict.fromList
+        [ ( "Particulate Matter", defaultSensorId )
+        , ( "Humidity", "Humidity-airbeam2-rh (%)" )
+        , ( "Sound Level", "Sound Level-phone microphone (dB)" )
+        , ( "Temperature", "Temperature-airbeam2-f (F)" )
+        ]
+
+
+mainSensors : Dict.Dict String (List String)
+mainSensors =
+    Dict.fromList
+        [ ( "Particulate Matter"
+          , [ "AirBeam2-PM2.5 (µg/m³)"
+            , "AirBeam2-PM1 (µg/m³)"
+            , "AirBeam2-PM10 (µg/m³)"
+            , "AirBeam-PM (µg/m³)"
+            ]
+          )
+        , ( "Humidity", [ "AirBeam2-RH (%)", "AirBeam-RH (%)" ] )
+        , ( "Temperature", [ "AirBeam2-F (F)", "AirBeam-F (F)" ] )
+        , ( "Sound Level", [ "Phone Microphone (dB)" ] )
+        ]
+
+
+
+{-
+   { id_ = "Particulate Matter-airbeam2-pm2.5 (µg/m³)"
+   , label = "AirBeam2-PM2.5 (µg/m³)"
+   , parameter = "Particulate Matter"
+   , sensor = "AirBeam2-PM2.5"
+   , session_count = 24
+   , unit = "µg/m³"
+   }
+-}
+
+
 type alias Sensor =
     { id_ : String
     , sensor : String
@@ -103,34 +146,41 @@ labelsForParameter sensors sensorId =
 
 
 idForParameterOrLabel : String -> String -> List Sensor -> String
-idForParameterOrLabel key oldSensorId sensors =
-    sensors
-        |> List.filter (\sensor -> sensor.parameter == key)
-        |> List.sortBy .session_count
-        |> List.reverse
-        |> List.head
-        |> Maybe.map .id_
-        |> Maybe.withDefault
-            (sensors
-                |> List.filter (\sensor -> sensor.label == key && sensor.parameter == parameterForId sensors oldSensorId)
+idForParameterOrLabel parameterOrLabel oldSensorId sensors =
+    let
+        byId id =
+            sensors
+                |> List.filter (\sensor -> sensor.id_ == id)
                 |> List.head
                 |> Maybe.map .id_
-                |> Maybe.withDefault
-                    "Particulate Matter-airbeam2-pm2.5 (µg/m³)"
-            )
 
+        maybeDefault =
+            Dict.get parameterOrLabel defaultSensorIdByParameter
+                |> Maybe.andThen byId
 
-mainSensors : Dict.Dict String (List String)
-mainSensors =
-    Dict.fromList
-        [ ( "Particulate Matter"
-          , [ "AirBeam2-PM2.5 (µg/m³)"
-            , "AirBeam2-PM1 (µg/m³)"
-            , "AirBeam2-PM10 (µg/m³)"
-            , "AirBeam-PM (µg/m³)"
-            ]
-          )
-        , ( "Humidity", [ "AirBeam2-RH (%)", "AirBeam-RH (%)" ] )
-        , ( "Temperature", [ "AirBeam2-F (F)", "AirBeam-F (F)" ] )
-        , ( "Sound Level", [ "Phone Microphone (dB)" ] )
-        ]
+        maybeByParameter =
+            sensors
+                |> List.filter (\sensor -> sensor.parameter == parameterOrLabel)
+                |> List.sortBy .session_count
+                |> List.reverse
+                |> List.head
+                |> Maybe.map .id_
+
+        maybeByLabel =
+            sensors
+                |> List.filter (\sensor -> sensor.label == parameterOrLabel && sensor.parameter == parameterForId sensors oldSensorId)
+                |> List.head
+                |> Maybe.map .id_
+    in
+    case ( maybeDefault, maybeByParameter, maybeByLabel ) of
+        ( Just default, _, _ ) ->
+            default
+
+        ( _, Just byParameter, _ ) ->
+            byParameter
+
+        ( _, _, Just byLabel ) ->
+            byLabel
+
+        _ ->
+            defaultSensorId
