3f0537a34efeb6b7ace1bdbfbb7e8b1126c1cd6d
3v0k4 <riccardo.odone@gmail.com>
2019-04-17 10:58:40 +0200

    feat(heat): reset to defaults

--- app/javascript/elm/src/Data/HeatMapThresholds.elm
+++ app/javascript/elm/src/Data/HeatMapThresholds.elm
@@ -1,4 +1,4 @@
-module Data.HeatMapThresholds exposing (HeatMapThresholds, extremes, fetch, updateMaximum, updateMinimum)
+module Data.HeatMapThresholds exposing (HeatMapThresholdValues, HeatMapThresholds, Threshold, extremes, fetch, resetToDefaults, toValues, updateMaximum, updateMinimum)
 
 import Http
 import Json.Decode as Decode exposing (Decoder(..))
@@ -6,7 +6,20 @@ import Sensor exposing (Sensor)
 import Url
 
 
+type alias Threshold =
+    { default : Int, value : Int }
+
+
 type alias HeatMapThresholds =
+    { threshold1 : Threshold
+    , threshold2 : Threshold
+    , threshold3 : Threshold
+    , threshold4 : Threshold
+    , threshold5 : Threshold
+    }
+
+
+type alias HeatMapThresholdValues =
     { threshold1 : Int
     , threshold2 : Int
     , threshold3 : Int
@@ -15,19 +28,49 @@ type alias HeatMapThresholds =
     }
 
 
+updateThresholdValue : Int -> Threshold -> Threshold
+updateThresholdValue value threshold =
+    { threshold | value = value }
+
+
+resetThresholdValueToDefault : Threshold -> Threshold
+resetThresholdValueToDefault threshold =
+    { threshold | value = threshold.default }
+
+
+toValues : HeatMapThresholds -> HeatMapThresholdValues
+toValues { threshold1, threshold2, threshold3, threshold4, threshold5 } =
+    { threshold1 = threshold1.value
+    , threshold2 = threshold2.value
+    , threshold3 = threshold3.value
+    , threshold4 = threshold4.value
+    , threshold5 = threshold5.value
+    }
+
+
+resetToDefaults : HeatMapThresholds -> HeatMapThresholds
+resetToDefaults heatMapThresholds =
+    { threshold1 = resetThresholdValueToDefault heatMapThresholds.threshold1
+    , threshold2 = resetThresholdValueToDefault heatMapThresholds.threshold2
+    , threshold3 = resetThresholdValueToDefault heatMapThresholds.threshold3
+    , threshold4 = resetThresholdValueToDefault heatMapThresholds.threshold4
+    , threshold5 = resetThresholdValueToDefault heatMapThresholds.threshold5
+    }
+
+
 extremes : HeatMapThresholds -> ( Int, Int )
-extremes heatMapThresholds =
-    ( heatMapThresholds.threshold1, heatMapThresholds.threshold5 )
+extremes { threshold1, threshold5 } =
+    ( threshold1.value, threshold5.value )
 
 
 updateMinimum : Int -> HeatMapThresholds -> HeatMapThresholds
-updateMinimum threshold1 heatMapThresholds =
-    { heatMapThresholds | threshold1 = threshold1 }
+updateMinimum value heatMapThresholds =
+    { heatMapThresholds | threshold1 = updateThresholdValue value heatMapThresholds.threshold1 }
 
 
 updateMaximum : Int -> HeatMapThresholds -> HeatMapThresholds
-updateMaximum threshold5 heatMapThresholds =
-    { heatMapThresholds | threshold5 = threshold5 }
+updateMaximum value heatMapThresholds =
+    { heatMapThresholds | threshold5 = updateThresholdValue value heatMapThresholds.threshold5 }
 
 
 fetch : List Sensor -> String -> (Result Http.Error HeatMapThresholds -> msg) -> Maybe (Cmd msg)
@@ -67,9 +110,19 @@ intAsStringDecoder =
 
 decoder : Decoder HeatMapThresholds
 decoder =
-    Decode.map5 HeatMapThresholds
+    Decode.map5 toHeatMapThresholds
         (Decode.index 0 intAsStringDecoder)
         (Decode.index 1 intAsStringDecoder)
         (Decode.index 2 intAsStringDecoder)
         (Decode.index 3 intAsStringDecoder)
         (Decode.index 4 intAsStringDecoder)
+
+
+toHeatMapThresholds : Int -> Int -> Int -> Int -> Int -> HeatMapThresholds
+toHeatMapThresholds t1 t2 t3 t4 t5 =
+    HeatMapThresholds
+        { value = t1, default = t1 }
+        { value = t2, default = t2 }
+        { value = t3, default = t3 }
+        { value = t4, default = t4 }
+        { value = t5, default = t5 }
