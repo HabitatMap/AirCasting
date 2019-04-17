fb9873f873d65e6b55a61e1ef1129e9cf61946a1
Julia Jacobs <julia@lunarlogic.io>
2019-04-17 14:44:20 +0200

    refactor: prettify fixed ctrl js

--- app/javascript/angular/code/controllers/_fixed_sessions_map_ctrl.js
+++ app/javascript/angular/code/controllers/_fixed_sessions_map_ctrl.js
@@ -1,10 +1,18 @@
-import _ from 'underscore';
-import moment from 'moment'
+import _ from "underscore";
+import moment from "moment";
 
-import * as FiltersUtils from '../filtersUtils'
+import * as FiltersUtils from "../filtersUtils";
 
-const endOfToday = moment().utc().endOf('day').format('X');
-const oneYearAgo = moment().utc().startOf('day').subtract(1, 'year').format('X');
+const endOfToday = moment()
+  .utc()
+  .endOf("day")
+  .format("X");
+
+const oneYearAgo = moment()
+  .utc()
+  .startOf("day")
+  .subtract(1, "year")
+  .format("X");
 
 export const FixedSessionsMapCtrl = (
   $scope,
@@ -30,26 +38,28 @@ export const FixedSessionsMapCtrl = (
     $scope.singleSession = singleFixedSession;
     $scope.$window = $window;
 
-    functionBlocker.block("sessionHeat", !_(params.get('selectedSessionIds')).isEmpty());
+    functionBlocker.block(
+      "sessionHeat",
+      !_(params.get("selectedSessionIds")).isEmpty()
+    );
 
     map.clearRectangles();
     infoWindow.hide();
     map.unregisterAll();
     map.removeAllMarkers();
 
-    if (process.env.NODE_ENV !== 'test') {
+    if (process.env.NODE_ENV !== "test") {
       $($window).resize(function() {
         $scope.$digest();
       });
     }
 
-    const sensorId = params
-      .get('data', { sensorId: sensors.defaultSensorId })
+    const sensorId = params.get("data", { sensorId: sensors.defaultSensorId })
       .sensorId;
 
     const defaults = {
       sensorId,
-      location: '',
+      location: "",
       isIndoor: false,
       isStreaming: true,
       tags: "",
@@ -58,27 +68,36 @@ export const FixedSessionsMapCtrl = (
       timeTo: endOfToday
     };
 
-    if (!params.get('data').heat) sensors.fetchHeatLevels();
+    if (!params.get("data").heat) sensors.fetchHeatLevels();
 
     params.updateFromDefaults(defaults);
   };
 
-  $scope.$watch("params.get('data').heat", function(newValue, oldValue) {
-    console.log("watch - params.get('data').heat - ", newValue, " - ", oldValue);
-    if (newValue != oldValue) {
-      $scope.sessions.drawSessionsInLocation();
-    }
-  }, true);
+  $scope.$watch(
+    "params.get('data').heat",
+    function(newValue, oldValue) {
+      console.log(
+        "watch - params.get('data').heat - ",
+        newValue,
+        " - ",
+        oldValue
+      );
+      if (newValue != oldValue) {
+        $scope.sessions.drawSessionsInLocation();
+      }
+    },
+    true
+  );
 
   $scope.setDefaults();
 
-  if (process.env.NODE_ENV !== 'test') {
-    angular.element(document).ready(function () {
+  if (process.env.NODE_ENV !== "test") {
+    angular.element(document).ready(function() {
       const elmApp = window.__elmApp;
 
-      elmApp.ports.selectSensorId.subscribe(sensorId =>{
+      elmApp.ports.selectSensorId.subscribe(sensorId => {
         params.update({ selectedSessionIds: [] });
-        params.update({ data: { sensorId }});
+        params.update({ data: { sensorId } });
         sensors.fetchHeatLevels();
         $scope.sessions.fetch();
       });
@@ -88,12 +107,12 @@ export const FixedSessionsMapCtrl = (
       });
 
       elmApp.ports.toggleIndoor.subscribe(isIndoor => {
-        params.update({ data: { isIndoor: isIndoor }});
+        params.update({ data: { isIndoor: isIndoor } });
         $scope.sessions.fetch();
       });
 
       elmApp.ports.toggleStreaming.subscribe(isStreaming => {
-        params.update({ data: { isStreaming: isStreaming }});
+        params.update({ data: { isStreaming: isStreaming } });
         $scope.sessions.fetch();
       });
 
@@ -102,47 +121,54 @@ export const FixedSessionsMapCtrl = (
       });
 
       FiltersUtils.setupAutocomplete(
-        (selectedValue) => elmApp.ports.profileSelected.send(selectedValue)
-        , "profile-names"
-        , "/autocomplete/usernames"
-      )
+        selectedValue => elmApp.ports.profileSelected.send(selectedValue),
+        "profile-names",
+        "/autocomplete/usernames"
+      );
 
       FiltersUtils.setupAutocomplete(
-        (selectedValue) => elmApp.ports.tagSelected.send(selectedValue)
-        , "tags"
-        , "/autocomplete/tags"
-      )
+        selectedValue => elmApp.ports.tagSelected.send(selectedValue),
+        "tags",
+        "/autocomplete/tags"
+      );
 
-      elmApp.ports.updateTags.subscribe((tags) => {
-        params.update({data: {tags: tags.join(", ")}});
+      elmApp.ports.updateTags.subscribe(tags => {
+        params.update({ data: { tags: tags.join(", ") } });
         $scope.sessions.fetch();
       });
 
-      elmApp.ports.updateProfiles.subscribe((profiles) => {
-        params.update({data: {usernames: profiles.join(", ")}});
+      elmApp.ports.updateProfiles.subscribe(profiles => {
+        params.update({ data: { usernames: profiles.join(", ") } });
         $scope.sessions.fetch();
       });
-
 
       const onTimeRangeChanged = (timeFrom, timeTo) => {
         elmApp.ports.timeRangeSelected.send({ timeFrom, timeTo });
 
-        params.update({ data: { timeFrom, timeTo }});
+        params.update({ data: { timeFrom, timeTo } });
 
         sessions.fetch();
       };
 
-      FiltersUtils.setupTimeRangeFilter(onTimeRangeChanged,  params.get('data').timeFrom, params.get('data').timeTo);
+      FiltersUtils.setupTimeRangeFilter(
+        onTimeRangeChanged,
+        params.get("data").timeFrom,
+        params.get("data").timeTo
+      );
 
       elmApp.ports.refreshTimeRange.subscribe(() => {
-        FiltersUtils.setupTimeRangeFilter(onTimeRangeChanged, oneYearAgo , endOfToday);
+        FiltersUtils.setupTimeRangeFilter(
+          onTimeRangeChanged,
+          oneYearAgo,
+          endOfToday
+        );
 
         onTimeRangeChanged(oneYearAgo, endOfToday);
       });
 
       FiltersUtils.setupClipboard();
 
-      const tooltip = FiltersUtils.tooltipInstance()
+      const tooltip = FiltersUtils.tooltipInstance();
 
       elmApp.ports.showCopyLinkTooltip.subscribe(() => {
         const currentUrl = encodeURIComponent(window.location.href);
@@ -150,5 +176,5 @@ export const FixedSessionsMapCtrl = (
         FiltersUtils.fetchShortUrl(currentUrl, tooltip);
       });
     });
-  };
+  }
 };
