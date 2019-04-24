be4ee59dd91a5aea24ba0b27cd1960cf61cef098
Julia Jacobs <julia@lunarlogic.io>
2019-04-24 12:50:58 +0200

    chore: prettify webpack config files

--- config/webpack/loaders/elm.js
+++ config/webpack/loaders/elm.js
@@ -1,33 +1,33 @@
-const { resolve } = require('path')
+const { resolve } = require("path");
 
-const isProduction = process.env.NODE_ENV === 'production'
-const elmSource = resolve(process.cwd())
-const elmBinary = `${elmSource}/node_modules/.bin/elm`
+const isProduction = process.env.NODE_ENV === "production";
+const elmSource = resolve(process.cwd());
+const elmBinary = `${elmSource}/node_modules/.bin/elm`;
 
 const elmDefaultOptions = {
   cwd: elmSource,
   pathToElm: elmBinary,
-  files: [
-    resolve(__dirname, "../../../app/javascript/elm/src/Main.elm"),
-  ]
-}
+  files: [resolve(__dirname, "../../../app/javascript/elm/src/Main.elm")]
+};
 const developmentOptions = Object.assign({}, elmDefaultOptions, {
   verbose: true,
   // when running the app in debug mode decoding a long list crashes the app
   // that happens for example when receiving all measurements for a selected session
   debug: false
-})
+});
 const productionOptions = Object.assign({}, elmDefaultOptions, {
   optimize: true
-})
+});
 
 const elmWebpackLoader = {
-  loader: 'elm-webpack-loader',
+  loader: "elm-webpack-loader",
   options: isProduction ? productionOptions : developmentOptions
-}
+};
 
 module.exports = {
   test: /\.elm(\.erb)?$/,
   exclude: [/elm-stuff/, /node_modules/],
-  use: isProduction ? [elmWebpackLoader] : [{ loader: 'elm-hot-webpack-loader' }, elmWebpackLoader]
-}
+  use: isProduction
+    ? [elmWebpackLoader]
+    : [{ loader: "elm-hot-webpack-loader" }, elmWebpackLoader]
+};
