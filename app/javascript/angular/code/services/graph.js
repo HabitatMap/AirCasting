0f40e0f0b5c76dda97bbc10ffb7cc63eb8e784b0
3v0k4 <riccardo.odone@gmail.com>
2019-04-25 12:26:42 +0200

    refactor(graph): mobile vs fixed

--- app/javascript/angular/code/services/graph.js
+++ app/javascript/angular/code/services/graph.js
@@ -7,7 +7,7 @@ let measurementsByTime = {};
 let chart = null;
 const RENDER_TO_ID = "graph";
 
-export const getInitialData = (selectedSensor, heat, singleFixedSession) => {
+export const fetchAndDrawFixed = (selectedSensor, heat, singleFixedSession) => {
   var end_date = new Date(singleFixedSession.endTime()).getTime();
   var start_date = end_date - 24 * 60 * 60 * 1000;
 
@@ -18,90 +18,13 @@ export const getInitialData = (selectedSensor, heat, singleFixedSession) => {
       end_date
     })
     .then(xs => {
-      draw(
+      drawFixed(
         singleFixedSession.measurementsToTime(xs),
-        singleFixedSession.isFixed(),
         selectedSensor,
         heat,
         singleFixedSession
       );
     });
-};
-
-export const draw = (
-  data,
-  isFixed,
-  selectedSensor,
-  heat,
-  singleFixedSession
-) => {
-  var sensor = selectedSensor;
-  var low = heat.getValue("lowest");
-  var high = heat.getValue("highest");
-  var tick = Math.round((high - low) / 4);
-  var ticks = [low, low + tick, low + 2 * tick, high - tick, high];
-
-  var min1 = { count: 1, type: "minute", text: "1min" };
-  var min5 = { count: 5, type: "minute", text: "5min" };
-  var min30 = { count: 30, type: "minute", text: "30min" };
-  var hr1 = { count: 1, type: "hour", text: "1hr" };
-  var hrs12 = { count: 12, type: "hour", text: "12hrs" };
-  var hrs24 = { count: 24, type: "hour", text: "24hrs" };
-  var wk1 = { count: 1, type: "week", text: "1wk" };
-  var mth1 = { count: 1, type: "month", text: "1mth" };
-  var all = { type: "all", text: "All" };
-
-  const buttons = isFixed
-    ? [hr1, hrs12, hrs24, wk1, mth1, all]
-    : [min1, min5, min30, hr1, hrs12, all];
-  const selectedButton = isFixed ? 2 : 4;
-
-  const scrollbar = isFixed ? { liveRedraw: false } : {};
-
-  const xAxis = isFixed
-    ? {
-        events: {
-          afterSetExtremes: afterSetExtremes(singleFixedSession)
-        },
-        ordinal: false
-      }
-    : {};
-
-  const series = [
-    {
-      name: sensor.measurement_type,
-      data: _(data).values()
-    }
-  ];
-
-  const options = buildOptions({
-    renderTo: RENDER_TO_ID,
-    buttons,
-    selectedButton,
-    series,
-    xAxis,
-    low,
-    high,
-    ticks,
-    scrollbar,
-    measurementType: sensor.measurement_type,
-    unitSymbol: sensor.unit_symbol,
-    onMouseOverSingle,
-    onMouseOverMultiple
-  });
-
-  _(heat.toLevels()).each(function(level) {
-    options.yAxis.plotBands.push({
-      from: level.from,
-      to: level.to,
-      color: level.color
-    });
-  });
-  //TODO:
-  //to speed up graph provide data as array not object
-  chart ? chart.destroy() : null;
-  chart = new Highcharts.StockChart(options);
-  measurementsByTime = data;
 };
 
 const onMouseOverSingle = point => graphHighlight.show([point]);
@@ -146,4 +69,90 @@ const afterSetExtremes = singleFixedSession => e => {
       chart.redraw();
       chart.hideLoading();
     });
+};
+
+const min1 = { count: 1, type: "minute", text: "1min" };
+const min5 = { count: 5, type: "minute", text: "5min" };
+const min30 = { count: 30, type: "minute", text: "30min" };
+const hr1 = { count: 1, type: "hour", text: "1hr" };
+const hrs12 = { count: 12, type: "hour", text: "12hrs" };
+const hrs24 = { count: 24, type: "hour", text: "24hrs" };
+const wk1 = { count: 1, type: "week", text: "1wk" };
+const mth1 = { count: 1, type: "month", text: "1mth" };
+const all = { type: "all", text: "All" };
+
+const fixedButtons = [[hr1, hrs12, hrs24, wk1, mth1, all], 2];
+
+const mobileButtons = [[min1, min5, min30, hr1, hrs12, all], 4];
+
+export const drawMobile = (data, selectedSensor, heat) => {
+  const [buttons, selectedButton] = mobileButtons;
+  const scrollbar = {};
+  const xAxis = {};
+  draw(buttons, selectedButton, scrollbar, xAxis, data, selectedSensor, heat);
+};
+
+const drawFixed = (data, selectedSensor, heat, singleFixedSession) => {
+  const [buttons, selectedButton] = fixedButtons;
+  const scrollbar = { liveRedraw: false };
+
+  const xAxis = {
+    events: {
+      afterSetExtremes: afterSetExtremes(singleFixedSession)
+    },
+    ordinal: false
+  };
+
+  draw(buttons, selectedButton, scrollbar, xAxis, data, selectedSensor, heat);
+};
+
+const draw = (
+  buttons,
+  selectedButton,
+  scrollbar,
+  xAxis,
+  data,
+  selectedSensor,
+  heat
+) => {
+  const low = heat.getValue("lowest");
+  const high = heat.getValue("highest");
+  const tick = Math.round((high - low) / 4);
+  const ticks = [low, low + tick, low + 2 * tick, high - tick, high];
+
+  const series = [
+    {
+      name: selectedSensor.measurement_type,
+      data: _(data).values()
+    }
+  ];
+
+  const options = buildOptions({
+    renderTo: RENDER_TO_ID,
+    buttons,
+    selectedButton,
+    series,
+    xAxis,
+    low,
+    high,
+    ticks,
+    scrollbar,
+    measurementType: selectedSensor.measurement_type,
+    unitSymbol: selectedSensor.unit_symbol,
+    onMouseOverSingle,
+    onMouseOverMultiple
+  });
+
+  _(heat.toLevels()).each(function(level) {
+    options.yAxis.plotBands.push({
+      from: level.from,
+      to: level.to,
+      color: level.color
+    });
+  });
+  //TODO:
+  //to speed up graph provide data as array not object
+  chart ? chart.destroy() : null;
+  chart = new Highcharts.StockChart(options);
+  measurementsByTime = data;
 };
