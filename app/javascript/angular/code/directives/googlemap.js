65af2bc5527ae57c224c771f36b37a3243714702
Julia Jacobs <julia@lunarlogic.io>
2019-05-10 18:11:45 +0200

    ui: shrink controls
    removed "style: google.maps.MapTypeControlStyle.DROPDOWN_MENU" option
    because then the conrol was inconsistently bigger than the rest (only
    dropdown opions shrunk)

--- app/javascript/angular/code/directives/googlemap.js
+++ app/javascript/angular/code/directives/googlemap.js
@@ -4,30 +4,29 @@ angular.module("aircasting").directive("googlemap", function() {
   return {
     link: function(scope, element) {
       // United States
-      var point = {
+      let point = {
         lat: 37.09024,
         lng: -95.712891,
         zoom: 5
       };
 
-      var map = scope.map;
-      var params = scope.params.get("map") || {};
-      var lat = params.lat || map.getMapCookie("vp_lat") || point.lat;
-      var lng = params.lng || map.getMapCookie("vp_lng") || point.lng;
-      var latlng = new google.maps.LatLng(lat, lng);
-      var zoom = params.zoom || map.getMapCookie("vp_zoom") || point.zoom;
-      var mapType = params.mapType || google.maps.MapTypeId.roadmap;
+      let map = scope.map;
+      let params = scope.params.get("map") || {};
+      let lat = params.lat || map.getMapCookie("vp_lat") || point.lat;
+      let lng = params.lng || map.getMapCookie("vp_lng") || point.lng;
+      let latlng = new google.maps.LatLng(lat, lng);
+      let zoom = params.zoom || map.getMapCookie("vp_zoom") || point.zoom;
+      let mapType = params.mapType || google.maps.MapTypeId.roadmap;
       const minZoom = 3;
-      var options = {
-        zoom: parseInt(zoom, 10),
-        minZoom,
+      let options = {
         center: latlng,
-        styles: MapSettings.mapSettings(),
+        controlSize: 24,
+        fullscreenControl: false,
+        minZoom,
         mapTypeId: mapType,
         mapTypeControl: true,
         mapTypeControlOptions: {
           position: google.maps.ControlPosition.TOP_RIGHT,
-          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
           mapTypeIds: [
             google.maps.MapTypeId.ROADMAP,
             google.maps.MapTypeId.SATELLITE,
@@ -35,16 +34,17 @@ angular.module("aircasting").directive("googlemap", function() {
             google.maps.MapTypeId.HYBRID
           ]
         },
-        zoomControl: true,
-        zoomControlOptions: {
-          position: google.maps.ControlPosition.RIGHT_TOP
-        },
         panControl: false,
         streetViewControl: true,
         streetViewControlOptions: {
           position: google.maps.ControlPosition.RIGHT_TOP
         },
-        fullscreenControl: false
+        styles: MapSettings.mapSettings(),
+        zoom: parseInt(zoom, 10),
+        zoomControl: true,
+        zoomControlOptions: {
+          position: google.maps.ControlPosition.RIGHT_TOP
+        }
       };
       map.init(element[0], options);
     }
