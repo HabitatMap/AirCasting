bc8d81a8a879096aad6888e54e66388e12f8ecea
3v0k4 <riccardo.odone@gmail.com>
2019-05-15 14:02:10 +0200

    upgrade webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,9 @@ const elm = require("./loaders/elm");
 const erb = require("./loaders/erb");
 const webpack = require("webpack");
 
+// https://github.com/rails/webpacker/blob/master/docs/v4-upgrade.md#excluding-node_modules-from-being-transpiled-by-babel-loader
+environment.loaders.delete("nodeModules");
+
 environment.plugins.append(
   "nouislider",
   new webpack.ProvidePlugin({
@@ -10,6 +13,6 @@ environment.plugins.append(
   })
 );
 
-environment.loaders.append("elm", elm);
 environment.loaders.append("erb", erb);
+environment.loaders.prepend("elm", elm);
 module.exports = environment;
