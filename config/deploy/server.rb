4ad318510801d3863494cf344640ee83b7b439f7
3v0k4 <riccardo.odone@gmail.com>
2019-05-22 11:04:15 +0200

    ruby: upgrade to 2.4.6

--- config/deploy/server.rb
+++ config/deploy/server.rb
@@ -32,7 +32,7 @@ set :branch, ENV.fetch("BRANCH", "master")
 set :keep_releases, 3
 
 set :rbenv_type, :system
-set :rbenv_ruby, "2.2.10"
+set :rbenv_ruby, "2.4.6"
 #set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 #set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
@@ -41,7 +41,7 @@ set :default_env, {
     rbenv_root: '/usr/local/rbenv'
 }
 set :rbenv_roles, :all
-set :rbenv_ruby_dir, '/usr/local/rbenv/versions/2.2.10'
+set :rbenv_ruby_dir, '/usr/local/rbenv/versions/2.4.6'
 set :rbenv_custom_path, '/usr/local/rbenv'
 set :rbenv_path, '/usr/local/rbenv'
 
