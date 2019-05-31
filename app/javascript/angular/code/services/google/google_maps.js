e351b2aafd56ae154cdbd2d33b4aeb7d30fbb9e8
3v0k4 <riccardo.odone@gmail.com>
2019-05-31 16:11:44 +0200

    fix(sessions): fetch when page is reloaded

--- app/javascript/angular/code/services/google/google_maps.js
+++ app/javascript/angular/code/services/google/google_maps.js
@@ -44,6 +44,9 @@ angular.module("google").factory("googleMaps", [
       addListener: (obj, name, callback) =>
         google.maps.event.addListener(obj, name, callback),
 
+      addListenerOnce: (obj, name, callback) =>
+        google.maps.event.addListenerOnce(obj, name, callback),
+
       latLng: (lat, lng) => new google.maps.LatLng(lat, lng),
 
       latLngBounds: (lat, lng) => new google.maps.LatLngBounds(lat, lng),
