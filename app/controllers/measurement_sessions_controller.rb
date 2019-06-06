7cc31ea9435daf6fdbd79ef7ec7730416bfed8ec
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-06-06 14:31:12 +0200

    feat(session-link): allow users of mobile app to select a stream

--- app/controllers/measurement_sessions_controller.rb
+++ app/controllers/measurement_sessions_controller.rb
@@ -1,40 +1,42 @@
-# AirCasting - Share your Air!
-# Copyright (C) 2011-2012 HabitatMap, Inc.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# You can contact the authors by email at <info@habitatmap.org>
-
 class MeasurementSessionsController < ApplicationController
   layout 'map'
 
   def show
+    form = Api::ParamsForm.new(params: params, schema: Api::Links::Schema, struct: Api::Links::Struct)
+    result = Api::ToLink.new(form: form).call
+
+    if result.success?
+      redirect_to result.value
+    else
+      render json: result.errors, status: :bad_request
+    end
+  end
+
+  def show_old
+    # suporrts legacy mobile apps relesed before 06.2019
     session = Session.find_by_url_token(params[:url_token]) or raise NotFound
 
     selected_session_ids = [session.id]
-    sensorId = "Temperature-airbeam2-f (F)"
+    stream = session.streams.first!
+    sensorId = stream.sensor_id
+
+    data =
+    { sensorId: sensorId,
+      usernames: session.user.username,
+      heat: { highest: stream.threshold_very_high,
+              high: stream.threshold_high,
+              mid: stream.threshold_medium,
+              low: stream.threshold_low,
+              lowest: stream.threshold_very_low },
+    }
 
     if (session.type == "FixedSession")
-      data =
-        { sensorId: sensorId,
+      fix_data = data.merge(
           isIndoor: session.is_indoor,
-          isStreaming: (session.last_measurement_at > Time.current - 1.hour),
-          usernames: session.user.username
-        }
-      redirect_to fixed_map_path(:anchor => "?selectedSessionIds=#{selected_session_ids.to_json}&data=#{data.to_json}")
+          isStreaming: (session.last_measurement_at > Time.current - 1.hour)
+        )
+      redirect_to fixed_map_path(:anchor => "?selectedSessionIds=#{selected_session_ids.to_json}&data=#{fix_data.to_json}")
     else
-      data = { sensorId: sensorId, usernames: session.user.username, }
       redirect_to map_path(:anchor => "?selectedSessionIds=#{selected_session_ids.to_json}&data=#{data.to_json}")
     end
   end
--- spec/routing/session_route_spec.rb
+++ spec/routing/session_route_spec.rb
@@ -1,28 +1,10 @@
-# AirCasting - Share your Air!
-# Copyright (C) 2011-2012 HabitatMap, Inc.
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-# 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
-# You can contact the authors by email at <info@habitatmap.org>
-
 require 'spec_helper'
 
 describe 'routing to sessions' do
-  it 'it routes /s/:url_token to measurement_sessions#show for url_token' do
+  it 'it routes /s/:url_token to measurement_sessions#show_old for url_token' do
     expect({ :get => "/s/ab4c5" }).to route_to(
       :controller => "measurement_sessions",
-      :action => "show",
+      :action => "show_old",
       :url_token => "ab4c5"
     )
   end
