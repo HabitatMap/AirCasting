1b05b6a2706acf19ecfc6d14a5bc6a465f04dd9d
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-06-06 15:27:42 +0200

    refactor: replace conditionals with polimorphism

--- app/controllers/measurement_sessions_controller.rb
+++ app/controllers/measurement_sessions_controller.rb
@@ -13,31 +13,10 @@ class MeasurementSessionsController < ApplicationController
   end
 
   def show_old
-    # suporrts legacy mobile apps relesed before 06.2019
+    # supports legacy mobile apps relesed before 06.2019
     session = Session.find_by_url_token(params[:url_token]) or raise NotFound
-
-    selected_session_ids = [session.id]
     stream = session.streams.first!
-    sensorId = stream.sensor_id
 
-    data =
-    { sensorId: sensorId,
-      usernames: session.user.username,
-      heat: { highest: stream.threshold_very_high,
-              high: stream.threshold_high,
-              mid: stream.threshold_medium,
-              low: stream.threshold_low,
-              lowest: stream.threshold_very_low },
-    }
-
-    if (session.type == "FixedSession")
-      fix_data = data.merge(
-          isIndoor: session.is_indoor,
-          isStreaming: (session.last_measurement_at > Time.current - 1.hour)
-        )
-      redirect_to fixed_map_path(:anchor => "?selectedSessionIds=#{selected_session_ids.to_json}&data=#{fix_data.to_json}")
-    else
-      redirect_to map_path(:anchor => "?selectedSessionIds=#{selected_session_ids.to_json}&data=#{data.to_json}")
-    end
+    redirect_to session.generate_link(stream)
   end
 end
--- app/services/api/to_link.rb
+++ app/services/api/to_link.rb
@@ -1,5 +1,3 @@
-include Rails.application.routes.url_helpers
-
 class Api::ToLink
   def initialize(form:)
     @form = form
@@ -9,28 +7,9 @@ class Api::ToLink
     return Failure.new(form.errors) if form.invalid?
 
     session = ::Session.find_by_url_token(params[:url_token]) or raise NotFound
-    selected_session_ids = [session.id]
     stream = session.streams.where(sensor_name: params[:sensor_name]).first!
 
-    data =
-    { sensorId: stream.sensor_id,
-      usernames: session.user.username,
-      heat: { highest: stream.threshold_very_high,
-              high: stream.threshold_high,
-              mid: stream.threshold_medium,
-              low: stream.threshold_low,
-              lowest: stream.threshold_very_low },
-    }
-
-    if (session.type == "FixedSession")
-      fix_data = data.merge(
-          isIndoor: session.is_indoor,
-          isStreaming: (session.last_measurement_at > Time.current - 1.hour)
-        )
-      Success.new(fixed_map_path(:anchor => "?selectedSessionIds=#{selected_session_ids.to_json}&data=#{fix_data.to_json}"))
-    else
-      Success.new(map_path(:anchor => "?selectedSessionIds=#{selected_session_ids.to_json}&data=#{data.to_json}"))
-    end
+    Success.new(session.generate_link(stream))
   end
 
   private
