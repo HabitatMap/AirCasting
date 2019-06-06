566a8d68eb84cedac250422fd88f4603ce92822c
3v0k4 <riccardo.odone@gmail.com>
2019-06-06 09:19:23 +0200

    refactor(rails): before_filter -> before_action

--- app/controllers/api/regressions_controller.rb
+++ app/controllers/api/regressions_controller.rb
@@ -3,8 +3,8 @@ module Api
     respond_to :json
     # TokenAuthenticatable was removed from Devise in 3.1
     # https://gist.github.com/josevalim/fb706b1e933ef01e4fb6
-    before_filter :authenticate_user_from_token!, only: [:create, :destroy]
-    before_filter :authenticate_user!, only: [:create, :destroy]
+    before_action :authenticate_user_from_token!, only: [:create, :destroy]
+    before_action :authenticate_user!, only: [:create, :destroy]
 
     def create
       session = current_user.sessions.find_by_uuid!(params[:session_uuid])
