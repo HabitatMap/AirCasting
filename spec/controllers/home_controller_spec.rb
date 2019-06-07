c64ed5a184f7706a2a04842c6c8ef29a94ff2780
3v0k4 <riccardo.odone@gmail.com>
2019-06-07 17:01:43 +0200

    update(gem): rails 5.2.x

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -23,8 +23,7 @@ describe HomeController do
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
-
 end
