65dfb766a34b0f28d191ed9e4ae1b33550a810c9
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-06-20 17:23:13 +0200

    fix: save crowd map toggle state in the params

--- app/javascript/angular/code/controllers/_mobile_sessions_map_ctrl.js
+++ app/javascript/angular/code/controllers/_mobile_sessions_map_ctrl.js
@@ -102,6 +102,8 @@ export const MobileSessionsMapCtrl = (
 
       elmApp.ports.toggleCrowdMap.subscribe(crowdMap => {
         params.updateData({ crowdMap });
+        $scope.$apply();
+
         $scope.sessions.toggleCrowdMapView();
       });
 
