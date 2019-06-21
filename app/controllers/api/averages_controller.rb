d04effc243a70f8df7ea97200e78a353807f7482
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-06-21 14:18:38 +0200

    fix(crowd-map): extract duplication and add tests for averages_controller

--- app/controllers/api/averages_controller.rb
+++ app/controllers/api/averages_controller.rb
@@ -1,47 +1,21 @@
-# AirCasting - Share your Air!
-# Copyright (C) 2011-2012 HabitatMap, Inc.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# You can contact the authors by email at <info@habitatmap.org>
-
 require_dependency 'average_info'
 
 module Api
   class AveragesController < BaseController
     FLOAT_Q_ATTRS = %i[north south east west]
+    INT_Q_ATTRS = %i[
+      time_from
+      time_to
+      day_from
+      day_to
+      year_from
+      year_to
+      grid_size_x
+      grid_size_y
+    ]
 
     def index
-      int_q_attrs = %i[
-        time_from
-        time_to
-        day_from
-        day_to
-        year_from
-        year_to
-        grid_size_x
-        grid_size_y
-      ]
-
-      if params[:q].is_a?(String)
-        data =
-          ActiveSupport::JSON.decode(params.to_unsafe_hash[:q]).symbolize_keys
-      else
-        data = params.to_unsafe_hash[:q].symbolize_keys
-      end
-      FLOAT_Q_ATTRS.each { |key| data[key] = data[key].to_f if data.key?(key) }
-      int_q_attrs.each { |key| data[key] = data[key].to_i if data.key?(key) }
+      data = prepareData(params)
 
       data[:time_from] = data[:time_from] || 0
       data[:time_to] = data[:time_to] || 2_359
@@ -58,16 +32,7 @@ module Api
     end
 
     def index2
-      int_q_attrs = %i[time_from time_to grid_size_x grid_size_y]
-
-      if params[:q].is_a?(String)
-        data =
-          ActiveSupport::JSON.decode(params.to_unsafe_hash[:q]).symbolize_keys
-      else
-        data = params.to_unsafe_hash[:q].symbolize_keys
-      end
-      FLOAT_Q_ATTRS.each { |key| data[key] = data[key].to_f if data.key?(key) }
-      int_q_attrs.each { |key| data[key] = data[key].to_i if data.key?(key) }
+      data = prepareData(params)
 
       data[:time_from] = data[:time_from] || Time.new(2_010).to_i
       data[:time_to] = data[:time_to] || Time.new(2_100).end_of_year.to_i
@@ -75,6 +40,21 @@ module Api
       data[:session_ids] ||= []
 
       respond_with AverageInfo.new(data)
+    end
+
+    private
+
+    def prepareData(params)
+      if params[:q].is_a?(String)
+        data =
+          ActiveSupport::JSON.decode(params.to_unsafe_hash[:q]).symbolize_keys
+      else
+        data = params.to_unsafe_hash[:q].symbolize_keys
+      end
+      FLOAT_Q_ATTRS.each { |key| data[key] = data[key].to_f if data.key?(key) }
+      INT_Q_ATTRS.each { |key| data[key] = data[key].to_i if data.key?(key) }
+
+      data
     end
   end
 end
--- spec/controllers/averages_controller_spec.rb
+++ spec/controllers/averages_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Api::AveragesController do
-  it '#index adds defaults to missing params and returns result from AverageInfo#as_json' do
+  it '#index2 adds defaults to missing params and returns result from AverageInfo#as_json' do
     q = {
       north: '1.5',
       south: '2.5',
@@ -30,4 +30,39 @@ describe Api::AveragesController do
 
     expect(response.body).to eq(result.to_json)
   end
+
+  it '#index adds defaults to missing params and returns result from AverageInfo#as_json' do
+    q = {
+      north: '1.5',
+      south: '2.5',
+      west: '3.5',
+      east: '4.5',
+      grid_size_x: '5',
+      grid_size_y: '6'
+    }
+    expected = {
+      north: 1.5,
+      south: 2.5,
+      west: 3.5,
+      east: 4.5,
+      grid_size_x: 5,
+      grid_size_y: 6,
+      day_from: 0,
+      day_to: 365,
+      time_from: 0,
+      time_to: 2359,
+      year_from: 2010,
+      year_to: 2050,
+      session_ids: []
+    }
+    result = [{}, {}]
+    info = double
+    expect(AverageInfo).to receive(:new).with(expected).and_return(info)
+    expect(info).to receive(:as_json).and_return(result)
+
+    get :index, params: { q: q }, format: :json
+
+    expect(response.body).to eq(result.to_json)
+  end
+
 end
