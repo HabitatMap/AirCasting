c0c4e4175669b881f3f144468db405e0b07c4769
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-06-27 11:07:11 +0200

    refactor: use custom markers to draw selected session measurements

--- app/javascript/assets.js
+++ app/javascript/assets.js
@@ -1,28 +1,3 @@
-export const locationMarker1Path =
-  process.env.NODE_ENV === "test"
-    ? ""
-    : require("../assets/images/location_marker1.svg");
-
-export const locationMarker2Path =
-  process.env.NODE_ENV === "test"
-    ? ""
-    : require("../assets/images/location_marker2.svg");
-
-export const locationMarker3Path =
-  process.env.NODE_ENV === "test"
-    ? ""
-    : require("../assets/images/location_marker3.svg");
-
-export const locationMarker4Path =
-  process.env.NODE_ENV === "test"
-    ? ""
-    : require("../assets/images/location_marker4.svg");
-
-export const locationMarkerPath =
-  process.env.NODE_ENV === "test"
-    ? ""
-    : require("../assets/images/location_marker.svg");
-
 export const pulsingLocationMarkerPath =
   process.env.NODE_ENV === "test"
     ? ""
