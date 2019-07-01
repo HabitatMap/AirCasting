afac7f60f8dc0958648d2b702b6fcd3d29d82a8b
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-07-01 11:57:21 +0200

    feat(theme-switch): use theme colours for crowd map

--- app/javascript/angular/code/services/google/rectangles.js
+++ app/javascript/angular/code/services/google/rectangles.js
@@ -1,6 +1,15 @@
 angular.module("google").factory("rectangles", function() {
   var Rectangles = function() {
-    this.colors = [null, "#2DA641", "#F9DC2E", "#F57F22", "#F4001C"];
+    this.colors = [null, "#96d788", "#ffd960", "#fca443", "#e95f5f"];
+
+    window.__elmApp.ports.toggleTheme.subscribe(isCustomThemeOn => {
+      if (isCustomThemeOn) {
+        this.colors = [null, "#81dbcb", "#4ebcd5", "#2a70b8", "#19237e"];
+      } else {
+        this.colors = [null, "#96d788", "#ffd960", "#fca443", "#e95f5f"];
+      }
+      this.updateRectangles();
+    });
   };
   Rectangles.prototype = {
     init: function(googleMap) {
@@ -14,16 +23,16 @@ angular.module("google").factory("rectangles", function() {
       var lng = (region.east + region.west) / 2;
       return new google.maps.LatLng(lat, lng);
     },
-    draw: function(rectangles, thresholds) {
+    draw: function(rectangles) {
       var rectOptions, rectangle, color;
       var self = this;
       _(rectangles).each(function(data) {
-        color = self.getColor(thresholds, data.value);
+        color = self.getColor(heats(), data.value);
         if (color) {
           rectOptions = {
             strokeWeight: 0,
             fillColor: color,
-            fillOpacity: 0.35,
+            fillOpacity: 0.5,
             map: self.googleMap,
             bounds: new google.maps.LatLngBounds(
               new google.maps.LatLng(data.south, data.west),
@@ -46,8 +55,29 @@ angular.module("google").factory("rectangles", function() {
       });
 
       return this.colors[_(levels).indexOf(level)];
+    },
+    updateRectangles: function() {
+      window.__map.rectangles.forEach(rec => {
+        rec.setOptions({
+          fillColor: this.getColor(heats(), rec.data.value)
+        });
+      });
     }
   };
 
   return new Rectangles();
 });
+
+const heats = () => {
+  return Object.values(params().data.heat).sort();
+};
+
+const params = () =>
+  window.location.hash
+    .slice(2)
+    .split("&")
+    .filter(x => x.length !== 0)
+    .map(x => x.split("="))
+    .map(([k, v]) => [k, decodeURIComponent(v)])
+    .map(([k, v]) => [k, JSON.parse(v)])
+    .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {});
