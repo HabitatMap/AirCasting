c2485cc9853884b91a2176ed49c3e65b5934826d
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2019-07-08 12:28:12 +0200

    (mobile crowdmap): only use time_from and time_to params for mobile averages endpoint

--- app/controllers/api/averages_controller.rb
+++ app/controllers/api/averages_controller.rb
@@ -17,14 +17,8 @@ module Api
     def index
       data = prepareData(params)
 
-      data[:time_from] = data[:time_from] || 0
-      data[:time_to] = data[:time_to] || 2_359
-
-      data[:day_from] = data[:day_from] || 0
-      data[:day_to] = data[:day_to] || 365
-
-      data[:year_from] = data[:year_from] || 2_010
-      data[:year_to] = data[:year_to] || 2_050
+      data[:time_from] = data[:time_from] || Time.new(2_010).to_i
+      data[:time_to] = data[:time_to] || Time.new(2_100).end_of_year.to_i
 
       data[:session_ids] ||= []
 
--- spec/lib/average_info_spec.rb
+++ spec/lib/average_info_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require './lib/average_info'
 
 describe AverageInfo do
-  it 'calculates average for selected session ids' do
+  it 'returns average for selected session ids when session_ids provided' do
     user = create_user!(username: 'name')
     session1 = create_session!(id: 1, user: user)
     stream = create_stream!(session: session1)
@@ -29,28 +29,28 @@ describe AverageInfo do
 
     average_info = AverageInfo.new(data).as_json
 
-    expect(average_info.first[:value]).to eq(1.5)
+    expect(average_info.map { |average| average[:value] }).to eq([1.5])
   end
 
-  it 'filters by day_from, day_to, time_from, time_to, year_from, year_to when no session_ids provided' do
+  it 'returns average from requested time range when no session_ids provided' do
     user = create_user!(username: 'name')
     session1 = create_session!(id: 1, user: user)
     stream = create_stream!(session: session1)
     create_measurement!(
-      stream: stream, time: Time.new(2_010, 0o1, 0o1, 0o1), value: 1
+      stream: stream, time: Time.new(2_010, 1, 1, 1, 1), value: 1
     )
     session2 = create_session!(id: 2, user: user)
     stream2 = create_stream!(session: session2)
     create_measurement!(
-      stream: stream2, time: Time.new(2_010, 0o1, 0o3, 0o1), value: 2
+      stream: stream2, time: Time.new(2_010, 1, 3, 1, 1), value: 2
     )
     stream3 = create_stream!(session: session2)
     create_measurement!(
-      stream: stream3, time: Time.new(2_010, 0o1, 0o1, 0o3), value: 2
+      stream: stream3, time: Time.new(2_010, 1, 1, 1, 3), value: 2
     )
     stream4 = create_stream!(session: session2)
     create_measurement!(
-      stream: stream4, time: Time.new(2_011, 0o1, 0o3, 0o1), value: 3
+      stream: stream4, time: Time.new(2_011, 1, 3, 1, 1), value: 3
     )
 
     data = {
@@ -65,16 +65,12 @@ describe AverageInfo do
       east: 180,
       grid_size_x: 5,
       grid_size_y: 6,
-      day_from: 0,
-      day_to: 2,
-      time_from: 0,
-      time_to: 121,
-      year_from: 2_010,
-      year_to: 2_010
+      time_from: Time.new(2_010, 1, 1, 1, 1).to_i,
+      time_to: Time.new(2_010, 1, 1, 1, 2).to_i
     }
 
     average_info = AverageInfo.new(data).as_json
 
-    expect(average_info.first[:value]).to eq(1)
+    expect(average_info.map { |average| average[:value] }).to eq([1])
   end
 end
