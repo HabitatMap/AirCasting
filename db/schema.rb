58d7c7248a4f41de3f87ff9430883c50d435c7b2
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-07-16 14:34:25 +0200

    feat(email-alert): add session_stopped_alert column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_095427) do
+ActiveRecord::Schema.define(version: 2019_07_16_115122) do
 
   create_table "deleted_sessions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -139,10 +139,10 @@ ActiveRecord::Schema.define(version: 2019_07_16_095427) do
 
   create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "tag_id"
-    t.integer "taggable_id"
     t.string "taggable_type"
-    t.integer "tagger_id"
+    t.integer "taggable_id"
     t.string "tagger_type"
+    t.integer "tagger_id"
     t.string "context"
     t.datetime "created_at"
     t.index ["context"], name: "index_taggings_on_context"
@@ -179,6 +179,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_095427) do
     t.string "username"
     t.boolean "send_emails"
     t.boolean "admin", default: false
+    t.boolean "session_stopped_alert", default: false
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
