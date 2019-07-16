6064db75d50fc15b21fa3d11461b82b4aa38e213
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-07-16 11:57:32 +0200

    fix(schema): rename indexes to much the columns that have been renamed

---
+++ db/migrate/20190716095427_fix_indexes_naming.rb
@@ -0,0 +1,6 @@
+class FixIndexesNaming < ActiveRecord::Migration[5.2]
+  def change
+    rename_index :sessions, 'index_sessions_on_local_end_time', 'index_sessions_on_end_time_local'
+    rename_index :sessions, 'index_sessions_on_local_start_time', 'index_sessions_on_start_time_local'
+  end
+end
