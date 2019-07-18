1cb3170b83f93fee113576c6efc5fc14ccb3fd23
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-07-18 09:56:26 +0200

    feat(email-alert): add sidekiq scheduler gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
