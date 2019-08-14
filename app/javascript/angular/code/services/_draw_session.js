27461ac6b98c8eb276d1ed78952efc00e1b393ea
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-08-14 09:39:41 +0200

    Revert "refactor: use custom markers to draw selected session measurements"
    This reverts commit 362f0b3c64e876ba1b6860d7d11cc453f472a66c.

--- app/javascript/angular/code/services/_draw_session.js
+++ app/javascript/angular/code/services/_draw_session.js
@@ -3,6 +3,13 @@ import * as assets from "../../../assets";
 import { drawNotes } from "../../../javascript/note";
 import * as Session from "../../../javascript/values/session";
 
+const locationMarkersByLevel = {
+  1: assets.locationMarker1Path,
+  2: assets.locationMarker2Path,
+  3: assets.locationMarker3Path,
+  4: assets.locationMarker4Path
+};
+
 export const drawSession = (sensors, map, heat, empty) => {
   var DrawSession = function() {};
 
@@ -20,8 +27,14 @@ export const drawSession = (sensors, map, heat, empty) => {
       var suffix = " " + sensors.anySelected().unit_symbol;
       var points = [];
 
-      this.measurements(session).forEach(function(measurement) {
-        const marker = createMeasurementMarker(measurement, heat, map, suffix);
+      this.measurements(session).forEach(function(measurement, idx) {
+        const marker = createMeasurementMarker(
+          measurement,
+          idx,
+          heat,
+          map,
+          suffix
+        );
 
         points.push(measurement);
       });
@@ -57,21 +70,23 @@ export const drawSession = (sensors, map, heat, empty) => {
   return new DrawSession();
 };
 
-const calculateHeatLevel = (heat, value) => heat.levelName(value);
+const calculateHeatLevel = (heat, value) => heat.getLevel(value);
 
-const createMeasurementMarker = (measurement, heat, map, suffix) => {
+const createMeasurementMarker = (measurement, idx, heat, map, suffix) => {
   const roundedValue = Math.round(measurement.value);
   if (heat.outsideOfScope(roundedValue)) return;
 
   const level = calculateHeatLevel(heat, roundedValue);
-  const latLng = {
-    lat: () => measurement.latitude,
-    lng: () => measurement.longitude
-  };
 
-  const marker = map.drawMarkerWithoutLabel({
-    object: { latLng, title: roundedValue.toString() + suffix },
-    colorClass: level
+  const marker = map.drawMarker({
+    position: { lat: measurement.latitude, lng: measurement.longitude },
+    title: roundedValue.toString() + suffix,
+    zIndex: idx,
+    icon: {
+      anchor: new google.maps.Point(6, 6),
+      size: new google.maps.Size(12, 12),
+      url: locationMarkersByLevel[level]
+    }
   });
 
   return marker;
