97439b56ef13a9942365e8da836759fee4657d89
3v0k4 <riccardo.odone@gmail.com>
2019-12-12 10:56:06 +0100

    refactor(header): extract links

---
+++ app/javascript/elm/src/ExternalUrl.elm
@@ -0,0 +1,65 @@
+module ExternalUrl exposing (..)
+
+
+habitatMap =
+    "https://habitatmap.org"
+
+
+android =
+    "https://play.google.com/store/apps/details?id=pl.llp.aircasting&hl=en_US"
+
+
+
+-- top level
+
+
+airbeam =
+    habitatMap ++ "/airbeam"
+
+
+blog =
+    habitatMap ++ "/blog"
+
+
+search =
+    habitatMap ++ "/search"
+
+
+donate =
+    habitatMap ++ "/donate"
+
+
+about =
+    habitatMap ++ "/about"
+
+
+
+-- airbeam
+
+
+airbeamUserStories =
+    airbeam ++ "/user-stories"
+
+
+airbeamHowItWorks =
+    airbeam ++ "/how-it-works"
+
+
+airbeamFaq =
+    airbeam ++ "/FAQ"
+
+
+airbeamUsersGuide =
+    airbeam ++ "/users-guide"
+
+
+airbeamBuy =
+    airbeam ++ "/buy-it-now"
+
+
+
+-- about
+
+
+history =
+    about ++ "/history"
