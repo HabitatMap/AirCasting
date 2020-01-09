6a009c9b1223d70a58405e3c35cffba431e8767c
3v0k4 <riccardo.odone@gmail.com>
2020-01-09 12:10:37 +0100

    remove(ng): heat watch from heat service
    This seems to be unused.

--- app/javascript/angular/code/services/_heat.js
+++ app/javascript/angular/code/services/_heat.js
@@ -1,69 +1,11 @@
 export const heat = ($rootScope, params) => {
-  var Heat = function() {
-    var self = this;
-    this.namesBySensor = ["very_low", "low", "medium", "high", "very_high"];
-    this.heatPercentage = {};
-    this.scope = $rootScope.$new();
-    this.scope.params = params;
-    this.scope.$watch(
-      "params.get('data').heat",
-      function(newValue, oldValue) {
-        console.log("watch - params.get('data').heat");
-        if (!newValue || _(newValue).isEmpty()) {
-          return;
-        }
-        var value = newValue;
-        var scale = value.highest - value.lowest;
-        var percentageHeat = {
-          highest: Math.round(
-            ((1.0 * (value.highest - value.high)) / scale) * 100
-          ),
-          high: Math.round(((1.0 * (value.high - value.mid)) / scale) * 100),
-          mid: Math.round(((1.0 * (value.mid - value.low)) / scale) * 100)
-        };
-        percentageHeat.low =
-          100 -
-          percentageHeat.highest -
-          percentageHeat.high -
-          percentageHeat.mid;
-        _(["highest", "high", "mid", "low"]).each(function(heat) {
-          self.heatPercentage[heat] = { width: percentageHeat[heat] + "%" };
-        });
-      },
-      true
-    );
-  };
+  var Heat = function() {};
 
   Heat.prototype = {
-    get: function() {
-      return this.heatPercentage;
-    },
     getValue: function(name) {
-      return (this.getValues() || {})[name];
-    },
-    getValues: function() {
-      return params.get("data").heat;
-    },
-    parse: function(heat) {
-      var parsedHeat = _(heat).map(function(item) {
-        return _.str.toNumber(item);
-      });
-      return {
-        highest: parsedHeat[4],
-        high: parsedHeat[3],
-        mid: parsedHeat[2],
-        low: parsedHeat[1],
-        lowest: parsedHeat[0]
-      };
+      return (params.get("data").heat || {})[name];
     },
 
-    toSensoredList: function(sensor) {
-      var self = this;
-      if (!sensor) return [];
-      return _(this.namesBySensor).map(function(name) {
-        return _.str.toNumber(sensor["threshold_" + name]);
-      });
-    },
     getLevel: function(value) {
       if (value < this.getValue("lowest")) {
         return null;
@@ -95,11 +37,13 @@ export const heat = ($rootScope, params) => {
         return "default";
       }
     },
+
     outsideOfScope: function(value) {
       return (
         value < this.getValue("lowest") || value > this.getValue("highest")
       );
     },
+
     classByValue: function(value) {
       switch (this.getLevel(Math.round(value))) {
         case 1:
