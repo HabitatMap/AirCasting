51e7393245758d75d288b71c574c1cb23246ba6e
3v0k4 <riccardo.odone@gmail.com>
2020-01-21 16:36:57 +0100

    remove(ng): http interceptor

--- app/javascript/angular/code/aircasting.js
+++ app/javascript/angular/code/aircasting.js
@@ -1,11 +1,2 @@
 angular.module("google", []);
 angular.module("aircasting", ["google"]);
-
-angular.module("aircasting").config([
-  "$httpProvider",
-  function($httpProvider) {
-    $httpProvider.defaults.headers.common["X-Requested-With"] =
-      "XMLHttpRequest";
-    $httpProvider.interceptors.push("http_spinner_interceptor");
-  }
-]);
--- app/javascript/javascript/http.js
+++ app/javascript/javascript/http.js
@@ -1,12 +1,30 @@
+let counter = 0;
+
+const update = by => {
+  counter += by;
+  window.__elmApp.ports.updateIsHttping.send(counter !== 0);
+};
+
 export const get = (url, params) => {
   const query = Object.keys(params)
     .map(k => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
     .join("&");
 
-  return fetch(url + "?" + query).then(x => x.json());
+  return fetch(url + "?" + query).then(x => {
+    return x.json();
+  });
 };
 
-export const getQ = (url, params) =>
-  fetch(url + "?" + "q=" + encodeURIComponent(JSON.stringify(params))).then(x =>
-    x.json()
-  );
+export const getQ = (url, params) => {
+  update(+1);
+
+  return fetch(url + "?" + "q=" + encodeURIComponent(JSON.stringify(params)))
+    .then(x => {
+      update(-1);
+      return x.json();
+    })
+    .catch(() => {
+      update(-1);
+      throw new Error("failed");
+    });
+};
