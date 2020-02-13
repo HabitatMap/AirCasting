5a0a8470f919891426204f59cba8eab0d91f22b5
3v0k4 <riccardo.odone@gmail.com>
2020-02-13 15:31:59 +0100

    fix(openaq): toggle

--- app/workers/open_aq_import_measurements_worker.rb
+++ app/workers/open_aq_import_measurements_worker.rb
@@ -4,6 +4,8 @@ class OpenAqImportMeasurementsWorker
   include Sidekiq::Worker
 
   def perform
+    return unless A9n.sidekiq_open_aq_import_measurements_enabled
+
     OpenAq::ImportMeasurements.new.call
   end
 end
