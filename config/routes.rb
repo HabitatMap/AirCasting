c401cbdb8e1440964e16e27de1b4c7e7e6181fb4
Artur Trzop <artur.trzop@lunarlogic.io>
2020-05-12 13:32:50 +0200

    fix(typo): in routes comment

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,7 @@ Rails.application.routes.draw do
     resource :user, only: %i[show create] do
       resources :sessions, only: %i[show], controller: 'user_sessions' do
         collection do
-          post :sync # legacy API - supports mobile apps relesed before 07.2019
+          post :sync # legacy API - supports mobile apps released before 07.2019
           post :sync_with_versioning
           post :update_session
           post :delete_session
