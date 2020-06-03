c52875fd5567ee2cd556e3ef8bac2f8391a31fa9
Artur Trzop <artur.trzop@lunarlogic.io>
2020-06-03 14:01:25 +0200

    fix(deprecation notice for capistrano): use Capistrano::SCM::Git plugin
    [Deprecation Notice] `set :scm, :git` is deprecated.
    To ensure your project is compatible with future versions of Capistrano,
    remove the :scm setting and instead add these lines to your Capfile after
    `require "capistrano/deploy"`:
    
        require "capistrano/scm/git"
        install_plugin Capistrano::SCM::Git

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,6 @@ set :ssh_options, { forward_agent: true }
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Default value for :scm is :git
-set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
 
