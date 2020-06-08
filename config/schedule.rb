057f35a991a515b0440cf7e58d30724433fd893d
Artur Trzop <artur.trzop@lunarlogic.io>
2020-06-08 11:34:08 +0200

    Heaviest app usage is from the US and Western Europe so let's have the daily sidekiq restart at 6am UTC

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,10 +19,11 @@
 
 # Learn more: http://github.com/javan/whenever
 
-# 9:00 AM UTC is:
-# 5:00 AM in EST
-# 2:00 AM in PST
-every 1.day, at: '9:00 am', roles: %i[web] do
+# Heaviest app usage is from the US and Western Europe so let's have the daily sidekiq restart at:
+# 6:00 AM UTC is:
+# 2:00 AM in EST
+# 11:00 PM in PST
+every 1.day, at: '6:00 am', roles: %i[web] do
   # restart sidekiq every day to save RAM
   # because sidekiq can take over 7GB RAM when not restarted for long time
   command 'sudo systemctl restart sidekiq'
