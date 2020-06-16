86d9c21858456a4ecb986b16ab8b92d9ad7fc92b
Artur Trzop <artur.trzop@lunarlogic.io>
2020-06-16 15:08:52 +0200

    Create test_worker.rb

---
+++ app/workers/test_worker.rb
@@ -0,0 +1,14 @@
+class TestWorker
+  include Sidekiq::Worker
+
+  sidekiq_options lock: :until_executed, on_conflict: :log
+
+  def perform
+    #t = 60*10
+    t = 10
+    t.times do |i|
+      puts i
+      sleep 1
+    end
+  end
+end
