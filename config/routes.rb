f1ce46b7231fce8961afb4ce2db576b0355ff622
Artur Trzop <artur.trzop@lunarlogic.io>
2020-06-17 16:52:11 +0200

    add sidekiq unique jobs web tab in Sidekiq web UI
    https://github.com/mhenrixon/sidekiq-unique-jobs#sidekiq-web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
+require 'sidekiq_unique_jobs/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
