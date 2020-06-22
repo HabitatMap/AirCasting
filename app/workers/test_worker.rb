46e10e105f20eb86db72b022c18ce85cc8e01a44
Artur Trzop <artur.trzop@lunarlogic.io>
2020-06-22 14:59:15 +0200

    add example of tested strategy

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -1,11 +1,19 @@
 class TestWorker
   include Sidekiq::Worker
 
-  sidekiq_options lock: :until_executed, on_conflict: :log
+  #sidekiq_options lock: :until_executed,
+    #log_duplicate: true,
+    #on_conflict: :replace
+
+  # this won't help us
+  # when sidekiq killed then we can schedule unlimited jobs and they run in parallel ;(
+  sidekiq_options lock: :until_and_while_executing,
+    log_duplicate: true,
+    on_conflict: :log
 
   def perform
-    #t = 60*10
-    t = 10
+    t = 60*10
+    #t = 10
     t.times do |i|
       puts i
       sleep 1
