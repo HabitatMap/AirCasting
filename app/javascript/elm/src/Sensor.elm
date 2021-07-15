65cdcd56c93b492752bd8530e0ec97815b7f59b6
3v0k4 <riccardo.odone@gmail.com>
2021-07-15 14:24:49 +0200

    feat(sensors): change default sensors to ab3 for each parameter

--- app/javascript/elm/src/Sensor.elm
+++ app/javascript/elm/src/Sensor.elm
@@ -34,9 +34,9 @@ defaultSensorIdByParameter page =
     let
         common =
             Dict.fromList
-                [ ( "Humidity", "Humidity-airbeam2-rh (%)" )
+                [ ( "Humidity", "Humidity-airbeam3-rh (%)" )
                 , ( "Sound Level", "Sound Level-phone microphone (dB)" )
-                , ( "Temperature", "Temperature-airbeam2-f (F)" )
+                , ( "Temperature", "Temperature-airbeam3-f (F)" )
                 ]
     in
     case page of
