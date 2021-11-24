4ee63f5c67b9b768e20dbf3684eb972591549f9b
3v0k4 <riccardo.odone@gmail.com>
2021-11-24 13:51:02 +0100

    rails: use 5.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require_relative '../lib/aircasting/time_to_local_in_utc'
 module AirCasting
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
