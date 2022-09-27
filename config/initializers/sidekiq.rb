2b079aeed55094e8ab9544cdecc95403f37422c6
Julia Jacobs <julia@lunarlogic.io>
2022-09-27 16:27:00 +0200

    deps(sidekiq): Bump sidekiq-unique-jobs and sidekiq-scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,24 @@
+require 'sidekiq-unique-jobs'
+
 Sidekiq.configure_server do |config|
-  config.failures_max_count = 10000
+  config.redis = { url: ENV['REDIS_URL'] }
+  config.failures_max_count = 2000
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
