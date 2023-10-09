72ffa3e83347ad5e316d6adcb2c15f4b61fd8b20
franiakat <frania.kat@gmail.com>
2023-10-09 13:04:24 +0200

    Add Navigation

---
+++ app/javascript/react/const/urls.ts
@@ -0,0 +1,46 @@
+const habitatMap = "https://habitatmap.org";
+const android =
+  "https://play.google.com/store/apps/details?id=pl.llp.aircasting&hl=en_US";
+const iOS =
+  "https://apps.apple.com/us/app/aircasting/id1587685281#?platform=iphone";
+const actions = "https://aircastingactions.org/";
+
+// Top level
+const about = `${habitatMap}/about`;
+const airbeam = `${habitatMap}/airbeam`;
+const aircasting = `${habitatMap}/aircasting`;
+const map = "/mobile_map";
+const blog = `${habitatMap}/blog`;
+const donate = `${habitatMap}/donate`;
+const press = `${habitatMap}/about/press`;
+const search = `${habitatMap}/search`;
+
+// Airbeam
+const airbeamUserStories = `${airbeam}/user-stories`;
+const airbeamHowItWorks = `${airbeam}/how-it-works`;
+const airbeamFaq = `${airbeam}/FAQ`;
+const airbeamUsersGuide = `${airbeam}/users-guide`;
+const airbeamBuy = `${airbeam}/buy-it-now`;
+
+const history = `${about}/history`;
+
+export const urls = {
+  habitatMap,
+  android,
+  iOS,
+  actions,
+  about,
+  airbeam,
+  aircasting,
+  map,
+  blog,
+  donate,
+  press,
+  search,
+  airbeamUserStories,
+  airbeamHowItWorks,
+  airbeamFaq,
+  airbeamUsersGuide,
+  airbeamBuy,
+  history,
+};
