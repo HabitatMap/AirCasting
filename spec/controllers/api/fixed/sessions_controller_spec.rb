1567361fb42e00295840a7f442bed92ef0f91fa8
Marta Połeć <polec.marta@gmail.com>
2023-10-09 16:44:53 +0200

    Remove GET api/fixed/sessions2/:id
    Not used anymore, was used by already removed angular code.

--- spec/controllers/api/fixed/sessions_controller_spec.rb
+++ spec/controllers/api/fixed/sessions_controller_spec.rb
@@ -14,7 +14,7 @@ describe Api::Fixed::SessionsController do
           user: user,
           title: title,
           start_time_local: start_time_local,
-          end_time_local: end_time_local
+          end_time_local: end_time_local,
         )
       create_stream!(session: session, sensor_name: 'another-sensor-name')
       stream = create_stream!(session: session, sensor_name: sensor_name)
@@ -42,8 +42,8 @@ describe Api::Fixed::SessionsController do
             'latitude' => 1.0,
             'longitude' => 1.0,
             'time' => start_time_local.to_i * 1_000,
-            'value' => 123.0
-          }
+            'value' => 123.0,
+          },
         ],
         'minLatitude' => 1.0,
         'minLongitude' => 1.0,
@@ -74,10 +74,11 @@ describe Api::Fixed::SessionsController do
           user: user,
           title: title,
           start_time_local: start_time_local,
-          end_time_local: end_time_local
+          end_time_local: end_time_local,
         )
       stream = create_stream!(session: session, sensor_name: 'sensor-name')
-      other_stream = create_stream!(session: session, sensor_name: 'yet another-sensor-name')
+      other_stream =
+        create_stream!(session: session, sensor_name: 'yet another-sensor-name')
       create_measurement!(stream: stream, time: start_time_local)
       create_measurement!(stream: other_stream, time: start_time_local)
 
@@ -105,8 +106,8 @@ describe Api::Fixed::SessionsController do
                 'latitude' => 1.0,
                 'longitude' => 1.0,
                 'time' => start_time_local.to_i * 1_000,
-                'value' => 123.0
-              }
+                'value' => 123.0,
+              },
             ],
             'min_latitude' => 1.0,
             'min_longitude' => 1.0,
@@ -131,8 +132,8 @@ describe Api::Fixed::SessionsController do
                 'latitude' => 1.0,
                 'longitude' => 1.0,
                 'time' => start_time_local.to_i * 1_000,
-                'value' => 123.0
-              }
+                'value' => 123.0,
+              },
             ],
             'min_latitude' => 1.0,
             'min_longitude' => 1.0,
@@ -146,71 +147,7 @@ describe Api::Fixed::SessionsController do
             'measurement_short_type' => 'F',
             'measurement_type' => 'Temperature',
           },
-        ]
-      }
-      expect(json_response).to eq(expected)
-    end
-  end
-
-  describe '#show2' do
-    it 'returns session json including stream' do
-      sensor_name = 'sensor-name'
-      user = create_user!({ username: 'username' })
-      session = create_fixed_session!(user: user)
-      create_stream!(session: session, sensor_name: 'another-sensor-name')
-      stream = create_stream!(session: session, sensor_name: sensor_name)
-      create_measurements!({ stream: stream, value: 1 })
-
-      get :show2, params: { id: session.id, sensor_name: sensor_name }
-
-      expected = {
-        'title' => session.title,
-        'id' => session.id,
-        'contribute' => session.contribute,
-        'created_at' => format_time(session.created_at),
-        'data_type' => nil,
-        'end_time' => format_time(session.end_time),
-        'end_time_local' => format_time(session.end_time_local),
-        'instrument' => nil,
-        'is_indoor' => session.is_indoor,
-        'last_measurement_at' => nil,
-        'latitude' => session.latitude,
-        'longitude' => session.longitude,
-        'measurements_count' => nil,
-        'start_time' => format_time(session.start_time),
-        'start_time_local' => format_time(session.start_time_local),
-        'type' => 'FixedSession',
-        'updated_at' => format_time(session.updated_at),
-        'url_token' => session.url_token,
-        'user_id' => user.id,
-        'uuid' => session.uuid,
-        'last_measurement_value' => stream.average_value,
-        'streams' => {
-          stream.sensor_name => {
-            'average_value' => stream.average_value,
-            'id' => stream.id,
-            'max_latitude' => stream.max_latitude,
-            'max_longitude' => stream.max_longitude,
-            'measurement_short_type' => stream.measurement_short_type,
-            'measurement_type' => stream.measurement_type,
-            'measurements_count' => 1,
-            'min_latitude' => stream.min_latitude,
-            'min_longitude' => stream.min_longitude,
-            'sensor_name' => stream.sensor_name,
-            'sensor_package_name' => stream.sensor_package_name,
-            'session_id' => session.id,
-            'size' => 1,
-            'start_latitude' => stream.start_latitude,
-            'start_longitude' => stream.start_longitude,
-            'threshold_high' => stream.threshold_high,
-            'threshold_low' => stream.threshold_low,
-            'threshold_medium' => stream.threshold_medium,
-            'threshold_very_high' => stream.threshold_very_high,
-            'threshold_very_low' => stream.threshold_very_low,
-            'unit_name' => stream.unit_name,
-            'unit_symbol' => stream.unit_symbol
-          }
-        }
+        ],
       }
       expect(json_response).to eq(expected)
     end
@@ -229,7 +166,7 @@ describe Api::Fixed::SessionsController do
       end_time_local: attr.fetch(:end_time_local, DateTime.current),
       is_indoor: false,
       latitude: 123,
-      longitude: 123
+      longitude: 123,
     )
   end
 
