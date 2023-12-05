292260f9c06772941bfa53c78ae7563e8947a4b7
Monika Śmiałko <monika.smialko@gmail.com>
2023-12-05 14:25:15 +0100

    Make colors great again

--- app/javascript/react/components/Multislider/MultiSlider.tsx
+++ app/javascript/react/components/Multislider/MultiSlider.tsx
@@ -6,21 +6,26 @@ import { red, orange, yellow, green } from "../../assets/styles/colors";
 
 const MultiSlider = () => {
   const [thresholds, setThresholds] = useState([0, 50, 20, 40, 100]);
-
   const startThreshold = thresholds[0];
   const endThreshold = thresholds[thresholds.length - 1];
 
-  function getGradientColor(percentage: number) {
-    const startColor = [135, 208, 104];
-    const endColor = [255, 204, 199];
-
-    const midColor = startColor.map((start, i) => {
-      const end = endColor[i];
-      const delta = end - start;
-      return (start + delta * percentage).toFixed(0);
-    });
-    return `rgb(${midColor.join(",")})`;
+  function makeAscending(arr: number[]): number[] {
+    return arr.slice().sort((a, b) => a - b);
   }
+
+  function convertToPercentage(arr: number[]): number[] {
+    const max: number = Math.max(...arr);
+    const scaledArr: number[] = arr.map((value) => (value / max) * 100);
+    return scaledArr;
+  }
+  const percentagesThresholds = convertToPercentage(makeAscending(thresholds));
+
+  function convertToCssColors(arr: number[]): string {
+    const colors = `linear-gradient(to right,${green} ${arr[0]}% ${arr[1]}%, ${yellow} ${arr[1]}% ${arr[2]}%, ${orange} ${arr[2]}% ${arr[3]}%, ${red} ${arr[3]}% ${arr[4]}%) `;
+    console.log(colors);
+    return colors;
+  }
+  const cssColors = convertToCssColors(percentagesThresholds);
 
   return (
     <Slider
@@ -34,7 +39,7 @@ const MultiSlider = () => {
           background: "transparent",
         },
         tracks: {
-          background: `linear-gradient(to right, ${red} 0%, ${orange} 50%, ${yellow} 70%, ${green} 100%)`,
+          background: cssColors,
         },
       }}
     />
