4f54d10e1694d67a4d327d8ae483fee9c609d27c
Kasia Frania <frania.kat@gmail.com>
2023-12-11 09:21:37 +0100

    Add chart (#530)
    * Chart - static version introduced
    Co-authored-by: Monika Śmiałko <monika.smialko@lunarlogic.io>
    Co-authored-by: Janek <jankrzempek9@gmail.com>

--- app/javascript/react/assets/styles/colors.ts
+++ app/javascript/react/assets/styles/colors.ts
@@ -3,7 +3,26 @@ const theme = "#00b2ef";
 
 const white = "#fff";
 const darkGray = "#3E4449";
+const lightGray = "#C0C9D1";
 const mint = "#81dbcb";
 const blue = "#00b3ef";
+const red = "#ff464c";
+const orange = "#f99435";
+const yellow = "#fdd44f";
+const green = "#86d175";
+const dirtyWhite = "#ededed";
 
-export { cta, theme, white, darkGray, mint, blue };
+export {
+  cta,
+  theme,
+  white,
+  red,
+  orange,
+  yellow,
+  green,
+  darkGray,
+  lightGray,
+  mint,
+  blue,
+  dirtyWhite,
+};
