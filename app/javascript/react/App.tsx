80851de1310c2dede554fb65d43947f6893557c7
Janek <jankrzempek9@gmail.com>
2023-12-20 20:02:45 +0100

    WIP

--- app/javascript/react/App.tsx
+++ app/javascript/react/App.tsx
@@ -3,12 +3,14 @@ import './locales/i18n'
 
 import { CalendarPage } from "./pages/CalendarPage";
 import GlobalStyles from "./assets/styles/global-styles";
+import { CalendarStationHeader } from "./components/molecules/CalendarStationHeader/CalendarStationHeader";
 
 const App = () => {
   return (
     <>
-      <GlobalStyles />
-      <CalendarPage />
+      <CalendarStationHeader/>
+      {/* <GlobalStyles />
+      <CalendarPage /> */}
     </>
   );
 };
