634be30befb6b20f5ea6ed9d035d8602b42728f7
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-01-02 18:15:14 +0100

    rebase

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,17 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", id: :bigint, default: nil, force: :cascade do |t|
     t.string "name", limit: 255, null: false
     t.string "record_type", limit: 255, null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "public_active_storage_attachments_blob_id1_idx"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "public_active_storage_attachments_record_type0_idx", unique: true
+    t.index ["blob_id"], name: "active_storage_attachments_blob_id_idx"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "active_storage_attachmentse45ac-f35e-48f1-99ec-500f215fac03_idx", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", id: :bigint, default: nil, force: :cascade do |t|
     t.string "key", limit: 255, null: false
     t.string "filename", limit: 255, null: false
     t.string "content_type", limit: 255
@@ -34,35 +34,41 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.string "checksum", limit: 255, null: false
     t.datetime "created_at", null: false
     t.string "service_name", limit: 255, null: false
-    t.index ["key"], name: "public_active_storage_blobs_key0_idx", unique: true
+    t.index ["key"], name: "active_storage_blobs_key_idx", unique: true
   end
 
-  create_table "deleted_sessions", id: :serial, force: :cascade do |t|
+  create_table "active_storage_variant_records", id: :bigint, default: nil, force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", limit: 255, null: false
+    t.index ["blob_id", "variation_digest"], name: "active_storage_variant_records_blob_id_variation_digest_idx", unique: true
+  end
+
+  create_table "deleted_sessions", id: :integer, default: nil, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uuid", limit: 255
     t.integer "user_id"
-    t.index ["user_id"], name: "public_deleted_sessions_user_id1_idx"
-    t.index ["uuid", "user_id"], name: "public_deleted_sessions_uuid0_idx"
+    t.index ["user_id"], name: "deleted_sessions_user_id_idx"
+    t.index ["uuid", "user_id"], name: "deleted_sessions_uuid_user_id_idx"
   end
 
-  create_table "flipper_features", charset: "utf8mb3", force: :cascade do |t|
+  create_table "flipper_features", force: :cascade do |t|
     t.string "key", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["key"], name: "index_flipper_features_on_key", unique: true
   end
 
-  create_table "flipper_gates", charset: "utf8mb3", force: :cascade do |t|
+  create_table "flipper_gates", force: :cascade do |t|
     t.string "feature_key", null: false
     t.string "key", null: false
     t.text "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true, length: { value: 255 }
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
-  create_table "measurements", id: :integer, charset: "utf8mb3", force: :cascade do |t|
+  create_table "measurements", id: :bigint, default: nil, force: :cascade do |t|
     t.float "value"
     t.decimal "latitude", precision: 12, scale: 9
     t.decimal "longitude", precision: 12, scale: 9
@@ -71,10 +77,11 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.integer "stream_id"
     t.integer "milliseconds", default: 0
     t.float "measured_value"
-    t.index ["stream_id", "time"], name: "public_measurements_stream_id0_idx"
+    t.index ["stream_id", "time"], name: "measurements_stream_id_time_idx"
+    t.index ["stream_id"], name: "measurements_stream_id_idx"
   end
 
-  create_table "notes", id: :serial, force: :cascade do |t|
+  create_table "notes", id: :integer, default: nil, force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "date"
@@ -87,10 +94,10 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
     t.integer "number"
-    t.index ["session_id"], name: "public_notes_session_id0_idx"
+    t.index ["session_id"], name: "notes_session_id_idx"
   end
 
-  create_table "sessions", id: :serial, force: :cascade do |t|
+  create_table "sessions", id: :integer, default: nil, force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
@@ -101,20 +108,21 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.datetime "start_time_local"
     t.datetime "end_time_local"
     t.string "type", limit: 255, null: false
+    t.boolean "is_indoor"
     t.decimal "latitude", precision: 12, scale: 9
     t.decimal "longitude", precision: 12, scale: 9
     t.datetime "last_measurement_at"
     t.integer "version", default: 1
-    t.index ["contribute"], name: "index_sessions_on_contribute"
-    t.index ["end_time_local"], name: "index_sessions_on_end_time_local"
-    t.index ["last_measurement_at"], name: "index_sessions_on_last_measurement_at"
-    t.index ["start_time_local"], name: "index_sessions_on_start_time_local"
-    t.index ["url_token"], name: "index_sessions_on_url_token"
-    t.index ["user_id"], name: "index_sessions_on_user_id"
-    t.index ["uuid"], name: "index_sessions_on_uuid"
+    t.index ["contribute"], name: "sessions_contribute_idx"
+    t.index ["end_time_local"], name: "sessions_end_time_local_idx"
+    t.index ["last_measurement_at"], name: "sessions_last_measurement_at_idx"
+    t.index ["start_time_local"], name: "sessions_start_time_local_idx"
+    t.index ["url_token"], name: "sessions_url_token_idx"
+    t.index ["user_id"], name: "sessions_user_id_idx"
+    t.index ["uuid"], name: "sessions_uuid_idx"
   end
 
-  create_table "streams", id: :serial, force: :cascade do |t|
+  create_table "streams", id: :integer, default: nil, force: :cascade do |t|
     t.string "sensor_name", limit: 255
     t.string "unit_name", limit: 255
     t.string "measurement_type", limit: 255
@@ -135,16 +143,16 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.float "average_value"
     t.decimal "start_longitude", precision: 12, scale: 9
     t.decimal "start_latitude", precision: 12, scale: 9
-    t.index ["max_latitude"], name: "public_streams_max_latitude3_idx"
-    t.index ["max_longitude"], name: "public_streams_max_longitude5_idx"
-    t.index ["min_latitude"], name: "public_streams_min_latitude2_idx"
-    t.index ["min_longitude"], name: "public_streams_min_longitude4_idx"
-    t.index ["sensor_name", "measurement_type"], name: "public_streams_sensor_name1_idx"
-    t.index ["sensor_name"], name: "public_streams_sensor_name6_idx"
-    t.index ["session_id"], name: "public_streams_session_id0_idx"
+    t.index ["max_latitude"], name: "streams_max_latitude_idx"
+    t.index ["max_longitude"], name: "streams_max_longitude_idx"
+    t.index ["min_latitude"], name: "streams_min_latitude_idx"
+    t.index ["min_longitude"], name: "streams_min_longitude_idx"
+    t.index ["sensor_name", "measurement_type"], name: "streams_sensor_name_measurement_type_idx"
+    t.index ["sensor_name"], name: "streams_sensor_name_idx"
+    t.index ["session_id"], name: "streams_session_id_idx"
   end
 
-  create_table "taggings", id: :serial, force: :cascade do |t|
+  create_table "taggings", id: :integer, default: nil, force: :cascade do |t|
     t.integer "tag_id"
     t.integer "taggable_id"
     t.string "taggable_type", limit: 255
@@ -152,26 +160,24 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.string "tagger_type", limit: 255
     t.string "context", limit: 255
     t.datetime "created_at"
-    t.index ["context"], name: "public_taggings_context6_idx"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "public_taggings_tag_id0_idx", unique: true
-    t.index ["tag_id"], name: "public_taggings_tag_id2_idx"
-    t.index ["taggable_id", "taggable_type", "context"], name: "public_taggings_taggable_id1_idx"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "public_taggings_taggable_id8_idx"
-    t.index ["taggable_id"], name: "public_taggings_taggable_id3_idx"
-    t.index ["taggable_type"], name: "public_taggings_taggable_type4_idx"
-    t.index ["tagger_id", "tagger_type"], name: "public_taggings_tagger_id7_idx"
-    t.index ["tagger_id"], name: "public_taggings_tagger_id5_idx"
+    t.index ["context"], name: "taggings_context_idx"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_tag_id_taggable_i125f9-ec61-4655-b017-96802f28909c_idx", unique: true
+    t.index ["tag_id"], name: "taggings_tag_id_idx"
+    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_id_taggable_type_context_idx"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_taggable_id_taggable_type_tagger_id_context_idx"
+    t.index ["taggable_id"], name: "taggings_taggable_id_idx"
+    t.index ["taggable_type"], name: "taggings_taggable_type_idx"
+    t.index ["tagger_id", "tagger_type"], name: "taggings_tagger_id_tagger_type_idx"
+    t.index ["tagger_id"], name: "taggings_tagger_id_idx"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
+  create_table "tags", id: :integer, default: nil, force: :cascade do |t|
+    t.string "name", limit: 255
     t.integer "taggings_count", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_tags_on_name"
+    t.index ["name"], name: "tags_name_idx"
   end
 
-  create_table "threshold_alerts", force: :cascade do |t|
+  create_table "threshold_alerts", id: :bigint, default: nil, force: :cascade do |t|
     t.integer "user_id"
     t.string "session_uuid", limit: 255
     t.string "sensor_name", limit: 255
@@ -181,10 +187,10 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "timezone_offset", default: 0
-    t.index ["session_uuid", "sensor_name"], name: "public_threshold_alerts_session_uuid0_idx"
+    t.index ["session_uuid", "sensor_name"], name: "threshold_alerts_session_uuid_sensor_name_idx"
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
+  create_table "users", id: :integer, default: nil, force: :cascade do |t|
     t.string "email", limit: 255, default: "", null: false
     t.string "encrypted_password", limit: 128, default: "", null: false
     t.string "reset_password_token", limit: 255
@@ -202,10 +208,11 @@ ActiveRecord::Schema.define(version: 2023_12_19_084743) do
     t.boolean "send_emails"
     t.boolean "admin", default: false
     t.boolean "session_stopped_alert", default: false
-    t.index ["authentication_token"], name: "public_users_authentication_token2_idx", unique: true
-    t.index ["email"], name: "public_users_email0_idx", unique: true
-    t.index ["reset_password_token"], name: "public_users_reset_password_token1_idx", unique: true
+    t.index ["authentication_token"], name: "users_authentication_token_idx", unique: true
+    t.index ["email"], name: "users_email_idx", unique: true
+    t.index ["reset_password_token"], name: "users_reset_password_token_idx", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id", name: "active_storage_attachments_blob_id_fkey"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id", name: "active_storage_variant_records_blob_id_fkey"
 end
