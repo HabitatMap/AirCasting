c0e1c214f12813727ddd573400de5b2dcfe57172
Janek <jankrzempek9@gmail.com>
2024-01-02 15:58:20 +0100

    Diffrentiation between mobile and desktop, action button introduces.

--- app/javascript/react/components/molecules/StationValueLabel/StationValueLabel.style.tsx
+++ app/javascript/react/components/molecules/StationValueLabel/StationValueLabel.style.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { blue, mint } from "../../../assets/styles/colors";
 import hexToRGBA from "../../../utils/hexToRGB";
+import media from "../../../utils/media";
 
 const Container = styled.div`
   background: linear-gradient(
@@ -10,16 +11,28 @@ const Container = styled.div`
     ),
     ${mint};
   display: flex;
-  justify-content: space-between;
+  // justify-content: space-between;
   padding: 34px 20px;
   width: 290px;
   height: 180px;
   border-radius: 10px;
+
+  @media ${media.mobile} {
+    flex-direction: column;
+    width: 111px;
+    height: 274px;
+    border-radius: 0px;
+  }
 `;
 
 const ImageContainer = styled.img`
   height: 115px;
   width: 115px;
+
+  @media ${media.mobile} {
+    height: 85px;
+    width: 85px;
+  }
 `;
 
 const TextContainer = styled.div`
@@ -27,17 +40,32 @@ const TextContainer = styled.div`
   flex-direction: column;
   text-align: right;
   color: white;
+
+  @media ${media.mobile} {
+    text-align: left;
+    padding-top: 16px;
+  }
 `;
 
 const Label = styled.span`
   font-size: 16px;
   font-weight: 400;
+
+  @media ${media.mobile} {
+    font-size: 12px;
+    font-weight: 700;
+  }
 `;
 
 const Header = styled.span`
   font-size: 72px;
   font-weight: 600;
   padding-bottom: 5px;
+
+  @media ${media.mobile} {
+    font-size: 42px;
+    font-weight: 700;
+  }
 `;
 
 export { Container, ImageContainer, TextContainer, Label, Header };
