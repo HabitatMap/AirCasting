ce591b7093c368480d2a6e329edf695da8636ff8
Kasia Frania <frania.kat@gmail.com>
2024-01-10 12:04:20 +0100

    Display calendar page when feature flag enabled (#556)

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,6 +2,18 @@ class CalendarsController < ApplicationController
   include ApplicationHelper
 
   def index
-    render layout: 'calendar'
+    if Flipper.enabled?(:calendar)
+      render layout: 'calendar'
+    else
+      render_not_found
+    end
+  end
+
+  private
+
+  def render_not_found
+    render file: "#{Rails.root}/public/404.html",
+           layout: false,
+           status: :not_found
   end
 end
