f70a2eb0d13b552f0e4bc79d40e60e76ebaf90fe
Janek <jankrzempek9@gmail.com>
2024-01-12 13:28:28 +0100

    Seven bars with color adjusted.

--- app/javascript/react/assets/styles/colors.ts
+++ app/javascript/react/assets/styles/colors.ts
@@ -7,7 +7,10 @@ const grey200 = "#C0C9D1";
 const grey300 = "#7D858C";
 const grey400 = "#3E4449";
 const mint = "#81dbcb";
+const darkMint = "#4EBCD5";
 const blue = "#00b3ef";
+const cloudyBlue = "#2A70B8";
+const eveningBlue = "#19237E";
 const red = "#ff464c";
 const orange = "#f99435";
 const yellow = "#fdd44f";
@@ -27,6 +30,10 @@ export {
   yellow,
   green,
   mint,
+  darkMint,
   blue,
   dirtyWhite,
+  backgroundGray,
+  cloudyBlue,
+  eveningBlue
 };
--- app/javascript/react/components/DayView/DayView.tsx
+++ app/javascript/react/components/DayView/DayView.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import * as S from "./DayView.style";
 
-const DayView = () => {
-    return (
-      <S.RectangleContainer>
-        <S.BackgroundContainer />
-        <S.ColorfullRectangleContainer />
-      </S.RectangleContainer>
-    );
-  };
+interface ColorRanges {
+  bottom: number;
+  lower: number;
+  middle: number;
+  higher: number;
+  top: number;
+}
+
+interface DayViewProps {
+  value: number;
+  date: Date;
+  colorRanges: ColorRanges;
+}
+
+const DayView = ({ value, date, colorRanges }: DayViewProps) => {
+  return (
+    <S.RectangleContainer>
+      <S.Label>{value} (µg/m)</S.Label>
+      <S.BackgroundContainer />
+      <S.ColorfullRectangleContainer value={value} colorRanges={colorRanges} />
+      <S.BottomLabel>
+        {date.getDate()} {date.toLocaleString("default", { month: "short" })}
+      </S.BottomLabel>
+    </S.RectangleContainer>
+  );
+};
 
 export { DayView };
