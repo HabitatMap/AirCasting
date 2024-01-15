5ec468e08846e204789768e08fbe1a96d4fc3559
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-01-15 16:53:18 +0100

    tags logic fixed

--- app/services/api/to_fixed_tags.rb
+++ app/services/api/to_fixed_tags.rb
@@ -6,30 +6,11 @@ class Api::ToFixedTags
   def call
     return Failure.new(form.errors) if form.invalid?
 
-    sessions =
-      (data[:is_active] ? FixedSession.active : FixedSession.dormant)
-        .with_user_and_streams
-        .order('sessions.start_time_local DESC')
-        .where(contribute: true)
-        .where(is_indoor: data[:is_indoor])
-        .joins(:streams)
-        .where(streams: { sensor_name: data[:sensor_name] })
-        .where(streams: { unit_symbol: data[:unit_symbol] })
-        .where('streams.measurements_count > 0')
-        .merge(Stream.in_rectangle(data))
+    sessions = data[:is_active] ? FixedSession.active.filter_(data) : FixedSession.dormant.filter_(data)
 
-    sessions2 =
-      Session.filter_by_time_range(sessions, data[:time_from], data[:time_to])
+    tags = sessions.tag_counts.where(['tags.name LIKE ?', "#{data[:input]}%"])
 
-    usernames = AirCasting::UsernameParam.split(data[:usernames])
-    if usernames.present?
-      sessions2 = sessions2.joins(:user).where(users: { username: usernames })
-    end
-
-    # tag_counts is a method from the gem acts-as-taggable-on and the query inside it is not compatible with postgres
-    tags = sessions2.tag_counts.where(['tags.name LIKE ?', "#{data[:input]}%"])
-
-    Success.new(tags.map(&:name))
+    Success.new(tags.map(&:name).sort_by { |word| words_first(word) })
   end
 
   private
@@ -37,6 +18,10 @@ class Api::ToFixedTags
   attr_reader :form
 
   def data
-    form.to_h
+    form.to_h.to_h
+  end
+
+  def words_first(str)
+    str[0] =~ /[[:alpha:]]/ ? '0' + str.downcase : '1' + str.downcase
   end
 end
