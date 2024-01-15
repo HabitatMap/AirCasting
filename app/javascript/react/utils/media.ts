1898bcccaa0a7f21f432ea9facbc520a18a98809
Janek <jankrzempek9@gmail.com>
2024-01-15 12:09:58 +0100

    Following structured methods and rules + fixes.

--- app/javascript/react/utils/media.ts
+++ app/javascript/react/utils/media.ts
@@ -1,4 +1,7 @@
 const sizes = {
+  // Why such numbers? Explenation:
+  // While there is no hard and fast rule for what breakpoints should be used,
+  // values of common CSS breakpoints are 320px or 480px (for mobile phones), 768px (for tablets)
   mobile: "768px",
   desktop: "769px",
 };
