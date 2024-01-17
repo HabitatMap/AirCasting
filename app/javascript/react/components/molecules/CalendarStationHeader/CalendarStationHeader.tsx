53cfe7b82426d1f0688eab8c9f0689cc164f7e67
Janek <jankrzempek9@gmail.com>
2024-01-17 14:08:44 +0100

    Approach of designing UI switched after code review.

--- app/javascript/react/components/molecules/CalendarStationHeader/CalendarStationHeader.tsx
+++ app/javascript/react/components/molecules/CalendarStationHeader/CalendarStationHeader.tsx
@@ -9,8 +9,6 @@ import bellAlert from "../../../assets/icons/bellAlert.svg";
 import shareLink from "../../../assets/icons/shareLink.svg";
 import downloadImage from "../../../assets/icons/download.svg";
 import copyLink from "../../../assets/icons/copyLink.svg";
-import media from "../../../utils/media";
-import MediaQuery from "react-responsive";
 
 interface CalendarStation {
   stationName: string;
@@ -26,6 +24,96 @@ interface StreamData {
   parameter: string;
 }
 
+const CalendarHeaderName: React.FC<{ stationName: string }> = ({
+  stationName,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <S.HorizontalContainer>
+        <S.Description>{t("calendarHeader.stationPrefix")}</S.Description>
+        <S.Header>{stationName}</S.Header>
+      </S.HorizontalContainer>
+    </>
+  );
+};
+
+const CalendarStationProfileSensor: React.FC<{
+  profile: string;
+  sensor: string;
+}> = ({ profile, sensor }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <S.HorizontalContainer>
+        <S.MixContainer>
+          <S.DataDescriptionText>
+            {t("calendarHeader.profile")}
+          </S.DataDescriptionText>
+          <S.DataDescriptionValue>{profile}</S.DataDescriptionValue>
+        </S.MixContainer>
+
+        <S.MixContainer>
+          <S.DataDescriptionText>
+            {t("calendarHeader.sensor")}
+          </S.DataDescriptionText>
+          <S.DataDescriptionValue>{sensor}</S.DataDescriptionValue>
+        </S.MixContainer>
+      </S.HorizontalContainer>
+    </>
+  );
+};
+
+const CalendarUpdateOccurance: React.FC<{ lastUpdate: string }> = ({
+  lastUpdate,
+}) => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <S.HorizontalContainer>
+      <S.MixContainer>
+        <S.UpdateLabel>
+          {t("calendarHeader.updateFrequencyTitle")}
+        </S.UpdateLabel>
+        <S.UpdateFrequencyLabel>
+          {i18n.t("calendarHeader.updateFrequencyValue", { value: 15 })}
+        </S.UpdateFrequencyLabel>
+      </S.MixContainer>
+
+      <S.MixContainer>
+        <S.UpdateLabel>{t("calendarHeader.lastUpdate")}</S.UpdateLabel>
+        <S.UpdateDateLabel>{lastUpdate} (local time)</S.UpdateDateLabel>
+      </S.MixContainer>
+    </S.HorizontalContainer>
+  );
+};
+
+const CalendarActionButtons = () => {
+  return (
+    <>
+      <S.MobileButtons>
+        <ActionButton>
+          <img src={bellAlert} />
+        </ActionButton>
+        <ActionButton>
+          <img src={shareLink} />
+        </ActionButton>
+      </S.MobileButtons>
+
+      <S.DesktopButtons>
+        <Button onClick={() => {}}>
+          COPY LINK <img src={copyLink} />
+        </Button>
+        <Button onClick={() => {}}>
+          EXPORT SESSION <img src={downloadImage} />
+        </Button>
+      </S.DesktopButtons>
+    </>
+  );
+};
+
 const CalendarStationHeader = ({
   stationName,
   profile,
@@ -33,137 +121,25 @@ const CalendarStationHeader = ({
   lastUpdate,
   streamData,
 }: CalendarStation) => {
-  const { t, i18n } = useTranslation();
-
-  const stationNameAndHeader = () => {
-    return (
-      <>
-        <S.Description>{t("calendarHeader.stationPrefix")}</S.Description>
-        <S.Header>{stationName}</S.Header>
-      </>
-    );
-  };
-
-  const profileAndSensor = () => {
-    return (
-      <>
-        <S.HorizontalContainer>
-          <S.DataDescriptionText>
-            {t("calendarHeader.profile")}
-          </S.DataDescriptionText>
-          <S.DataDescriptionValue>{profile}</S.DataDescriptionValue>
-        </S.HorizontalContainer>
-
-        <S.HorizontalContainer>
-          <S.DataDescriptionText>
-            {t("calendarHeader.sensor")}
-          </S.DataDescriptionText>
-          <S.DataDescriptionValue>{sensor}</S.DataDescriptionValue>
-        </S.HorizontalContainer>
-      </>
-    );
-  };
-
-  const updateOccurance = () => {
-    return (
-      <>
-        <MediaQuery query={media.desktop}>
-          <S.HorizontalContainer>
-            <S.RowContainer>
-              <S.UpdateLabel>
-                {t("calendarHeader.updateFrequencyTitle")}
-              </S.UpdateLabel>
-              <S.UpdateLabel>{t("calendarHeader.lastUpdate")}</S.UpdateLabel>
-            </S.RowContainer>
-
-            <S.RowContainer>
-              <S.UpdateFrequencyLabel>
-                {i18n.t("calendarHeader.updateFrequencyValue", { value: 15 })}
-              </S.UpdateFrequencyLabel>
-              <S.UpdateDateLabel>{lastUpdate} (local time)</S.UpdateDateLabel>
-            </S.RowContainer>
-          </S.HorizontalContainer>
-        </MediaQuery>
-
-        <MediaQuery query={media.mobile}>
-            <S.HorizontalContainer>
-              <S.UpdateLabel>
-                {t("calendarHeader.updateFrequencyTitle")}
-              </S.UpdateLabel>
-              <S.UpdateFrequencyLabel>
-                {i18n.t("calendarHeader.updateFrequencyValue", { value: 15 })}
-              </S.UpdateFrequencyLabel>
-            </S.HorizontalContainer>
-
-            <S.HorizontalContainer>
-              <S.UpdateLabel>{t("calendarHeader.lastUpdate")}</S.UpdateLabel>
-              <S.UpdateDateLabel>{lastUpdate} (local time)</S.UpdateDateLabel>
-            </S.HorizontalContainer>
-        </MediaQuery>
-      </>
-    );
-  };
-
-  const actionableButtons = () => {
-    return (
-      <>
-        <MediaQuery query={media.mobile}>
-          <S.HorizontalContainer>
-            <ActionButton>
-              <img src={bellAlert} />
-            </ActionButton>
-            <ActionButton>
-              <img src={shareLink} />
-            </ActionButton>
-          </S.HorizontalContainer>
-        </MediaQuery>
-
-        <MediaQuery query={media.desktop}>
-          <S.HorizontalContainer>
-            <Button onClick={() => {}}>
-              COPY LINK <img src={copyLink} />
-            </Button>
-            <Button onClick={() => {}}>
-              EXPORT SESSION <img src={downloadImage} />
-            </Button>
-          </S.HorizontalContainer>
-        </MediaQuery>
-      </>
-    );
-  };
-
   return (
     <S.Container>
-      <S.ImageContainer>
-        <StationValueLabel
-          date={streamData.day}
-          value={streamData.value}
-          parameter={streamData.parameter}
-        />
-      </S.ImageContainer>
+      <StationValueLabel
+        date={streamData.day}
+        value={streamData.value}
+        parameter={streamData.parameter}
+      />
 
-      <MediaQuery query={media.mobile}>
-        <S.TextContainer>
-          {stationNameAndHeader()}
-          {profileAndSensor()}
-          {updateOccurance()}
-          {actionableButtons()}
-        </S.TextContainer>
-      </MediaQuery>
+      <S.DetailsContainer>
+        <S.ExtraContainer>
+          <CalendarHeaderName stationName={stationName} />
+          <CalendarStationProfileSensor profile={profile} sensor={sensor} />
+        </S.ExtraContainer>
 
-      <MediaQuery query={media.desktop}>
-        <S.HorizontalSpacingContainer>
-          <S.TextContainer>
-            {stationNameAndHeader()}
-            {updateOccurance()}
-          </S.TextContainer>
-
-          <S.TextContainer>
-            {profileAndSensor()}
-            {actionableButtons()}
-          </S.TextContainer>
-        </S.HorizontalSpacingContainer>
-      </MediaQuery>
+        <S.ExtraContainer>
+          <CalendarUpdateOccurance lastUpdate={lastUpdate} />
+          <CalendarActionButtons />
+        </S.ExtraContainer>
+      </S.DetailsContainer>
     </S.Container>
   );
 };
--- app/javascript/react/components/molecules/StationValueLabel/StationValueLabel.style.tsx
+++ app/javascript/react/components/molecules/StationValueLabel/StationValueLabel.style.tsx
@@ -11,59 +11,62 @@ const Container = styled.div`
     ),
     ${mint};
   display: flex;
-  padding: 34px 20px;
-  width: 290px;
-  height: 180px;
-  border-radius: 10px;
+  flex-direction: column;
+  align-self: stretch;
+  padding: 16px 12px;
+  width: 111px;
+  border-radius: 8px;
 
-  @media ${media.mobile} {
-    flex-direction: column;
-    width: 111px;
-    height: 274px;
-    border-radius: 8px;
+  @media ${media.desktop} {
+    align-self: auto;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 290px;
+    border-radius: 10px;
+    padding: 34px 20px;
   }
 `;
 
 const ImageContainer = styled.img`
-  height: 115px;
-  width: 115px;
+  height: 85px;
+  width: 85px;
 
-  @media ${media.mobile} {
-    height: 85px;
-    width: 85px;
+  @media ${media.desktop} {
+    height: 115px;
+    width: 115px;
   }
 `;
 
 const TextContainer = styled.div`
+  text-align: left;
+  padding-top: 16px;
   display: flex;
   flex-direction: column;
-  text-align: right;
   color: white;
 
-  @media ${media.mobile} {
-    text-align: left;
-    padding-top: 16px;
+  @media ${media.desktop} {
+    text-align: right;
   }
 `;
 
 const Label = styled.span`
-  font-size: 16px;
-  font-weight: 400;
+  font-size: 12px;
+  font-weight: 700;
 
-  @media ${media.mobile} {
-    font-size: 12px;
-    font-weight: 700;
+  @media ${media.desktop} {
+    font-size: 16px;
+    font-weight: 400;
   }
 `;
 
 const Header = styled.span`
-  font-size: 72px;
-  font-weight: 600;
-  padding-bottom: 5px;
+  font-size: 42px;
+  font-weight: 700;
 
-  @media ${media.mobile} {
-    font-size: 42px;
-    font-weight: 700;
+  @media ${media.desktop} {
+    font-size: 72px;
+    font-weight: 600;
+    padding-bottom: 5px;
   }
 `;
 
