54b7248a73fa2ea724830bd750cad445639a01a3
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-01-31 12:23:07 +0100

    location presence validation added to measurement model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -10,7 +10,7 @@ class Measurement < ApplicationRecord
   has_one :session, through: :stream
   has_one :user, through: :session
 
-  validates :stream_id, :value, :longitude, :latitude, :time, presence: true
+  validates :stream_id, :value, :longitude, :latitude, :location, :time, presence: true
   validate :time_not_in_the_future
 
 
