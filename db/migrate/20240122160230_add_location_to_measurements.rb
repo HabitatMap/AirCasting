d5979b33613e59575f4045ed9f9f475d347d1608
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-02-16 11:02:54 +0100

    migration reversed to geometry data type

--- db/migrate/20240122160230_add_location_to_measurements.rb
+++ db/migrate/20240122160230_add_location_to_measurements.rb
@@ -1,6 +1,6 @@
 class AddLocationToMeasurements < ActiveRecord::Migration[6.1]
   def up
-    add_column :measurements, :location, :point, geographic: true, srid: 4326
+    add_column :measurements, :location, :geometry, srid: 4326
   end
 
   def down
