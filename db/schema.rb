e0e5cf656e8d224128b51a657658a3cadea5a508
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-02-27 11:56:38 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_20_151309) do
+ActiveRecord::Schema.define(version: 2024_02_23_103934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,11 @@ ActiveRecord::Schema.define(version: 2024_02_20_151309) do
     t.datetime "photo_updated_at"
     t.integer "number"
     t.index ["session_id"], name: "index_notes_on_session_id"
+  end
+
+  create_table "session_timezones", force: :cascade do |t|
+    t.integer "session_id"
+    t.string "timezone_name"
   end
 
   create_table "sessions", id: :serial, force: :cascade do |t|
