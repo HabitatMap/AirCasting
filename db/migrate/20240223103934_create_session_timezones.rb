95db25b216913176c6ee0d7e4494fe4c5b68a4e1
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-03-14 15:35:59 +0100

    delete old migration

--- db/migrate/20240223103934_create_session_timezones.rb
+++
@@ -1,8 +0,0 @@
-class CreateSessionTimezones < ActiveRecord::Migration[6.1]
-  def change
-    create_table :session_timezones do |t|
-      t.integer :session_id
-      t.string :timezone_name
-    end
-  end
-end
