32ef4b7066fab90e8b0e4c84f897d0b237ba50bd
franiakat <frania.kat@gmail.com>
2024-03-22 12:01:46 +0100

    Apply code review changes

--- app/javascript/react/utils/getErrorMessage.ts
+++ app/javascript/react/utils/getErrorMessage.ts
@@ -3,16 +3,13 @@ type ErrorWithMessage = {
   message: string;
 };
 
-function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
-  return (
-    typeof error === "object" &&
-    error !== null &&
-    "message" in error &&
-    typeof (error as Record<string, unknown>).message === "string"
-  );
-}
+const isErrorWithMessage = (error: unknown): error is ErrorWithMessage =>
+  typeof error === "object" &&
+  error !== null &&
+  "message" in error &&
+  typeof (error as Record<string, unknown>).message === "string";
 
-function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
+const toErrorWithMessage = (maybeError: unknown): ErrorWithMessage => {
   if (isErrorWithMessage(maybeError)) return maybeError;
 
   try {
@@ -20,10 +17,10 @@ function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
   } catch {
     return new Error(String(maybeError));
   }
-}
+};
 
-function getErrorMessage(error: unknown) {
+const getErrorMessage = (error: unknown) => {
   return toErrorWithMessage(error).message;
-}
+};
 
 export { getErrorMessage };
