d22c794e09e1e0a9926242cb901bf45dbf6b5ec5
Marta Połeć <polec.marta@gmail.com>
2024-03-28 09:24:10 +0100

    Add specs

--- app/services/stream_daily_averages/fixed_active_sessions_traverser.rb
+++ app/services/stream_daily_averages/fixed_active_sessions_traverser.rb
@@ -2,29 +2,25 @@ module StreamDailyAverages
   class FixedActiveSessionsTraverser
     def initialize(
       fixed_sessions_repository: FixedSessionsRepository.new,
-      timezone_finder: TimezoneFinder.create,
       stream_interactor: StreamInteractor.new
     )
       @fixed_sessions_repository = fixed_sessions_repository
-      @timezone_finder = timezone_finder
       @stream_interactor = stream_interactor
     end
 
     def call
       fixed_sessions_repository.active_with_streams.each do |session|
-        time_zone =
-          timezone_finder.timezone_at(
-            lng: session.longitude,
-            lat: session.latitude,
-          )
         session.streams.map do |stream|
-          stream_interactor.call(stream_id: stream.id, time_zone: time_zone)
+          stream_interactor.call(
+            stream_id: stream.id,
+            time_zone: session.time_zone,
+          )
         end
       end
     end
 
     private
 
-    attr_reader :fixed_sessions_repository, :timezone_finder, :stream_interactor
+    attr_reader :fixed_sessions_repository, :stream_interactor
   end
 end
