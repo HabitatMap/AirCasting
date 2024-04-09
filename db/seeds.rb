54eab029c07fc7635ab185e16fc77b70b099f454
Ewelina Sarad <evelinasarad@gmail.com>
2024-04-09 12:05:46 +0200

    Change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ user =
   User.find_by(username: 'Jim Air', email: 'jim@test.com') ||
     User.create!(username: 'Jim Air', email: 'jim@test.com', password: '123456')
 
-session = Session.find_by(user: user, title: 'Krakow session 3')
+session = Session.find_by(user: user, title: 'Krakow session')
 
 unless session
   latitude = 50.04
@@ -10,21 +10,21 @@ unless session
   s =
     Session.create!(
       user: user,
-      title: 'Krakow session 2',
+      title: 'Krakow session',
       start_time_local: Time.current - 4.months,
       end_time_local: Time.current,
       last_measurement_at: Time.current,
       type: 'FixedSession',
       latitude: latitude,
       longitude: longitude,
-      uuid: '123456',
+      uuid: '123',
       is_indoor: false,
     )
 
   stream =
     Stream.create!(
       session: s,
-      sensor_name: 'AirBeam3-PM2.5',
+      sensor_name: 'PurpleAir-PM2.5',
       unit_name: 'microgram per cubic meter',
       measurement_type: 'Particulate Matter',
       measurement_short_type: 'PM',
@@ -34,7 +34,7 @@ unless session
       threshold_medium: 35,
       threshold_high: 55,
       threshold_very_high: 150,
-      sensor_package_name: 'AirBeam3-PM2.5',
+      sensor_package_name: 'PurpleAir-PM2.5',
       measurements_count: 20,
       average_value: 10.5,
     )
