4af3bdbdb98ffd422b42f9fee9b956358d6139cc
Ewelina Sarad <evelinasarad@gmail.com>
2024-04-10 10:36:10 +0200

    Remove new unused version of schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_22_101215) do
+ActiveRecord::Schema.define(version: 2024_03_13_144943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
