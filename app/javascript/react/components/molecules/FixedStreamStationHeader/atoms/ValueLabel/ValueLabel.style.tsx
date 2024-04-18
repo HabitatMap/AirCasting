8700de50c9f3e397a1156e3f5640e2d7e676ca0a
Monika Śmiałko <monika.smialko@gmail.com>
2024-04-18 14:52:35 +0200

    Gray-out station header for dormant sessions wip

--- app/javascript/react/components/molecules/FixedStreamStationHeader/atoms/ValueLabel/ValueLabel.style.tsx
+++ app/javascript/react/components/molecules/FixedStreamStationHeader/atoms/ValueLabel/ValueLabel.style.tsx
@@ -1,15 +1,30 @@
 import styled from "styled-components";
-import { blue, green } from "../../../../../assets/styles/colors";
+import {
+  blue,
+  green,
+  gray100,
+  gray400,
+} from "../../../../../assets/styles/colors";
 import hexToRGBA from "../../../../../utils/hexToRGB";
 import { media } from "../../../../../utils/media";
 
-const Container = styled.div`
-  background: linear-gradient(
+interface ContainerProps {
+  isActive?: boolean;
+}
+
+const Container = styled.div<ContainerProps>`
+  background: ${(props) =>
+    props.isActive
+      ? `linear-gradient(
       241deg,
-      ${hexToRGBA(blue, 0.4)} -2.4%,
-      ${hexToRGBA(blue, 0.0)} 94.94%
-    ),
-    ${green};
+      ${hexToRGBA(gray100, 0.4)} -2.4%,
+      ${hexToRGBA(gray100, 0.0)} 94.94%
+    ), ${gray400}`
+      : `linear-gradient(
+        241deg,
+        ${hexToRGBA(blue, 0.4)} -2.4%,
+        ${hexToRGBA(blue, 0.0)} 94.94%
+      ), ${green}`};
   display: flex;
   flex-direction: column;
   align-self: stretch;
