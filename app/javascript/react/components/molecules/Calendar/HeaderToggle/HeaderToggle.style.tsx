f848d0fe472f82735b25945bde817dcd91963ae3
Monika Śmiałko <monika.smialko@gmail.com>
2024-04-25 09:25:50 +0200

    UI fixes

--- app/javascript/react/components/molecules/Calendar/HeaderToggle/HeaderToggle.style.tsx
+++ app/javascript/react/components/molecules/Calendar/HeaderToggle/HeaderToggle.style.tsx
@@ -1,17 +1,20 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import { H3 } from "../../../Typography";
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  padding-bottom: 5px;
+  padding-bottom: 20px;
 `;
 
 export const RotatedIcon = styled.img<{ rotated: boolean }>`
-  margin-right: 8px;
+  margin-right: 10px;
   transform: ${({ rotated }) => (rotated ? "rotate(180deg)" : "none")};
   cursor: pointer;
 `;
 
-export const Heading = styled.h3`
+export const Heading = styled(H3)`
+  font-weight: 700;
+  font-size: 22px;
   cursor: pointer;
 `;
