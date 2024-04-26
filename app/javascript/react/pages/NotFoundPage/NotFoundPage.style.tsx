97f45828eddcae74e24f630fa64c11b456c666c2
Monika Śmiałko <monika.smialko@gmail.com>
2024-04-26 14:19:27 +0200

    Style page not found

---
+++ app/javascript/react/pages/NotFoundPage/NotFoundPage.style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+`;
+const NotFoundContent = styled.div`
+  text-align: center;
+`;
+const Title = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 20px;
+`;
+const Description = styled.p`
+  font-size: 1.2rem;
+`;
+export { NotFoundContainer, NotFoundContent, Title, Description };
