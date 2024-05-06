7a4f0ce13ee1c3621c71e0a8badca5a60d241ed4
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-06 13:48:35 +0200

    24h airnow pull test

--- app/services/air_now/import_data.rb
+++ app/services/air_now/import_data.rb
@@ -4,8 +4,20 @@ class AirNow::ImportData
   end
 
   def call
+    current_utc = DateTime.now.new_offset(0).beginning_of_hour - 1.hour
+
+    hourly_data = ""
+
+    (current_utc - 24.hours).step(current_utc, 1/24.0).each do |utc_time|
+      data = http_client.get(hourly_data_endpoint(utc_time))
+      measurements_to_import = substract_cached_data(data, utc_time)
+      hourly_data.concat(measurements_to_import)
+
+      measurements_to_import_count = measurements_to_import.split("\n").size
+      Sidekiq.logger.info "AirNow: Imported data for #{utc_time}, added #{measurements_to_import_count} measurements"
+    end
+
     locations_data = http_client.get(locations_endpoint)
-    hourly_data = http_client.get(hourly_data_endpoint)
 
     [locations_data, hourly_data]
   end
@@ -18,10 +30,22 @@ class AirNow::ImportData
     'https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/today/monitoring_site_locations.dat'
   end
 
-  def hourly_data_endpoint
-    current_utc = DateTime.now.new_offset(0) - 1.hour
-    formatted_date = current_utc.strftime('%Y%m%d')
-    formatted_hour = current_utc.strftime('%H')
+  def hourly_data_endpoint(utc_time)
+    formatted_date = utc_time.strftime('%Y%m%d')
+    formatted_hour = utc_time.strftime('%H')
     "https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/today/HourlyData_#{formatted_date}#{formatted_hour}.dat"
+  end
+
+  def substract_cached_data(data, utc_time)
+    cached_data = Rails.cache.read("air_now_#{utc_time.to_i}")
+    Rails.cache.write("air_now_#{utc_time.to_i}", data, expires_in: 25.hours)
+
+    if cached_data
+      cached_aqsids = cached_data.split("\n").map { |line| line.split('|')[2] }
+      new_data = data.split("\n").reject { |line| cached_aqsids.include?(line.split('|')[2]) }
+      data = new_data.join("\n").to_s
+    end
+
+    data
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,6 +12,11 @@ Sidekiq.configure_server do |config|
     chain.add SidekiqUniqueJobs::Middleware::Server
   end
 
+  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
+  logfile = File.open("#{Rails.root}/log/sidekiq.log", 'a')
+  logfile.sync = true
+  config.logger = ActiveSupport::Logger.new(logfile, level: Logger::INFO)
+
   SidekiqUniqueJobs::Server.configure(config)
 end
 
