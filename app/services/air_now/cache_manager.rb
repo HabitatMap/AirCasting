65c07f3239d3b6281922c05f4b3ee5741f0459d4
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-07 17:06:03 +0200

    refactor

--- app/services/air_now/cache_manager.rb
+++ app/services/air_now/cache_manager.rb
@@ -1,3 +1,6 @@
+# file format:
+# measurements date|time|aqsid|location|timezone|parameter|unit|value|attribution
+
 class AirNow::CacheManager
   def call(measurements_data)
     data_to_import = ""
@@ -8,12 +11,12 @@ class AirNow::CacheManager
       cache_current_aqsids(hourly_data, cache_key)
 
       if previously_cached_aqsids.present?
-        hourly_data = substract_already_saved_data(hourly_data, previously_cached_aqsids).join("\n").to_s
+        hourly_data = subtract_already_saved_data(hourly_data, previously_cached_aqsids).join("\n").to_s
       end
 
       data_to_import.concat(hourly_data)
 
-      # only for logging purposes - delete before merge
+      # logging into /home/aircasting/application/current/log/sidekiq.log
 
       measurements_count = hourly_data.split("\n").count
       Sidekiq.logger.info "AirNow: Imported data for #{cache_key}, added #{measurements_count} measurements."
@@ -24,23 +27,35 @@ class AirNow::CacheManager
     data_to_import
   end
 
+  private
+
   def create_cache_key(hourly_data)
-    first_line = hourly_data.split("\n").first
-    date = first_line.split('|')[0]
-    utc_time = first_line.split('|')[1]
+    date, utc_time = hourly_data.split("\n").first.split('|').values_at(0, 1)
     "air_now_#{date}_#{utc_time}"
   end
 
   def cache_current_aqsids(hourly_data, cache_key)
-    aqsids = hourly_data.split("\n").map { |line| line.split('|')[2] }
-    Rails.cache.write(cache_key, aqsids, expires_in: 25.hours)
+    aqsids = hourly_data_lines(hourly_data).map { |line| aqsid_from_line(line) }
+    Rails.cache.write(cache_key, aqsids, expires_in: cache_expiry_time)
+  end
+
+  def hourly_data_lines(hourly_data)
+    hourly_data.split("\n")
   end
 
   def cached_aqsids(cache_key)
     Rails.cache.read(cache_key)
   end
 
-  def substract_already_saved_data(hourly_data, saved_measurements_aqsids)
-    hourly_data.split("\n").reject { |line| saved_measurements_aqsids.include?(line.split('|')[2]) }
+  def subtract_already_saved_data(hourly_data, saved_measurements_aqsids)
+    hourly_data_lines(hourly_data).reject { |line| saved_measurements_aqsids.include?(aqsid_from_line(line)) }
+  end
+
+  def aqsid_from_line(line)
+    line.split('|')[2]
+  end
+
+  def cache_expiry_time
+    25.hours
   end
 end
