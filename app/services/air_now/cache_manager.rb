81fe72082c4a2d36f4299947ae70df085ac2bcf9
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-07 11:16:48 +0200

    refactor

---
+++ app/services/air_now/cache_manager.rb
@@ -0,0 +1,39 @@
+class AirNow::CacheManager
+  def call(measurements_data)
+    data_to_import = ""
+
+    measurements_data.each do |hourly_data|
+      cache_key = create_cache_key(hourly_data)
+      previously_cached_aqsids = cached_aqsids(cache_key)
+      cache_current_aqsids(hourly_data, cache_key)
+
+      if previously_cached_aqsids.present?
+        hourly_data = substract_already_saved_data(hourly_data, previously_cached_aqsids).join("\n").to_s
+      end
+
+      data_to_import.concat(hourly_data)
+    end
+
+    data_to_import
+  end
+
+  def create_cache_key(hourly_data)
+    first_line = hourly_data.split("\n").first
+    date = first_line.split('|')[0]
+    utc_time = first_line.split('|')[1]
+    "air_now_#{date}_#{utc_time}"
+  end
+
+  def cache_current_aqsids(hourly_data, cache_key)
+    aqsids = hourly_data.split("\n").map { |line| line.split('|')[2] }
+    Rails.cache.write(cache_key, aqsids, expires_in: 25.hours)
+  end
+
+  def cached_aqsids(cache_key)
+    Rails.cache.read(cache_key)
+  end
+
+  def substract_saved_data(hourly_data, saved_measurements_aqsids)
+    hourly_data.split("\n").reject { |line| saved_measurements_aqsids.include?(line.split('|')[2]) }
+  end
+end
--- app/services/air_now/import_data.rb
+++ app/services/air_now/import_data.rb
@@ -6,24 +6,10 @@ class AirNow::ImportData
   def call
     current_utc = DateTime.now.new_offset(0).beginning_of_hour - 1.hour
 
-    hourly_data = ""
+    hourly_data = []
 
     (current_utc - 24.hours).step(current_utc, 1/24.0).each do |utc_time|
-      data = http_client.get(hourly_data_endpoint(utc_time))
-      measurements_to_import = substract_cached_data(data, utc_time)
-      hourly_data.concat(measurements_to_import)
-
-      # only for logging, delete berofe merge
-
-      measurements_to_import_count = measurements_to_import.split("\n").size
-
-      measurements_with_wanted_sensors = measurements_to_import.split("\n").select do |line|
-        line.split('|')[5].in?(['PM2.5', 'O3', 'NO2', 'OZONE'])
-      end.size
-
-      Sidekiq.logger.info "AirNow: Imported data for #{utc_time}, added #{measurements_to_import_count} measurements, #{measurements_with_wanted_sensors} with wanted sensors."
-
-      # end of logging logic
+      hourly_data << http_client.get(hourly_data_endpoint(utc_time))
     end
 
     locations_data = http_client.get(locations_endpoint)
@@ -43,18 +29,5 @@ class AirNow::ImportData
     formatted_date = utc_time.strftime('%Y%m%d')
     formatted_hour = utc_time.strftime('%H')
     "https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/today/HourlyData_#{formatted_date}#{formatted_hour}.dat"
-  end
-
-  def substract_cached_data(data, utc_time)
-    cached_data = Rails.cache.read("air_now_#{utc_time.to_i}")
-    Rails.cache.write("air_now_#{utc_time.to_i}", data, expires_in: 25.hours)
-
-    if cached_data
-      cached_aqsids = cached_data.split("\n").map { |line| line.split('|')[2] }
-      new_data = data.split("\n").reject { |line| cached_aqsids.include?(line.split('|')[2]) }
-      data = new_data.join("\n").to_s
-    end
-
-    data
   end
 end
