34dfc903fd88dd1a59a20ad6cbc9c7d0fc1295b9
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-14 11:02:41 +0200

    defaul thresholds creating in rake task typo in migration/schema

---
+++ app/models/default_threshold.rb
@@ -0,0 +1,2 @@
+class DefaultThreshold < ApplicationRecord
+end
--- db/migrate/20240513144147_create_default_thresholds.rb
+++ db/migrate/20240513144147_create_default_thresholds.rb
@@ -1,7 +1,7 @@
 class CreateDefaultThresholds < ActiveRecord::Migration[6.1]
   def change
     create_table :default_thresholds do |t|
-      t.integer :stream_id
+      t.integer :sensor_name
       t.string :unit_symbol
       t.float :threshold_very_low
       t.float :threshold_low
