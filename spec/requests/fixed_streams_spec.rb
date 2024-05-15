7b799e03baf9e096d483a1d068e0d0b8cf5ea13e
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-15 17:26:31 +0200

    specs added, typo in migration

--- spec/requests/fixed_streams_spec.rb
+++ spec/requests/fixed_streams_spec.rb
@@ -127,6 +127,65 @@ describe 'GET api/v3/fixed_streams/:id' do
     end
   end
 
+  context 'calendar feature enabled, default thresholds assigned' do
+    it 'returns fixed stream data' do
+      session = create_fixed_session!
+      stream = create_stream!({ session: session })
+      measurement_1 = create_measurement!({ stream: stream })
+      measurement_2 = create_measurement!({ stream: stream })
+      default_thresholds = create_default_thresholds!({sensor_name: stream.sensor_name, unit_symbol: stream.unit_symbol})
+      stream_daily_average_1 =
+        create_stream_daily_average!(
+          { stream: stream, date: Date.current, value: 10 },
+        )
+      stream_daily_average_2 =
+        create_stream_daily_average!(
+          { stream: stream, date: Date.current.prev_day, value: 9 },
+        )
+
+      expected_response = {
+        stream: {
+          active: session.is_active,
+          title: session.title,
+          profile: session.username,
+          session_id: session.id,
+          sensor_name: stream.sensor_name,
+          unit_symbol: stream.unit_symbol,
+          update_frequency: '1 minute',
+          last_update: stream.session.last_measurement_at,
+        },
+        measurements: [
+          { time: measurement_1.time.to_i * 1_000, value: measurement_1.value },
+          { time: measurement_2.time.to_i * 1_000, value: measurement_2.value },
+        ],
+        stream_daily_averages: [
+          {
+            date: stream_daily_average_1.date.strftime('%Y-%m-%d'),
+            value: stream_daily_average_1.value.round,
+          },
+          {
+            date: stream_daily_average_2.date.strftime('%Y-%m-%d'),
+            value: stream_daily_average_2.value.round,
+          },
+        ],
+        thresholds: {
+          min: default_thresholds.threshold_very_low,
+          low: default_thresholds.threshold_low,
+          middle: default_thresholds.threshold_medium,
+          high: default_thresholds.threshold_high,
+          max: default_thresholds.threshold_very_high,
+        },
+      }
+
+      expect(Flipper).to receive(:enabled?).with(:calendar).and_return(true)
+
+      get "/api/v3/fixed_streams/#{stream.id}"
+
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to eq(expected_response.as_json)
+    end
+  end
+
   context 'calendar feature disabled' do
     it 'returns not_found status' do
       expect(Flipper).to receive(:enabled?).with(:calendar).and_return(false)
