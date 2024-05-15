7c3bc2ae087d4bfa8bcdedba800170695720c0e0
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-15 10:24:09 +0200

    TimeZoneBuilder switched to singleton class

--- app/services/time_zone_finder_wrapper.rb
+++ app/services/time_zone_finder_wrapper.rb
@@ -5,7 +5,12 @@ class TimeZoneFinderWrapper
     @time_zone_finder = TimezoneFinder.create
   end
 
-  def timezone_at(lat:, lng:)
+  def time_zone_at(lat:, lng:)
+    if lat.nil? || lng.nil? || lat.zero? || lng.zero? || lat > 90 ||
+         lat < -90 || lng > 180 || lng < -180
+      return 'UTC'
+    end
+
     @time_zone_finder.timezone_at(lat: lat, lng: lng)
   end
 end
