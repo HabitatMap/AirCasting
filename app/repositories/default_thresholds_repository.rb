41aec7d475482e591bd0496f190b6bbfa87e4105
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-16 16:41:30 +0200

    method name change

--- app/repositories/default_thresholds_repository.rb
+++ app/repositories/default_thresholds_repository.rb
@@ -1,5 +1,5 @@
 class DefaultThresholdsRepository
-  def stream_thresholds(stream:)
+  def find_for(stream:)
     default_thresholds = DefaultThreshold.find_by(sensor_name: stream.sensor_name, unit_symbol: stream.unit_symbol)
     thresholds = default_thresholds ? default_thresholds : stream
   end
