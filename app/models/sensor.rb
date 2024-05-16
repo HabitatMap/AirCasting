77864e751abeeb141cd842c009ae63219b93f613
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-16 16:52:31 +0200

    abm added to default ab sensors

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -12,8 +12,8 @@ class Sensor
   def self.sensor_name(sensor_name)
     normalized_names = {
       'airbeam-pm10' => ['airbeam2-pm10', 'airbeam3-pm10'],
-      'airbeam-pm2.5' => ['airbeam-pm', 'airbeam2-pm2.5', 'airbeam3-pm2.5'],
-      'airbeam-pm1' => ['airbeam2-pm1', 'airbeam3-pm1'],
+      'airbeam-pm2.5' => ['airbeam-pm', 'airbeam2-pm2.5', 'airbeam3-pm2.5', 'airbeammini-pm2.5'],
+      'airbeam-pm1' => ['airbeam2-pm1', 'airbeam3-pm1', 'airbeammini-pm1'],
       'airbeam-rh' => ['airbeam3-rh', 'airbeam2-rh', 'airbeam-rh'],
       'airbeam-f' => ['airbeam3-f', 'airbeam2-f', 'airbeam-f'],
     }
