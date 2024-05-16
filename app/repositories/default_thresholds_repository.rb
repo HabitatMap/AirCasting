78ccbc75d0a3404a00611306b84b3b690147bb48
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-16 16:25:54 +0200

    changes after CR

--- app/repositories/default_thresholds_repository.rb
+++ app/repositories/default_thresholds_repository.rb
@@ -1,5 +1,6 @@
 class DefaultThresholdsRepository
   def stream_thresholds(stream:)
-    DefaultThreshold.find_by(sensor_name: stream.sensor_name, unit_symbol: stream.unit_symbol)
+    default_thresholds = DefaultThreshold.find_by(sensor_name: stream.sensor_name, unit_symbol: stream.unit_symbol)
+    thresholds = default_thresholds ? default_thresholds : stream
   end
 end
--- app/serializers/fixed_stream_serializer.rb
+++ app/serializers/fixed_stream_serializer.rb
@@ -3,34 +3,29 @@ class FixedStreamSerializer
     @stream_daily_averages_serializer = StreamDailyAveragesSerializer.new
     @measurements_serializer = MeasurementsSerializer.new
     @stream_serializer = StreamSerializer.new
-    @threshold_serializer = ThresholdSerializer.new
   end
 
-  def call(stream:, measurements:, stream_daily_averages:, default_thresholds:)
-    thresholds = default_or_custom_thresholds(default_thresholds, stream)
-
+  def call(stream:, measurements:, stream_daily_averages:, thresholds:)
     {
       stream: stream_serializer.call(stream),
       measurements: measurements_serializer.call(measurements),
       stream_daily_averages:
         stream_daily_averages_serializer.call(stream_daily_averages),
-      thresholds: threshold_serializer.call(thresholds),
+      thresholds: serialize_thresholds(thresholds),
     }
   end
 
   private
 
-  attr_reader :stream_daily_averages_serializer, :measurements_serializer, :stream_serializer, :threshold_serializer
+  attr_reader :stream_daily_averages_serializer, :measurements_serializer, :stream_serializer
 
-  def default_or_custom_thresholds(default_thresholds, stream)
-    source = default_thresholds || stream
-
+  def serialize_thresholds(thresholds)
     {
-      threshold_very_low: source.threshold_very_low,
-      threshold_low: source.threshold_low,
-      threshold_medium: source.threshold_medium,
-      threshold_high: source.threshold_high,
-      threshold_very_high: source.threshold_very_high,
+      min: thresholds.threshold_very_low,
+      low: thresholds.threshold_low,
+      middle: thresholds.threshold_medium,
+      high: thresholds.threshold_high,
+      max: thresholds.threshold_very_high,
     }
   end
 end
