c1620ec833e2d298256981fc9120c7f3c00bbd3e
Ewelina Sarad <evelinasarad@gmail.com>
2024-05-19 23:08:21 +0200

    Clean code

--- app/javascript/react/pages/MapPage/MapPage.tsx
+++ app/javascript/react/pages/MapPage/MapPage.tsx
@@ -5,7 +5,6 @@ import { Map } from "../../components/Map";
 import { APIProvider } from "@vis.gl/react-google-maps";
 
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY || "";
-// AIzaSyBXx5HLegPOVhuklbuPCsADK3WE5hDCQdE
 
 const MapPage = () => {
   return (
