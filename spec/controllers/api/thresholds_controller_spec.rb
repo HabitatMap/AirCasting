8f18ec5bad6714455cb31e3bfc8b3d7830c7c8f1
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-05-27 12:54:25 +0200

    threshold values change to integers in /thresholds endpoint

--- spec/controllers/api/thresholds_controller_spec.rb
+++ spec/controllers/api/thresholds_controller_spec.rb
@@ -8,12 +8,12 @@ describe Api::ThresholdsController do
       get :show, params: { id: 'AirBeam2-PM2.5', unit_symbol: stream.unit_symbol }, format: :json
 
       expect(json_response).to eq([
-        stream.threshold_set.threshold_very_low,
-        stream.threshold_set.threshold_low,
-        stream.threshold_set.threshold_medium,
-        stream.threshold_set.threshold_high,
-        stream.threshold_set.threshold_very_high,
-      ].map(&:to_s))
+        stream.threshold_set.threshold_very_low.to_i,
+        stream.threshold_set.threshold_low.to_i,
+        stream.threshold_set.threshold_medium.to_i,
+        stream.threshold_set.threshold_high.to_i,
+        stream.threshold_set.threshold_very_high.to_i,
+    ].map(&:to_s))
     end
 
     it 'checks across all AirBeam versions' do
@@ -22,11 +22,11 @@ describe Api::ThresholdsController do
       get :show, params: { id: 'AirBeam-PM2.5', unit_symbol: stream.unit_symbol }, format: :json
 
       expect(json_response).to eq([
-        stream.threshold_set.threshold_very_low,
-        stream.threshold_set.threshold_low,
-        stream.threshold_set.threshold_medium,
-        stream.threshold_set.threshold_high,
-        stream.threshold_set.threshold_very_high,
+        stream.threshold_set.threshold_very_low.to_i,
+        stream.threshold_set.threshold_low.to_i,
+        stream.threshold_set.threshold_medium.to_i,
+        stream.threshold_set.threshold_high.to_i,
+        stream.threshold_set.threshold_very_high.to_i,
       ].map(&:to_s))
     end
   end
