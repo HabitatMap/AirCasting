ea2f5e81f5cf8ffdcb0f60796e3d7450ee3274e3
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-06-06 11:40:10 +0200

    update frequency changed for AirNow streams, specs adjusted (temporary solution)

--- app/serializers/fixed_stream_serializer.rb
+++ app/serializers/fixed_stream_serializer.rb
@@ -12,6 +12,7 @@ class FixedStreamSerializer
 
   def serialized_stream(stream)
     thresolds = Stream.thresholds(stream.sensor_name, stream.unit_symbol)
+    update_frequency = stream.session.user.username == 'US EPA AirNow' ? '1 hour' : '1 minute'
 
     {
       id: stream.id,
@@ -21,7 +22,7 @@ class FixedStreamSerializer
       profile: stream.session.username,
       sensor_name: stream.sensor_name,
       unit_symbol: stream.unit_symbol,
-      update_frequency: '1 minute',
+      update_frequency: update_frequency,
       last_update: stream.session.last_measurement_at,
       start_time: stream.session.start_time_local,
       end_time: stream.session.end_time_local,
