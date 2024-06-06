6570d1598a20dc31eaae77bf77e8b88e8f988e20
Janek <jankrzempek9@gmail.com>
2024-06-06 20:25:41 +0200

    Focus Tab controll process improved.

--- app/javascript/react/pages/MapPage/MapPage.style.tsx
+++ app/javascript/react/pages/MapPage/MapPage.style.tsx
@@ -1,18 +1,8 @@
 import styled from "styled-components";
 
-import { blue } from "../../assets/styles/colors";
-
-interface StyledMapContainerProps {
-  isTabbing: boolean;
-}
-
-const StyledMapContainer = styled.div<StyledMapContainerProps>`
-  width: 100%;
-  height: 100vh;
-
-  .gm-style iframe + div {
-    border: ${({ isTabbing }) =>
-      isTabbing ? `2px solid ${blue}` : 'none !important'};
+const StyledMapContainer = styled.div`
+  body:not(.user-is-tabbing) .gm-style iframe + div {
+    border: none !important;
   }
 `;
 
--- app/javascript/react/pages/MapPage/MapPage.tsx
+++ app/javascript/react/pages/MapPage/MapPage.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { APIProvider } from "@vis.gl/react-google-maps";
 
 import { Map } from "../../components/Map";
 import { StyledMapContainer } from "./MapPage.style";
-import { EventType } from "../../types/eventType";
+import { FocusTabController } from "../../utils/focusTabController";
 
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY || "";
 
@@ -13,35 +13,14 @@ interface MapPageProps {
 }
 
 const MapPage: React.FC<MapPageProps> = ({ children }) => {
-  const [isTabbing, setIsTabbing] = useState<boolean>(false);
-
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Tab") {
-        setIsTabbing(true);
-      }
-    };
-
-    const handleMouseDown = () => {
-      setIsTabbing(false);
-    };
-
-    window.addEventListener(EventType.keyDown, handleKeyDown);
-    window.addEventListener(EventType.mouseDown, handleMouseDown);
-
-    return () => {
-      window.removeEventListener(EventType.keyDown, handleKeyDown);
-      window.removeEventListener(EventType.mouseDown, handleMouseDown);
-    };
-  }, []);
-
   return (
     <APIProvider
       apiKey={googleMapsApiKey}
       onLoad={() => console.log("Maps API has loaded.")}
     >
       {children}
-      <StyledMapContainer isTabbing={isTabbing}>
+      <StyledMapContainer>
+        <FocusTabController />
         <Map />
       </StyledMapContainer>
     </APIProvider>
