24224d56ca89897bc02a7b6b1fd0825fb3c5ea19
Janek <jankrzempek9@gmail.com>
2024-06-11 16:07:17 +0200

    Removed empty, not used styles.

--- app/javascript/react/pages/MapPage/MapPage.tsx
+++ app/javascript/react/pages/MapPage/MapPage.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { APIProvider } from "@vis.gl/react-google-maps";
 
 import { Map } from "../../components/Map";
-import { StyledMapContainer } from "./MapPage.style";
 import { FocusTabController } from "../../utils/focusTabController";
 
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY || "";
