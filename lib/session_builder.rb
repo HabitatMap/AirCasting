05b619004d83b327cac360a9ef1521a3c44767d0
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-06-13 11:09:43 +0200

    saving threshold sets with sessions/streams logic added

--- lib/session_builder.rb
+++ lib/session_builder.rb
@@ -31,7 +31,7 @@ class SessionBuilder
         measurements = a_stream.delete(:measurements)
         next unless measurements.any?
         a_stream.merge!(session: session)
-        stream = Stream.create!(a_stream)
+        stream = Stream.build_with_threshold_set!(a_stream)
         jobs.push([stream, measurements])
       end
     end
