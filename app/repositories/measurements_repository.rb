51ac92d8ded99b2953163ce34a740533fe056cce
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-06-28 13:04:44 +0200

    Revert "1h averages calculated for all time periods, then grouped"
    This reverts commit 1b0fa619ae1b76cf73c2cf7362a6b2761bf074f3.

--- app/repositories/measurements_repository.rb
+++ app/repositories/measurements_repository.rb
@@ -14,13 +14,30 @@ class MeasurementsRepository
       .average(:value)
   end
 
-  def cluster_averages(stream_ids:, end_of_last_time_slice:, time_period:)
+  def cluster_averages(time_period:, stream_ids:, end_of_last_time_slice:)
+    interval =
+      case time_period
+      when 1 then '1 hour'
+      when 3 then '3 hours'
+      when 7 then '7 hours'
+      end
+
     ActiveRecord::Base.connection.execute(
     <<~SQL
-      SELECT date_trunc('hour', time_with_time_zone) AS hour, AVG(value) AS avg_value
-      FROM measurements WHERE stream_id in (#{stream_ids.join(',')})
-      AND time_with_time_zone >= '#{end_of_last_time_slice - time_period.days}'
-      GROUP BY hour ORDER BY hour;
+      WITH time_slices AS (
+        SELECT generate_series(
+          date_trunc('hour', '#{end_of_last_time_slice - time_period.days}'::timestamp),
+          '#{end_of_last_time_slice}'::timestamp - interval '#{interval}',
+          interval '#{interval}'
+        ) AS slice
+      )
+      SELECT slice, AVG(value) AS avg_value
+      FROM time_slices
+      LEFT JOIN measurements ON date_trunc('hour', measurements.time_with_time_zone) >= slice
+                              AND date_trunc('hour', measurements.time_with_time_zone) < slice + interval '#{interval}'
+      WHERE stream_id IN (#{stream_ids.join(',')})
+      GROUP BY slice
+      ORDER BY slice
     SQL
   )
   end
--- app/services/timelapse/clusters_traverser.rb
+++ app/services/timelapse/clusters_traverser.rb
@@ -12,28 +12,14 @@ module Timelapse
         cluster_id = cluster.keys.first
         stream_ids = cluster.values.first
 
-        averages =
-          measurements_repository.cluster_averages(
-            time_period: time_period,
-            stream_ids: stream_ids,
-            end_of_last_time_slice: end_of_last_time_slice
-          )
-
-        24.times do |time_slice_number|
-          time_slice_averages = averages.select do |row|
-            row['hour'] < end_of_last_time_slice - time_period.hours * time_slice_number &&
-              row['hour'] >= end_of_last_time_slice - time_period.hours * (time_slice_number + 1)
+        measurements_repository.cluster_averages(
+          time_period: time_period,
+          stream_ids: stream_ids,
+          end_of_last_time_slice: end_of_last_time_slice
+        ).each do |row|
+            end_of_current_time_slice = row['slice'] + time_period.hours
+            cluster_averages << { cluster_id => { time: end_of_current_time_slice, value: row['avg_value'] } }
           end
-
-          if time_slice_averages.empty?
-            cluster_averages << { cluster_id => { time: end_of_last_time_slice - time_period.hours * time_slice_number, value: nil } }
-            next
-          end
-
-          average = time_slice_averages.sum { |row| row['avg_value'] } / time_slice_averages.size
-
-          cluster_averages << { cluster_id => { time: end_of_last_time_slice - time_period.hours * time_slice_number, value: average } }
-        end
       end
 
       cluster_averages
