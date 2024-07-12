43891d7232c4f1b0b5cbf4a53c77d372dbc729df
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-07-12 14:02:21 +0200

    Uncomment changes

--- app/javascript/react/components/Modals/SessionDetailsModal/SessionInfo/ModalDesktopHeader.tsx
+++ app/javascript/react/components/Modals/SessionDetailsModal/SessionInfo/ModalDesktopHeader.tsx
@@ -97,8 +97,8 @@ const ModalDesktopHeader: React.FC<ModalDesktopHeaderProps> = ({
 
   useEffect(() => {
     if (showConfirmation) {
-      // const timer = setTimeout(() => setShowConfirmation(false), 3000);
-      // return () => clearTimeout(timer);
+      const timer = setTimeout(() => setShowConfirmation(false), 3000);
+      return () => clearTimeout(timer);
     }
   }, [showConfirmation]);
 
