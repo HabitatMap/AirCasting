e32d56547e94d9bfb8b7f71e59f24bc60edd6a04
Joanna Płatek <joanna.platek@lunarlogic.io>
2024-07-25 13:18:50 +0200

    feat: add initial filter values

--- app/javascript/react/components/SessionFilters/CrowdmapToggle.tsx
+++ app/javascript/react/components/SessionFilters/CrowdmapToggle.tsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import { useMapParams } from "../../utils/mapParamsHandler";
 import * as S from "./SessionFilters.style";
 
 const CrowdMapToggle = () => {
-  const { searchParams, sessionType } = useMapParams();
+  const {
+    boundEast,
+    boundNorth,
+    boundSouth,
+    boundWest,
+    initialMeasurementType,
+    initialUnitSymbol,
+  } = useMapParams();
+
+  const filters = useMemo(
+    () =>
+      JSON.stringify({
+        east: boundEast,
+        grid_size_x: 53,
+        grid_size_y: 47,
+        measurement_type: initialMeasurementType,
+        north: boundNorth,
+        sensor_name: "AirBeam-PM2.5",
+        south: boundSouth,
+        stream_ids: [2495370, 2495205, 2494971],
+        tags: "",
+        time_from: "1685318400",
+        time_to: "1717027199",
+        unit_symbol: initialUnitSymbol,
+        usernames: "",
+        west: boundWest,
+      }),
+    [
+      boundEast,
+      boundNorth,
+      boundSouth,
+      boundWest,
+      initialMeasurementType,
+      initialUnitSymbol,
+    ]
+  );
 
   const handleCrowdmap = () => {
-    console.log("test");
+    console.log(filters);
   };
 
   return (
