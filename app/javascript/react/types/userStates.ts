edc75c34f702d71accba4e2cbaeb9957da24ad5c
Joanna Płatek <joanna.platek@lunarlogic.io>
2024-07-30 10:09:20 +0200

    feat: add crowd map view

--- app/javascript/react/types/userStates.ts
+++ app/javascript/react/types/userStates.ts
@@ -1,8 +1,9 @@
 export enum UserSettings {
+  CalendarView = "CALENDAR_VIEW",
+  CrowdMapView = "CROWD_MAP_VIEW",
+  FiltersView = "FILTERS_VIEW",
+  MapLegendView = "MAP_LEGEND_VIEW",
   MapView = "MAP_VIEW",
   ModalView = "MODAL_VIEW",
-  CalendarView = "CALENDAR_VIEW",
   SessionListView = "SESSION_LIST_VIEW",
-  MapLegendView = "MAP_LEGEND_VIEW",
-  FiltersView = "FILTERS_VIEW",
 }
