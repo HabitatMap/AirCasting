1931e4decd69230abfba235b5a9f8b10fccc9ffa
Joanna Płatek <joanna.platek@lunarlogic.io>
2024-07-31 12:00:05 +0200

    fix: use correct arrow rotation for calendar (revert part of #728)

--- app/javascript/react/components/molecules/Calendar/HeaderToggle/HeaderToggle.tsx
+++ app/javascript/react/components/molecules/Calendar/HeaderToggle/HeaderToggle.tsx
@@ -34,7 +34,7 @@ const HeaderToggle = ({
           <S.RotatedIcon
             src={headerArrowIcon}
             alt={t("headerToggle.arrowIcon")}
-            $rotated={isVisible}
+            $rotated={!isVisible}
             onClick={toggleVisibility}
           />
           <S.Heading onClick={toggleVisibility}>{titleText}</S.Heading>
