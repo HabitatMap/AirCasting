637a0706bfd4385428c566615281b6de2d832037
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-08-06 11:00:02 +0200

    Fix dates

--- app/javascript/react/components/Graph/TimeRage.tsx
+++ app/javascript/react/components/Graph/TimeRage.tsx
@@ -14,28 +14,32 @@ interface TimeRangeProps {
 
 const TimeRange = forwardRef<HTMLDivElement, TimeRangeProps>(
   ({ minTime, maxTime }, ref) => {
+    const [isInitialized, setIsInitialized] = useState(false);
     const [isEmpty, setIsEmpty] = useState(true);
 
     useEffect(() => {
       if (ref && "current" in ref && ref.current) {
         setIsEmpty(ref.current.childElementCount === 0);
+        setIsInitialized(true);
       }
-    }, [ref]);
+    }, []);
 
-    return isEmpty ? (
-      <S.TimeRangeContainer>
-        <S.TimeContainer>
-          <S.Date>{minTime.date}</S.Date>
-          <S.Time>{minTime.time}</S.Time>
-        </S.TimeContainer>
-        -
-        <S.TimeContainer>
-          <S.Date>{maxTime.date}</S.Date>
-          <S.Time>{maxTime.time}</S.Time>
-        </S.TimeContainer>
+    return (
+      <S.TimeRangeContainer ref={ref}>
+        {isInitialized && isEmpty && (
+          <>
+            <S.TimeContainer>
+              <S.Date>{minTime.date}</S.Date>
+              <S.Time>{minTime.time}</S.Time>
+            </S.TimeContainer>
+            -
+            <S.TimeContainer>
+              <S.Date>{maxTime.date}</S.Date>
+              <S.Time>{maxTime.time}</S.Time>
+            </S.TimeContainer>
+          </>
+        )}
       </S.TimeRangeContainer>
-    ) : (
-      <S.TimeRangeContainer ref={ref} />
     );
   }
 );
