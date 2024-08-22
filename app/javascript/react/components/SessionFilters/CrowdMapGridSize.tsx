56020b4c35a7dc6431b0f4f45ec208a404f9381f
Joanna Płatek <joanna.platek@lunarlogic.io>
2024-08-22 16:58:34 +0200

    feat: initial grid buttons

--- app/javascript/react/components/SessionFilters/CrowdMapGridSize.tsx
+++ app/javascript/react/components/SessionFilters/CrowdMapGridSize.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 
-import { UserSettings } from "../../types/userStates";
-import { useMapParams } from "../../utils/mapParamsHandler";
 import * as S from "./SessionFilters.style";
 
 const CrowdMapGridSize = () => {
-  const { currentUserSettings, goToUserSettings } = useMapParams();
-
-  const handleCrowdMap = () => {
-    goToUserSettings(UserSettings.CrowdMapView);
-    if (currentUserSettings === UserSettings.CrowdMapView) {
-      goToUserSettings(UserSettings.MapView);
-    }
+  const handleGridSize = (gridSize: number) => {
+    console.log("gridSize", gridSize);
   };
 
   return (
     <S.SessionToggleWrapper>
-      {/* temporary solution, ticket: Session Filter [Mobile]: Crowdmap Toggle */}
-      <S.CrowdMapButton
-        onClick={handleCrowdMap}
-        disabled={[UserSettings.ModalView].includes(currentUserSettings)}
-      >
-        crowdmap
+      <S.CrowdMapButton onClick={() => handleGridSize(1)} disabled={false}>
+        1
+      </S.CrowdMapButton>
+      <S.CrowdMapButton onClick={() => handleGridSize(10)} disabled={false}>
+        10
+      </S.CrowdMapButton>
+      <S.CrowdMapButton onClick={() => handleGridSize(20)} disabled={false}>
+        20
+      </S.CrowdMapButton>
+      <S.CrowdMapButton onClick={() => handleGridSize(30)} disabled={false}>
+        30
+      </S.CrowdMapButton>
+      <S.CrowdMapButton onClick={() => handleGridSize(40)} disabled={false}>
+        40
       </S.CrowdMapButton>
     </S.SessionToggleWrapper>
   );
