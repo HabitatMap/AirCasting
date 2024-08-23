660ef75d221065d9bcac77bc65de86795fb56593
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-08-23 18:09:56 +0200

    new approach to clustering on backend - WIP

--- app/controllers/api/v3/fixed_stream_clusters_controller.rb
+++ app/controllers/api/v3/fixed_stream_clusters_controller.rb
@@ -14,61 +14,11 @@ module Api
       def index2
         start_time = Time.current
         sessions = FixedSession.active.filter_(data)
-        Rails.logger.info("FixedStreamClustersController#index2: Filtered sessions in #{Time.current - start_time} seconds")
 
         end_of_last_time_slice = Time.current.end_of_hour - 1.hour
         begining_of_first_time_slice = end_of_last_time_slice.beginning_of_hour - 168.hours
 
-        time_current = Time.current
-
-        streams = Stream.where(session_id: sessions.pluck('sessions.id'))
-        selected_sensor_streams = streams.select { |stream| Sensor.sensor_name(data[:sensor_name]).include? stream.sensor_name.downcase }
-
-        points = []
-
-        sessions.each do |session|
-          related_stream_id = selected_sensor_streams.find { |stream| stream.session_id == session.id }.id
-
-          points << {
-            latitude: session.latitude,
-            longitude: session.longitude,
-            stream_id: related_stream_id
-          }
-        end
-
-        Rails.logger.info("FixedStreamClustersController#index2: Created points in #{Time.current - time_current} seconds")
-
-        time_current = Time.current
-
-        clusters = ClusterPoints.new(points, 400).perform_clustering
-
-        Rails.logger.info("FixedStreamClustersController#index2: Performed clustering in #{Time.current - time_current} seconds")
-
-        result = {}
-
-        time_current = Time.current
-
-        clusters.each do |cluster|
-          averages =
-          MeasurementsRepository.new.streams_averages_from_period(
-            stream_ids: cluster.points.map { |point| point[:stream_id] },
-            start_date: begining_of_first_time_slice,
-            end_date: end_of_last_time_slice
-          )
-
-          averages.each do |average|
-            result[average[:time]] ||= []
-            result[average[:time]] <<
-              {
-                "value" => average[:value],
-                "latitude" => cluster.latitude,
-                "longitude" => cluster.longitude,
-                "sessions" => cluster.points.count
-              }
-          end
-        end
-
-        Rails.logger.info("FixedStreamClustersController#index2: Calculated averages in #{Time.current - time_current} seconds")
+        result = Timelapse::ClustersCreator.new.call(sessions: sessions, begining_of_first_time_slice: begining_of_first_time_slice, end_of_last_time_slice: end_of_last_time_slice, sensor_name: data[:sensor_name])
 
         render json: result, status: :ok
       end
