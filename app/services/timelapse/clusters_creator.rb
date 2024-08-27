4d9779f6e9bf6ec81a34347918d2a01e580b49eb
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-08-27 13:03:35 +0200

    refactor

--- app/services/timelapse/clusters_creator.rb
+++ app/services/timelapse/clusters_creator.rb
@@ -1,36 +1,30 @@
 module Timelapse
   class ClustersCreator
     def initialize
-      @measurements_repository = MeasurementsRepository.new
       @streams_repository = StreamsRepository.new
+      @cluster_processor = ClusterProcessor.new
     end
 
     def call(sessions:, sensor_name:, zoom_level:)
-      end_of_last_time_slice = Time.current.end_of_hour - 1.hour
-      begining_of_first_time_slice = end_of_last_time_slice.beginning_of_hour - 7.days
-
       streams = streams_repository.find_by_session_id(sessions.pluck(:id))
       selected_sensor_streams = streams.select { |stream| Sensor.sensor_name(sensor_name).include? stream.sensor_name.downcase }
+      streams_with_coordinates = streams_repository.streams_coordinates(selected_sensor_streams.pluck(:id))
 
-      clusters = cluster_measurements(selected_sensor_streams, zoom_level)
+      clusters = cluster_measurements(streams_with_coordinates, zoom_level)
       clusters = calculate_centroids_for_clusters(clusters)
-      process_clusters(clusters, begining_of_first_time_slice, end_of_last_time_slice)
+      cluster_processor.call(clusters: clusters)
     end
 
     private
 
-    attr_reader :measurements_repository, :streams_repository
+    attr_reader :streams_repository, :cluster_processor
 
-    def cluster_measurements(selected_sensor_streams, zoom_level)
-      stream_ids = selected_sensor_streams.pluck(:id)
-
+    def cluster_measurements(streams_with_coordinates, zoom_level)
       grid_cell_size = determine_grid_cell_size(zoom_level)
 
       grid = Hash.new { |hash, key| hash[key] = [] }
 
-      result = measurements_repository.streams_coordinates(stream_ids)
-
-      result.rows.each do |row|
+      streams_with_coordinates.rows.each do |row|
         stream_id, latitude, longitude = row
 
         cell_x = (longitude.to_f / grid_cell_size).floor
@@ -68,32 +62,6 @@ module Timelapse
           session_count: streams.size
         }
       end.compact
-    end
-
-    def process_clusters(clusters, begining_of_first_time_slice, end_of_last_time_slice)
-      result = {}
-
-      clusters.each do |cluster|
-        averages =
-          measurements_repository.streams_averages_from_period(
-            stream_ids: cluster[:stream_ids],
-            start_date: begining_of_first_time_slice,
-            end_date: end_of_last_time_slice
-          )
-
-        averages.each do |average|
-          result[average[:time]] ||= []
-          result[average[:time]] <<
-            {
-              "value" => average[:value],
-              "latitude" => cluster[:latitude],
-              "longitude" => cluster[:longitude],
-              "sessions" => cluster[:session_count]
-            }
-        end
-      end
-
-      result
     end
   end
 end
