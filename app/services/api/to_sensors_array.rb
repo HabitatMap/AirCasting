e03e5a68657d757cf9d77b260858787b7fb9e7f4
“BartkiewiczM” <228684@edu.p.lodz.pl>
2024-08-28 11:43:38 +0200

    sensors merged into groups excluded from the response

--- app/services/api/to_sensors_array.rb
+++ app/services/api/to_sensors_array.rb
@@ -30,10 +30,17 @@ class Api::ToSensorsArray
   end
 
   def sensors(session_type)
+    excluded_sensors = [
+      "AirBeam2-PM1", "AirBeam3-PM1", "AirBeamMini-PM1",
+      "AirBeam2-PM2.5", "AirBeam3-PM2.5", "AirBeamMini-PM2.5",
+      "AirBeam2-PM10", "AirBeam3-PM10", "AirBeamMini-PM10",
+    ]
+
     Stream
       .joins(:session)
       .where('sessions.contribute' => true)
       .where('sessions.type' => session_type)
+      .where.not(sensor_name: excluded_sensors)
       .select(
         :sensor_name,
         :measurement_type,
