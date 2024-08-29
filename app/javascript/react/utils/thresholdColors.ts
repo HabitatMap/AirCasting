bf070280b630573ecd601895cc37ba5577c96a84
Sonje <soniakolacz@gmail.com>
2024-08-29 15:32:39 +0200

    calendar card green gradient tweak

--- app/javascript/react/utils/thresholdColors.ts
+++ app/javascript/react/utils/thresholdColors.ts
@@ -31,7 +31,7 @@ const getColorForValue = (
 const getColorForCalendarDataCard = (color: string) => {
   switch (color) {
     case colors.green:
-      return `linear-gradient(241deg, ${hexToRGBA(colors.blue, 0.48)} -2.4%, ${hexToRGBA(colors.blue, 0.15)} 94.94%), ${colors.green}`;
+      return `linear-gradient(241deg, ${hexToRGBA(colors.blue, 0.4)} -32.4%, ${hexToRGBA(colors.blue, 0.05)} 94.94%), ${colors.green}`;
     case colors.red:
       return `linear-gradient(241deg, ${hexToRGBA(colors.royalPurple, 0.4)} -38.43%, ${hexToRGBA(colors.royalPurple, 0)} 94.94%), ${colors.red}`;
     case colors.orange:
