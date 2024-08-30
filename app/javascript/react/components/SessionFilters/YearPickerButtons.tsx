f161b19188592db40d002ad80032ce12973dc216
Joanna Płatek <joanna.platek@lunarlogic.io>
2024-08-30 11:05:37 +0200

    feat(filters): display active year button

--- app/javascript/react/components/SessionFilters/YearPickerButtons.tsx
+++ app/javascript/react/components/SessionFilters/YearPickerButtons.tsx
@@ -5,14 +5,14 @@ import { setFetchingData } from "../../store/mapSlice";
 import { UrlParamsTypes, useMapParams } from "../../utils/mapParamsHandler";
 import * as S from "./SessionFilters.style";
 
-const years = ["2024", "2023", "2022", "2021", "2020"];
+const years = [2024, 2023, 2022, 2021, 2020];
 
 const YearPickerButtons = () => {
   const dispatch = useAppDispatch();
   const { setUrlParams, timeFrom, timeTo } = useMapParams();
 
   const handleYear = useCallback(
-    (year: string) => {
+    (year: number) => {
       const beginning = new Date(`${year}-01-01T00:00:00Z`).getTime() / 1000;
       const end = new Date(`${year}-12-31T23:59:59Z`).getTime() / 1000;
 
@@ -33,13 +33,18 @@ const YearPickerButtons = () => {
     [dispatch, setUrlParams, timeFrom, timeTo]
   );
 
+  const timestampToYear = (timestamp: string): number => {
+    const date = new Date(Number(timestamp) * 1000);
+    return date.getUTCFullYear();
+  };
+
   return (
     <S.CrowdMapGridButtonsContainer>
       {years.map((year) => (
         <S.CrowdGridSizeButton
           key={year}
           onClick={() => handleYear(year)}
-          $isActive={timeFrom === year}
+          $isActive={timestampToYear(timeFrom) === year}
         >
           {year}
         </S.CrowdGridSizeButton>
