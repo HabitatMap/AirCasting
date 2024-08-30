5bdf7fc8f5666fc52bece4b315a7757e0c585fdb
Joanna Płatek <joanna.platek@lunarlogic.io>
2024-08-30 11:47:20 +0200

    ref: cleanup YearPicker from copy pasted code

--- app/javascript/react/components/SessionFilters/YearPicker.tsx
+++ app/javascript/react/components/SessionFilters/YearPicker.tsx
@@ -1,53 +1,12 @@
-import { debounce } from "lodash";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { UserSettings } from "../../types/userStates";
-import { useMapParams } from "../../utils/mapParamsHandler";
 import { FilterInfoPopup } from "./FilterInfoPopup";
 import * as S from "./SessionFilters.style";
 import { YearPickerButtons } from "./YearPickerButtons";
 
 const YearPicker = () => {
-  const { currentUserSettings, previousUserSettings } = useMapParams();
   const { t } = useTranslation();
-
-  const getInitialMobileState = () =>
-    window.matchMedia("(max-width: 1023px)").matches;
-
-  const getInitialCrowdMapState = () => {
-    return getInitialMobileState()
-      ? previousUserSettings === UserSettings.CrowdMapView
-      : currentUserSettings === UserSettings.CrowdMapView;
-  };
-
-  const [isMobile, setIsMobile] = useState(getInitialMobileState);
-  const [isCrowdMapActive, setIsCrowdMapActive] = useState(
-    getInitialCrowdMapState
-  );
-
-  useEffect(() => {
-    const checkMobile = debounce(() => {
-      const mobileState = window.matchMedia("(max-width: 1023px)").matches;
-      setIsMobile(mobileState);
-    }, 200);
-
-    window.addEventListener("resize", checkMobile);
-    return () => {
-      window.removeEventListener("resize", checkMobile);
-      checkMobile.cancel();
-    };
-  }, []);
-
-  useEffect(() => {
-    const newCrowdMapState = isMobile
-      ? previousUserSettings === UserSettings.CrowdMapView
-      : currentUserSettings === UserSettings.CrowdMapView;
-
-    if (newCrowdMapState !== isCrowdMapActive) {
-      setIsCrowdMapActive(newCrowdMapState);
-    }
-  }, [currentUserSettings, previousUserSettings, isMobile, isCrowdMapActive]);
 
   return (
     <S.SingleFilterWrapper>
