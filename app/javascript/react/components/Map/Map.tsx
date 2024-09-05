23d8cb78f461d266e97106af83b5f5ba63d56f14
bazelajoanna <bazela.joanna@gmail.com>
2024-09-05 23:40:48 +0200

    Update dependency array

--- app/javascript/react/components/Map/Map.tsx
+++ app/javascript/react/components/Map/Map.tsx
@@ -336,6 +336,8 @@ const Map = () => {
     fixedSessionTypeSelected,
     offset,
     updateFetchedSessions,
+    isIndoorParameterInUrl,
+    isDormantParameterInUrl,
   ]);
 
   useEffect(() => {
