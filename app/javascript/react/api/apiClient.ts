1623fa88e8e8b2f05e2d4e618f424be3d785b683
bazelajoanna <bazela.joanna@gmail.com>
2024-09-06 16:27:20 +0200

    change env

--- app/javascript/react/api/apiClient.ts
+++ app/javascript/react/api/apiClient.ts
@@ -2,11 +2,11 @@ import axios, { AxiosResponse } from "axios";
 import { camelizeKeys } from "humps";
 
 const apiClient = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.API_STREAM_URL,
 });
 
 const oldApiClient = axios.create({
-  baseURL: process.env.EXPORT_SESSION_API_BASE_URL,
+  baseURL: process.env.API_BASE_URL,
 });
 
 apiClient.interceptors.response.use((response: AxiosResponse) => {
