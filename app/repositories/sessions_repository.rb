edd1250f5409baa23a2fed4d9c92a6a1d59bacac
BartkiewiczM <102188388+BartkiewiczM@users.noreply.github.com>
2024-09-12 13:22:48 +0200

    [Performance] split the logic for fetching airbeam/government data to improve performance of /sessions2 endpoint (#802)

--- app/repositories/sessions_repository.rb
+++ app/repositories/sessions_repository.rb
@@ -6,4 +6,38 @@ class SessionsRepository
   def active_in_last_7_days
     Session.where('last_measurement_at > ?', Time.current - 7.days)
   end
+
+  def fixed_active_government_sessions(sensor_name:, west:, east:, north:, south:)
+    sensor_name = ActiveRecord::Base.connection.quote(sensor_name)
+
+    sql = <<-SQL
+      WITH recent_sessions AS (
+        SELECT id, latitude, longitude, last_measurement_at, end_time_local, start_time_local, is_indoor, title, uuid
+        FROM sessions
+        WHERE last_measurement_at > CURRENT_TIMESTAMP - INTERVAL '24 hours'
+          AND latitude BETWEEN #{south} AND #{north}
+          AND (
+            (#{west} <= #{east} AND longitude BETWEEN #{west} AND #{east})
+            OR
+            (#{west} > #{east} AND (longitude >= #{west} OR longitude <= #{east}))
+          )
+      ),
+      relevant_streams AS (
+        SELECT id, session_id, sensor_name, unit_symbol, measurement_short_type, average_value
+        FROM streams
+        WHERE LOWER(sensor_name) = #{sensor_name}
+      ),
+      latest_daily_averages AS (
+        SELECT DISTINCT ON (stream_id) stream_id, value
+        FROM stream_daily_averages
+        ORDER BY stream_id, date DESC
+      )
+      SELECT s.*, st.*, lda.value AS last_daily_average
+      FROM recent_sessions s
+      JOIN relevant_streams st ON s.id = st.session_id
+      LEFT JOIN latest_daily_averages lda ON st.id = lda.stream_id;
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
---
+++ app/serializers/fixed_sessions_serializer.rb
@@ -0,0 +1,42 @@
+class FixedSessionsSerializer
+  def call(sessions)
+    sessions_array = []
+
+    sessions.map do |session|
+      sessions_array <<
+        {
+          'id' => session["session_id"],
+          'uuid' => session["uuid"],
+          'end_time_local' => formatted_time(session["end_time_local"]),
+          'start_time_local' => formatted_time(session["start_time_local"]),
+          'last_measurement_value' => session["average_value"]&.round,
+          'is_indoor' => session["is_indoor"],
+          'latitude' => session["latitude"],
+          'longitude' => session["longitude"],
+          'title' => session["title"],
+          'username' => "username",
+          'is_active' => true,
+          'streams' => {
+            session["sensor_name"] => {
+              'measurement_short_type' => session["measurement_short_type"],
+              'sensor_name' => session["sensor_name"],
+              'unit_symbol' => session["unit_symbol"],
+              'id' => session["id"],
+              'stream_daily_average' => session["last_daily_average"]&.round || 'no data',
+            }
+          }
+        }
+    end
+
+    {
+      'fetchableSessionsCount' => sessions.count,
+      'sessions' => sessions_array,
+    }
+  end
+
+  private
+
+  def formatted_time(timestamp)
+    timestamp ? timestamp.strftime('%Y-%m-%dT%H:%M:%S.%LZ') : nil
+  end
+end
