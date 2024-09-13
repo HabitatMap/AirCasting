89ba95e972571ccc4b97a8ae9354b4fb3603f41e
bazelajoanna <bazela.joanna@gmail.com>
2024-09-13 13:42:41 +0200

    Fix sorting measurements

--- app/javascript/react/utils/lastArrayItem.ts
+++ app/javascript/react/utils/lastArrayItem.ts
@@ -1,5 +1,17 @@
-const lastItemFromArray = <T>(array: T[]): T | undefined => {
-  return array.length ? array[array.length - 1] : undefined;
+interface DateValuePair {
+  date: string;
+  value: number;
+}
+
+const lastItemFromArray = (
+  array: DateValuePair[]
+): DateValuePair | undefined => {
+  if (array.length === 0) return undefined;
+
+  const sortedArray = [...array].sort(
+    (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+  );
+  return sortedArray[sortedArray.length - 1];
 };
 
 export { lastItemFromArray };
