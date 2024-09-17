fc56a979f8e34ea3a3f3a4ee3529ddc6a01b0389
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-09-17 22:19:29 +0200

    Add fixed sessions helpers

---
+++ app/javascript/react/store/fixedSessionsHelpers.ts
@@ -0,0 +1,124 @@
+import { Session, SessionList } from "../types/sessionType";
+import { FixedSession } from "./fixedSessionsSlice";
+
+interface TransformedSession {
+  id: number;
+  title: string;
+  sensorName: string;
+  lastMeasurementValue: number;
+  startTime: string;
+  endTime: string;
+  averageValue: number;
+  point: {
+    lat: number;
+    lng: number;
+    streamId: string;
+  };
+  streamId: number;
+}
+
+export const transformSessionData = (
+  sessions: FixedSession[]
+): TransformedSession[] =>
+  sessions.map(
+    ({
+      id,
+      title,
+      lastMeasurementValue,
+      startTimeLocal,
+      endTimeLocal,
+      latitude,
+      longitude,
+      streams,
+    }) => {
+      const firstStreamKey = Object.keys(streams)[0];
+      const firstStream: {
+        sensorName?: string;
+        streamDailyAverage?: number;
+        id?: number;
+      } = firstStreamKey ? streams[firstStreamKey] : {};
+      return {
+        id,
+        title,
+        sensorName: firstStream.sensorName ?? "",
+        lastMeasurementValue,
+        startTime: startTimeLocal,
+        endTime: endTimeLocal,
+        averageValue: firstStream.streamDailyAverage ?? 0,
+        point: {
+          lat: latitude,
+          lng: longitude,
+          streamId: firstStream.id?.toString() ?? "0",
+        },
+        streamId: firstStream.id ?? 0,
+      };
+    }
+  );
+
+export const getFixedSessionsPoints = (sessions: FixedSession[]): Session[] => {
+  const transformedSessions = transformSessionData(sessions);
+  return transformedSessions.map(
+    ({
+      id,
+      title,
+      sensorName,
+      lastMeasurementValue,
+      startTime,
+      endTime,
+      point,
+    }) => ({
+      id,
+      title,
+      sensorName,
+      startTime,
+      endTime,
+      lastMeasurementValue,
+      point,
+    })
+  );
+};
+
+export const getFixedSessionsList = (
+  sessions: FixedSession[]
+): SessionList[] => {
+  const transformedSessions = transformSessionData(sessions);
+  return transformedSessions.map(
+    ({
+      id,
+      title,
+      sensorName,
+      averageValue,
+      startTime,
+      endTime,
+      streamId,
+    }) => ({
+      id,
+      title,
+      sensorName,
+      averageValue,
+      startTime,
+      endTime,
+      streamId,
+    })
+  );
+};
+
+export const getFixedSessionPointsBySessionId = (
+  sessions: FixedSession[],
+  sessionId: number
+): Session[] => {
+  const transformedSessions = transformSessionData(sessions);
+  const session = transformedSessions.find(
+    (session) => Number(session.id) === Number(sessionId)
+  );
+
+  return session
+    ? [
+        {
+          id: session.id,
+          lastMeasurementValue: session.lastMeasurementValue,
+          point: session.point,
+        },
+      ]
+    : [];
+};
