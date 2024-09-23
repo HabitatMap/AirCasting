7a26a71a35e02dd109d2ed727d3e19a8b2855d4b
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-09-23 10:27:20 +0200

    Check if clusters crush the app

--- app/javascript/react/components/Map/Markers/FixedMarkers.tsx
+++ app/javascript/react/components/Map/Markers/FixedMarkers.tsx
@@ -9,36 +9,25 @@ import React, {
 import {
   Cluster,
   defaultOnClusterClickHandler,
-  GridAlgorithm,
   Marker,
   MarkerClusterer,
-  SuperClusterAlgorithm,
 } from "@googlemaps/markerclusterer";
 import { AdvancedMarker, useMap } from "@vis.gl/react-google-maps";
 
 import { RootState } from "../../../store";
-import { fetchClusterData, setVisibility } from "../../../store/clusterSlice";
+import { setVisibility } from "../../../store/clusterSlice";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import { selectThresholds } from "../../../store/thresholdSlice";
 import { Session } from "../../../types/sessionType";
-import { getClusterPixelPosition } from "../../../utils/getClusterPixelPosition";
 import { useMapParams } from "../../../utils/mapParamsHandler";
 import { getColorForValue } from "../../../utils/thresholdColors";
-import {
-  customRenderer,
-  pulsatingRenderer,
-  updateClusterStyle,
-} from "./ClusterConfiguration";
 
 import {
   selectFixedStreamData,
   selectFixedStreamStatus,
 } from "../../../store/fixedStreamSelectors";
-import { setMarkersLoading } from "../../../store/markersLoadingSlice";
 import { StatusEnum } from "../../../types/api";
 import type { LatLngLiteral } from "../../../types/googleMaps";
-import useMapEventListeners from "../../../utils/mapEventListeners";
-import { ClusterInfo } from "./ClusterInfo/ClusterInfo";
 import { SessionFullMarker } from "./SessionFullMarker/SessionFullMarker";
 import * as S from "./SessionFullMarker/SessionFullMarker.style";
 
@@ -113,64 +102,64 @@ const FixedMarkers = ({
     [map, selectedStreamId]
   );
 
-  const handleBoundsChanged = useCallback(() => {
-    if (selectedCluster && map) {
-      const pixelPosition = getClusterPixelPosition(
-        map,
-        selectedCluster.position
-      );
-      setClusterPosition({ top: pixelPosition.y, left: pixelPosition.x });
-    }
-  }, [map, selectedCluster]);
+  // const handleBoundsChanged = useCallback(() => {
+  //   if (selectedCluster && map) {
+  //     const pixelPosition = getClusterPixelPosition(
+  //       map,
+  //       selectedCluster.position
+  //     );
+  //     setClusterPosition({ top: pixelPosition.y, left: pixelPosition.x });
+  //   }
+  // }, [map, selectedCluster]);
 
-  const handleClusterClick = useCallback(
-    async (
-      event: google.maps.MapMouseEvent,
-      cluster: Cluster,
-      map: google.maps.Map
-    ) => {
-      dispatch(setVisibility(false));
+  // const handleClusterClick = useCallback(
+  //   async (
+  //     event: google.maps.MapMouseEvent,
+  //     cluster: Cluster,
+  //     map: google.maps.Map
+  //   ) => {
+  //     dispatch(setVisibility(false));
 
-      const markerStreamIdMap = clusterer.current?.markerStreamIdMap;
+  //     const markerStreamIdMap = clusterer.current?.markerStreamIdMap;
 
-      const streamIds =
-        cluster.markers &&
-        cluster.markers
-          .map((marker: Marker) => markerStreamIdMap?.get(marker))
-          .filter((streamId) => streamId !== undefined);
+  //     const streamIds =
+  //       cluster.markers &&
+  //       cluster.markers
+  //         .map((marker: Marker) => markerStreamIdMap?.get(marker))
+  //         .filter((streamId) => streamId !== undefined);
 
-      if (streamIds && streamIds.length > 0) {
-        dispatch(fetchClusterData(streamIds as string[]));
-      }
+  //     if (streamIds && streamIds.length > 0) {
+  //       dispatch(fetchClusterData(streamIds as string[]));
+  //     }
 
-      const pixelPosition = getClusterPixelPosition(map, cluster.position);
-      setClusterPosition({ top: pixelPosition.y, left: pixelPosition.x });
-      setSelectedCluster(cluster);
-      dispatch(setVisibility(true));
-    },
-    [dispatch]
-  );
+  //     const pixelPosition = getClusterPixelPosition(map, cluster.position);
+  //     setClusterPosition({ top: pixelPosition.y, left: pixelPosition.x });
+  //     setSelectedCluster(cluster);
+  //     dispatch(setVisibility(true));
+  //   },
+  //   [dispatch]
+  // );
 
-  const clusterElementsRef = useRef<
-    Map<Cluster, google.maps.marker.AdvancedMarkerElement>
-  >(new Map());
+  // const clusterElementsRef = useRef<
+  //   Map<Cluster, google.maps.marker.AdvancedMarkerElement>
+  // >(new Map());
 
-  const handleThresholdChange = useCallback(() => {
-    if (clusterElementsRef.current.size > 0) {
-      clusterElementsRef.current.forEach((clusterElement, cluster) => {
-        updateClusterStyle(
-          clusterElement,
-          cluster.markers as google.maps.marker.AdvancedMarkerElement[],
-          thresholds,
-          selectedStreamId
-        );
-      });
-    }
-  }, [thresholds]);
+  // const handleThresholdChange = useCallback(() => {
+  //   if (clusterElementsRef.current.size > 0) {
+  //     clusterElementsRef.current.forEach((clusterElement, cluster) => {
+  //       updateClusterStyle(
+  //         clusterElement,
+  //         cluster.markers as google.maps.marker.AdvancedMarkerElement[],
+  //         thresholds,
+  //         selectedStreamId
+  //       );
+  //     });
+  //   }
+  // }, [thresholds]);
 
-  useEffect(() => {
-    handleThresholdChange();
-  }, [thresholds, handleThresholdChange]);
+  // useEffect(() => {
+  //   handleThresholdChange();
+  // }, [thresholds, handleThresholdChange]);
 
   const handleMapInteraction = useCallback(() => {
     dispatch(setVisibility(false));
@@ -211,38 +200,38 @@ const FixedMarkers = ({
     []
   );
 
-  const updateClusterer = useCallback(() => {
-    if (clusterer.current && memoizedSessions.length > 0) {
-      const sessionStreamIds = memoizedSessions.map(
-        (session) => session.point.streamId
-      );
-      const markerStreamIdMap = new Map<Marker, string>();
+  // const updateClusterer = useCallback(() => {
+  //   if (clusterer.current && memoizedSessions.length > 0) {
+  //     const sessionStreamIds = memoizedSessions.map(
+  //       (session) => session.point.streamId
+  //     );
+  //     const markerStreamIdMap = new Map<Marker, string>();
 
-      Object.keys(memoizedMarkers).forEach((key) => {
-        if (!sessionStreamIds.includes(key)) {
-          delete memoizedMarkers[key];
-        }
-      });
+  //     Object.keys(memoizedMarkers).forEach((key) => {
+  //       if (!sessionStreamIds.includes(key)) {
+  //         delete memoizedMarkers[key];
+  //       }
+  //     });
 
-      const validMarkers = Object.values(memoizedMarkers).filter(
-        (marker): marker is google.maps.marker.AdvancedMarkerElement =>
-          marker !== null
-      );
+  //     const validMarkers = Object.values(memoizedMarkers).filter(
+  //       (marker): marker is google.maps.marker.AdvancedMarkerElement =>
+  //         marker !== null
+  //     );
 
-      validMarkers.forEach((marker) => {
-        const streamId = sessionStreamIds.find(
-          (id) => memoizedMarkers[id] === marker
-        );
-        if (streamId) {
-          markerStreamIdMap.set(marker, streamId);
-        }
-      });
+  //     validMarkers.forEach((marker) => {
+  //       const streamId = sessionStreamIds.find(
+  //         (id) => memoizedMarkers[id] === marker
+  //       );
+  //       if (streamId) {
+  //         markerStreamIdMap.set(marker, streamId);
+  //       }
+  //     });
 
-      clusterer.current.clearMarkers();
-      clusterer.current.addMarkers(validMarkers);
-      clusterer.current.markerStreamIdMap = markerStreamIdMap;
-    }
-  }, [memoizedSessions, memoizedMarkers, dispatch]);
+  //     clusterer.current.clearMarkers();
+  //     clusterer.current.addMarkers(validMarkers);
+  //     clusterer.current.markerStreamIdMap = markerStreamIdMap;
+  //   }
+  // }, [memoizedSessions, memoizedMarkers, dispatch]);
 
   useEffect(() => {
     if (selectedStreamId) {
@@ -256,16 +245,16 @@ const FixedMarkers = ({
     }
   }, [selectedStreamId, memoizedSessions]);
 
-  useEffect(() => {
-    return () => {
-      if (clusterer.current) {
-        clusterer.current.clearMarkers();
-      }
-      if (pulsatingClusterer.current) {
-        pulsatingClusterer.current.clearMarkers();
-      }
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     if (clusterer.current) {
+  //       clusterer.current.clearMarkers();
+  //     }
+  //     if (pulsatingClusterer.current) {
+  //       pulsatingClusterer.current.clearMarkers();
+  //     }
+  //   };
+  // }, []);
 
   useEffect(() => {
     const handleSelectedStreamId = (streamId: number | null) => {
@@ -285,9 +274,9 @@ const FixedMarkers = ({
     handleSelectedStreamId(selectedStreamId);
   }, [selectedStreamId, fixedStreamData, fixedStreamStatus, centerMapOnMarker]);
 
-  useEffect(() => {
-    dispatch(setMarkersLoading(true));
-  }, [dispatch, sessions.length]);
+  // useEffect(() => {
+  //   dispatch(setMarkersLoading(true));
+  // }, [dispatch, sessions.length]);
 
   // useEffect(() => {
   //   if (hoverStreamId) {
@@ -302,98 +291,98 @@ const FixedMarkers = ({
   //   }
   // }, [hoverStreamId, memoizedSessions]);
 
-  useEffect(() => {
-    if (markersCount >= sessions.length) {
-      dispatch(setMarkersLoading(false));
-    }
-  }, [dispatch, markersCount, sessions.length]);
+  // useEffect(() => {
+  //   if (markersCount >= sessions.length) {
+  //     dispatch(setMarkersLoading(false));
+  //   }
+  // }, [dispatch, markersCount, sessions.length]);
 
-  useEffect(() => {
-    if (pulsatingSessionId) {
-      const pulsatingSession = memoizedSessions.find(
-        (session) => session.id === pulsatingSessionId
-      );
-      const pulsatingSessionStreamId = pulsatingSession?.point.streamId;
-      if (pulsatingSessionStreamId && clusterer.current) {
-        const pulsatingCluster: Cluster | undefined =
-          //@ts-expect-error
-          clusterer.current.clusters.find(
-            (cluster: Cluster) =>
-              cluster.markers &&
-              cluster.markers.some(
-                (clusterMarker: Marker) =>
-                  clusterMarker === memoizedMarkers[pulsatingSessionStreamId]
-              )
-          );
+  // useEffect(() => {
+  //   if (pulsatingSessionId) {
+  //     const pulsatingSession = memoizedSessions.find(
+  //       (session) => session.id === pulsatingSessionId
+  //     );
+  //     const pulsatingSessionStreamId = pulsatingSession?.point.streamId;
+  //     if (pulsatingSessionStreamId && clusterer.current) {
+  //       const pulsatingCluster: Cluster | undefined =
+  //         //@ts-expect-error
+  //         clusterer.current.clusters.find(
+  //           (cluster: Cluster) =>
+  //             cluster.markers &&
+  //             cluster.markers.some(
+  //               (clusterMarker: Marker) =>
+  //                 clusterMarker === memoizedMarkers[pulsatingSessionStreamId]
+  //             )
+  //         );
 
-        if (
-          pulsatingCluster &&
-          pulsatingCluster.markers &&
-          pulsatingCluster.markers.length > 1
-        ) {
-          if (pulsatingClusterer.current) {
-            pulsatingClusterer.current.clearMarkers();
-          }
-          pulsatingClusterer.current = new MarkerClusterer({
-            map,
-            renderer: pulsatingRenderer(thresholds, pulsatingCluster.position),
-            markers: pulsatingCluster.markers,
-            algorithm: new GridAlgorithm({ gridSize: 1000 }),
-          });
-        }
-      }
-    } else {
-      if (pulsatingClusterer.current) {
-        pulsatingClusterer.current.clearMarkers();
-        pulsatingClusterer.current = null;
-      }
-    }
-  }, [pulsatingSessionId, memoizedMarkers, memoizedSessions, thresholds]);
+  //       if (
+  //         pulsatingCluster &&
+  //         pulsatingCluster.markers &&
+  //         pulsatingCluster.markers.length > 1
+  //       ) {
+  //         if (pulsatingClusterer.current) {
+  //           pulsatingClusterer.current.clearMarkers();
+  //         }
+  //         pulsatingClusterer.current = new MarkerClusterer({
+  //           map,
+  //           renderer: pulsatingRenderer(thresholds, pulsatingCluster.position),
+  //           markers: pulsatingCluster.markers,
+  //           algorithm: new GridAlgorithm({ gridSize: 1000 }),
+  //         });
+  //       }
+  //     }
+  //   } else {
+  //     if (pulsatingClusterer.current) {
+  //       pulsatingClusterer.current.clearMarkers();
+  //       pulsatingClusterer.current = null;
+  //     }
+  //   }
+  // }, [pulsatingSessionId, memoizedMarkers, memoizedSessions, thresholds]);
 
-  useEffect(() => {
-    updateClusterer();
-  }, [updateClusterer]);
+  // useEffect(() => {
+  //   updateClusterer();
+  // }, [updateClusterer]);
 
-  useEffect(() => {
-    if (map && memoizedSessions.length > 0) {
-      if (!clusterer.current) {
-        clusterer.current = new MarkerClusterer({
-          map,
-          renderer: customRenderer(
-            thresholds,
-            clusterElementsRef,
-            selectedStreamId
-          ),
-          algorithm: new SuperClusterAlgorithm({
-            maxZoom: 21,
-            radius: 40,
-          }),
-          onClusterClick: handleClusterClick,
-        }) as CustomMarkerClusterer;
-      } else {
-        updateClusterer();
-      }
-    }
-  }, [map, memoizedSessions, updateClusterer]);
+  // useEffect(() => {
+  //   if (map && memoizedSessions.length > 0) {
+  //     if (!clusterer.current) {
+  //       clusterer.current = new MarkerClusterer({
+  //         map,
+  //         renderer: customRenderer(
+  //           thresholds,
+  //           clusterElementsRef,
+  //           selectedStreamId
+  //         ),
+  //         algorithm: new SuperClusterAlgorithm({
+  //           maxZoom: 21,
+  //           radius: 40,
+  //         }),
+  //         onClusterClick: handleClusterClick,
+  //       }) as CustomMarkerClusterer;
+  //     } else {
+  //       updateClusterer();
+  //     }
+  //   }
+  // }, [map, memoizedSessions, updateClusterer]);
 
-  useEffect(() => {
-    map && map.addListener("zoom_changed", handleMapInteraction);
-  }, [map, selectedCluster, dispatch, clusterer.current]);
+  // useEffect(() => {
+  //   map && map.addListener("zoom_changed", handleMapInteraction);
+  // }, [map, selectedCluster, dispatch, clusterer.current]);
 
-  useMapEventListeners(map, {
-    click: () => {
-      handleMapInteraction();
-    },
-    touchend: () => {
-      handleMapInteraction();
-    },
-    dragstart: () => {
-      handleMapInteraction();
-    },
-    bounds_changed: () => {
-      handleBoundsChanged();
-    },
-  });
+  // useMapEventListeners(map, {
+  //   click: () => {
+  //     handleMapInteraction();
+  //   },
+  //   touchend: () => {
+  //     handleMapInteraction();
+  //   },
+  //   dragstart: () => {
+  //     handleMapInteraction();
+  //   },
+  //   bounds_changed: () => {
+  //     handleBoundsChanged();
+  //   },
+  // });
 
   return (
     <>
@@ -435,7 +424,7 @@ const FixedMarkers = ({
         </AdvancedMarker>
       ))}
       {/* {hoverPosition && <HoverMarker position={hoverPosition} />} */}
-      {selectedCluster && clusterPosition && !clusterLoading && clusterData && (
+      {/* {selectedCluster && clusterPosition && !clusterLoading && clusterData && (
         <ClusterInfo
           color={getColorForValue(thresholds, clusterData.average)}
           average={clusterData.average}
@@ -444,7 +433,7 @@ const FixedMarkers = ({
           position={clusterPosition}
           visible={clusterVisible}
         />
-      )}
+      )} */}
     </>
   );
 };
