3f419f3120321005334e655ecf391785198b0bfc
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-09-23 10:42:39 +0200

    Show only 10 sessions

--- app/javascript/react/store/fixedSessionsSlice.ts
+++ app/javascript/react/store/fixedSessionsSlice.ts
@@ -1,7 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AxiosResponse } from "axios";
 import { RootState } from ".";
-import { oldApiClient } from "../api/apiClient";
 import { API_ENDPOINTS } from "../api/apiEndpoints";
 import { ApiError, StatusEnum } from "../types/api";
 import { getErrorMessage } from "../utils/getErrorMessage";
@@ -74,10 +72,16 @@ const createSessionFetchThunk = (
     }FixedSessions`,
     async (sessionsData, { rejectWithValue }) => {
       try {
-        const response: AxiosResponse<SessionsResponse> =
-          await oldApiClient.get(endpoint(sessionsData.filters));
+        // const response: AxiosResponse<SessionsResponse> =
+        //   await oldApiClient.get(endpoint(sessionsData.filters));
 
-        return response.data;
+        // return response.data;
+        const mockResponse: SessionsResponse = {
+          fetchableSessionsCount: mockData.length,
+          sessions: mockData,
+        };
+
+        return mockResponse; // Return the mock data
       } catch (error) {
         const message = getErrorMessage(error);
 
@@ -178,3 +182,241 @@ const fixedSessionsSlice = createSlice({
 
 export const { cleanSessions } = fixedSessionsSlice.actions;
 export default fixedSessionsSlice.reducer;
+
+const data = [
+  {
+    id: 1879647,
+    uuid: "f831b94a-3c7e-4414-a5ad-afd256a5e570",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 5,
+    is_indoor: false,
+    latitude: 30.4889,
+    longitude: -87.8706,
+    title: "FAIRHOPE",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2595983,
+        stream_daily_average: 6,
+      },
+    },
+  },
+  {
+    id: 1879874,
+    uuid: "c3fbc48a-40f0-44c3-9043-6e2c45dd577c",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 24,
+    is_indoor: false,
+    latitude: 30.7697,
+    longitude: -88.0875,
+    title: "Chickasaw",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2595997,
+        stream_daily_average: 18,
+      },
+    },
+  },
+  {
+    id: 1880176,
+    uuid: "b6c33b69-9f3a-4066-9fb0-cc1f457b035b",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 5,
+    is_indoor: false,
+    latitude: 32.36253,
+    longitude: -88.27792,
+    title: "Ward",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596005,
+        stream_daily_average: 6,
+      },
+    },
+  },
+  {
+    id: 1879660,
+    uuid: "bed75e7d-bfbc-444a-814d-baa3f2820c7b",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 28,
+    is_indoor: false,
+    latitude: 34.756058,
+    longitude: -92.281034,
+    title: "PARR",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596075,
+        stream_daily_average: 25,
+      },
+    },
+  },
+  {
+    id: 1875019,
+    uuid: "cc68d788-5670-4c46-a674-25719c5999a8",
+    end_time_local: "2024-09-23T01:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 4,
+    is_indoor: false,
+    latitude: 40.052241,
+    longitude: -88.372549,
+    title: "BONDVILLE",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596708,
+        stream_daily_average: 4,
+      },
+    },
+  },
+  {
+    id: 1879790,
+    uuid: "5f0757fb-ac16-4ae7-a875-8f70d4dccdd7",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 2,
+    is_indoor: false,
+    latitude: 41.6708,
+    longitude: -87.7325,
+    title: "ALSIP",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596712,
+        stream_daily_average: 2,
+      },
+    },
+  },
+  {
+    id: 1875024,
+    uuid: "0d961ac2-af7a-40b1-a477-bd0c28ce364c",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 2,
+    is_indoor: false,
+    latitude: 41.9136,
+    longitude: -87.7239,
+    title: "CHI_SP",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596714,
+        stream_daily_average: 2,
+      },
+    },
+  },
+  {
+    id: 1879792,
+    uuid: "2f0fd824-9bd4-4998-9c92-c2d5f4e1b74b",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 3,
+    is_indoor: false,
+    latitude: 41.965,
+    longitude: -87.8761,
+    title: "SCHILPRK",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596720,
+        stream_daily_average: 3,
+      },
+    },
+  },
+  {
+    id: 1880470,
+    uuid: "20e2de41-be29-4b75-bafd-fced3f303550",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 2,
+    is_indoor: false,
+    latitude: 42.0603,
+    longitude: -87.8631,
+    title: "DESPLNS",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596723,
+        stream_daily_average: 2,
+      },
+    },
+  },
+  {
+    id: 1875031,
+    uuid: "05e27f72-d4ed-478a-8096-4e6e4bee6c20",
+    end_time_local: "2024-09-23T02:00:00.000Z",
+    start_time_local: "2024-04-24T11:00:00.000Z",
+    last_measurement_value: 2,
+    is_indoor: false,
+    latitude: 42.1406,
+    longitude: -87.7994,
+    title: "NORTHBRK",
+    username: "username",
+    is_active: true,
+    streams: {
+      "Government-PM2.5": {
+        measurement_short_type: "PM",
+        sensor_name: "Government-PM2.5",
+        unit_symbol: "µg/m³",
+        id: 2596724,
+        stream_daily_average: 2,
+      },
+    },
+  },
+];
+const mapToFixedSession = (session: any): FixedSession => ({
+  id: session.id,
+  uuid: session.uuid,
+  endTimeLocal: session.end_time_local,
+  startTimeLocal: session.start_time_local,
+  lastMeasurementValue: session.last_measurement_value,
+  isIndoor: session.is_indoor,
+  latitude: session.latitude,
+  longitude: session.longitude,
+  title: session.title,
+  username: session.username,
+  streams: session.streams,
+});
+
+const mockData: FixedSession[] = data.map(mapToFixedSession);
