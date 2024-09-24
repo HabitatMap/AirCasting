7d37c62f495d4f556f9c90bf92a0da95f4039d58
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-09-24 11:21:42 +0200

    Old stream markesr without session details

--- app/javascript/react/components/Map/Markers/StreamMarkers.tsx
+++ app/javascript/react/components/Map/Markers/StreamMarkers.tsx
@@ -1,12 +1,16 @@
-import { useMap } from "@vis.gl/react-google-maps";
-import { useEffect, useMemo, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
+import { Marker } from "@googlemaps/markerclusterer";
+import { AdvancedMarker, useMap } from "@vis.gl/react-google-maps";
+
 import { mobileStreamPath } from "../../../assets/styles/colors";
-import { useAppDispatch } from "../../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { selectHoverPosition } from "../../../store/mapSlice";
 import { selectThresholds } from "../../../store/thresholdSlice";
 import { Session } from "../../../types/sessionType";
 import { getColorForValue } from "../../../utils/thresholdColors";
+import { StreamMarker } from "./StreamMarker/StreamMarker";
 
 type Props = {
   sessions: Session[];
@@ -16,63 +20,29 @@ type Props = {
 const StreamMarkers = ({ sessions, unitSymbol }: Props) => {
   const dispatch = useAppDispatch();
   const map = useMap();
-  const markersRef = useRef<google.maps.Marker[]>([]);
+  const [markers, setMarkers] = useState<{ [streamId: string]: Marker | null }>(
+    {}
+  );
   const thresholds = useSelector(selectThresholds);
   const polylineRef = useRef<google.maps.Polyline | null>(null);
+  const hoverPosition = useAppSelector(selectHoverPosition);
 
   // Sort sessions by time
-  const sortedSessions = useMemo(
-    () =>
-      [...sessions].sort((a, b) => {
-        const timeA = a.time ? new Date(a.time).getTime() : 0;
-        const timeB = b.time ? new Date(b.time).getTime() : 0;
-        return timeA - timeB;
-      }),
-    [sessions]
-  );
+  const sortedSessions = sessions.sort((a, b) => {
+    const timeA = a.time ? new Date(a.time.toString()).getTime() : 0;
+    const timeB = b.time ? new Date(b.time.toString()).getTime() : 0;
+    return timeA - timeB;
+  });
 
   // useEffect(() => {
   //   dispatch(setMarkersLoading(true));
   //   dispatch(setTotalMarkers(sessions.length));
   // }, [dispatch, sessions.length]);
 
-  // Create and update polyline and markers
+  // Create and update polyline
   useEffect(() => {
     if (!map) return;
 
-    // Cleanup previous markers
-    markersRef.current.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markersRef.current = [];
-
-    // Create markers and add them to the map
-    sortedSessions.forEach((session) => {
-      const marker = new google.maps.Marker({
-        position: { lat: session.point.lat, lng: session.point.lng },
-        map,
-        title: `${session.lastMeasurementValue} ${unitSymbol}`,
-        zIndex: 0,
-        icon: {
-          // Customize your marker icon if needed
-          path: google.maps.SymbolPath.CIRCLE,
-          fillColor: getColorForValue(thresholds, session.lastMeasurementValue),
-          fillOpacity: 1,
-          strokeColor: getColorForValue(
-            thresholds,
-            session.lastMeasurementValue
-          ),
-          strokeWeight: 1,
-          scale: 6,
-        },
-      });
-
-      // Optionally, add event listeners to the marker here
-
-      markersRef.current.push(marker);
-    });
-
-    // Create or update the polyline
     const path = sortedSessions.map((session) => ({
       lat: session.point.lat,
       lng: session.point.lng,
@@ -90,23 +60,42 @@ const StreamMarkers = ({ sessions, unitSymbol }: Props) => {
       });
     }
 
-    // Cleanup function to remove markers and polyline when component unmounts or sessions change
+    // Cleanup function to remove the polyline
     return () => {
-      // Remove markers from map
-      markersRef.current.forEach((marker) => {
-        marker.setMap(null);
-      });
-      markersRef.current = [];
-
-      // Remove polyline from map
       if (polylineRef.current) {
-        polylineRef.current.setMap(null);
-        polylineRef.current = null;
+        polylineRef.current.setMap(null); // Remove the polyline from the map
+        polylineRef.current = null; // Cleanup the reference
       }
     };
-  }, [map, sortedSessions, unitSymbol, thresholds]);
+  }, [sortedSessions, map]);
 
-  return null; // No JSX needed since we're directly interacting with the Google Maps API
+  return (
+    <>
+      {sessions.map((session) => (
+        // <React.Fragment key={session.id}>
+        <AdvancedMarker
+          title={`${session.lastMeasurementValue} ${unitSymbol}`}
+          position={session.point}
+          key={`marker-${session.id}`}
+          zIndex={0}
+          ref={(marker) => {
+            if (marker && !markers[session.point.streamId]) {
+              setMarkers((prev) => ({
+                ...prev,
+                [session.point.streamId]: marker,
+              }));
+            }
+          }}
+        >
+          <StreamMarker
+            color={getColorForValue(thresholds, session.lastMeasurementValue)}
+          />
+        </AdvancedMarker>
+        // </React.Fragment>
+      ))}
+      {/* {hoverPosition && <HoverMarker position={hoverPosition} />} */}
+    </>
+  );
 };
 
 export { StreamMarkers };
