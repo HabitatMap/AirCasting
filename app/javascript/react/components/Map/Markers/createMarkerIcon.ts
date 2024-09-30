a913ec066450a53f015da90833691cf90a3c9fd3
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-09-30 22:42:43 +0200

    Create styling for selected markers

--- app/javascript/react/components/Map/Markers/createMarkerIcon.ts
+++ app/javascript/react/components/Map/Markers/createMarkerIcon.ts
@@ -25,27 +25,40 @@ export const createMarkerIcon = (
 
   const padding = 7;
   const baseCircleX = 19;
-  const baseCircleR = 5;
-  const rectHeight = 18;
+  const baseCircleR = 6;
+  const rectHeight = 20;
   const height = 40;
   const strokeWidth = isSelected ? 1 : 0;
+  const shadowRadius = isSelected ? 22 : 20;
 
   const font = "12px Roboto, Arial, sans-serif";
   const textWidth = getTextWidth(value, font);
-  const totalWidth =
-    baseCircleX + baseCircleR * 2 + padding * 2 + textWidth + 2;
+  const totalWidth = Math.max(
+    baseCircleX + baseCircleR * 2 + padding * 2 + textWidth + 2,
+    shadowRadius * 2
+  );
+
+  const shadowColor = `${color}66`; // More opaque for non-selected
 
   const svg = `
     <svg xmlns="http://www.w3.org/2000/svg" width="${totalWidth}" height="${height}" viewBox="0 0 ${totalWidth} ${height}">
       <defs>
-        <radialGradient id="shadowGradient" cx="50%" cy="50%" r="50%">
-          <stop offset="0%" stop-color="${color}" stop-opacity="0.6" />
-          <stop offset="50%" stop-color="${color}" stop-opacity="0.3" />
-          <stop offset="100%" stop-color="${color}"/>
+        <radialGradient id="shadowGradient" cx="50%" cy="50%" r="70%">
+          <stop offset="0%" stop-color="${shadowColor}" />
+          <stop offset="${
+            isSelected ? "30%" : "40%"
+          }" stop-color="${shadowColor}" />
+          <stop offset="100%" stop-color="${shadowColor}" stop-opacity="0" />
         </radialGradient>
-        <filter id="blurFilter" x="-50%" y="-50%" width="200%" height="200%" filterUnits="userSpaceOnUse">
-          <feGaussianBlur stdDeviation="4" />
+        ${
+          isSelected
+            ? `
+        <filter id="blur" x="-50%" y="-50%" width="200%" height="200%">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
         </filter>
+        `
+            : ""
+        }
       </defs>
       <style>
         .pulse {
@@ -58,9 +71,9 @@ export const createMarkerIcon = (
           100% { transform: scale(1); opacity: 1; }
         }
       </style>
-      <circle cx="${baseCircleX}" cy="20" r="15" fill="url(#shadowGradient)" filter="url(#blurFilter)" class="${
-    shouldPulse ? "pulse" : ""
-  }" />
+      <circle cx="${baseCircleX}" cy="20" r="${shadowRadius}" fill="url(#shadowGradient)" ${
+    isSelected ? 'filter="url(#blur)"' : ""
+  } class="${shouldPulse ? "pulse" : ""}" />
       <rect x="8" y="${(height - rectHeight) / 2}" rx="9" ry="9" width="${
     totalWidth - 14
   }" height="${rectHeight}" fill="white" stroke="${color}" stroke-width="${strokeWidth}"/>
@@ -76,7 +89,7 @@ export const createMarkerIcon = (
   const icon = {
     url: `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(svg)}`,
     scaledSize: new google.maps.Size(totalWidth, height),
-    anchor: new google.maps.Point(20, 20),
+    anchor: new google.maps.Point(baseCircleX, 20),
   };
 
   iconCache.set(cacheKey, icon);
