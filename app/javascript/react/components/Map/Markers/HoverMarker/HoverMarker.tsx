2a2bf413a2e79292bd332e57594dc7e27de77a99
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-10-07 11:26:14 +0200

    Change hover marker

--- app/javascript/react/components/Map/Markers/HoverMarker/HoverMarker.tsx
+++ app/javascript/react/components/Map/Markers/HoverMarker/HoverMarker.tsx
@@ -1,23 +1,51 @@
-import { AdvancedMarker } from "@vis.gl/react-google-maps";
-import React from "react";
+import { useMap } from "@vis.gl/react-google-maps";
+import React, { useEffect, useRef } from "react";
+import { blue } from "../../../../assets/styles/colors";
 import { LatLngLiteral } from "../../../../types/googleMaps";
-import * as S from "./HoverMarker.style";
 
 interface HoverMarkerProps {
   position: LatLngLiteral | null;
 }
 
 const HoverMarker: React.FC<HoverMarkerProps> = ({ position }) => {
-  if (!position) return null;
+  const map = useMap();
+  const markerRef = useRef<google.maps.Marker | null>(null);
 
-  return (
-    <AdvancedMarker
-      position={position}
-      zIndex={Number(google.maps.Marker.MAX_ZINDEX + 2)}
-    >
-      <S.MarkerCircle />
-    </AdvancedMarker>
-  );
+  useEffect(() => {
+    // Create or update marker
+    if (map && position) {
+      if (!markerRef.current) {
+        markerRef.current = new google.maps.Marker({
+          map,
+          zIndex: Number(google.maps.Marker.MAX_ZINDEX + 2),
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            fillColor: blue,
+            fillOpacity: 1,
+            strokeWeight: 0,
+            scale: 8,
+          },
+        });
+      }
+      markerRef.current.setPosition(position);
+    } else {
+      // Remove marker if there's no position or map
+      if (markerRef.current) {
+        markerRef.current.setMap(null);
+        markerRef.current = null;
+      }
+    }
+
+    // Cleanup function
+    return () => {
+      if (markerRef.current) {
+        markerRef.current.setMap(null);
+        markerRef.current = null;
+      }
+    };
+  }, [map, position]);
+
+  return null;
 };
 
 export default HoverMarker;
