4ebb2d20d119a1e5388b44ad261eb85472dbdfdf
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2024-10-07 13:50:17 +0200

    revert changes in hover (#845)

--- app/javascript/react/components/Map/Markers/HoverMarker/HoverMarker.tsx
+++ app/javascript/react/components/Map/Markers/HoverMarker/HoverMarker.tsx
@@ -1,51 +1,23 @@
-import { useMap } from "@vis.gl/react-google-maps";
-import React, { useEffect, useRef } from "react";
-import { blue } from "../../../../assets/styles/colors";
+import { AdvancedMarker } from "@vis.gl/react-google-maps";
+import React from "react";
 import { LatLngLiteral } from "../../../../types/googleMaps";
+import * as S from "./HoverMarker.style";
 
 interface HoverMarkerProps {
   position: LatLngLiteral | null;
 }
 
 const HoverMarker: React.FC<HoverMarkerProps> = ({ position }) => {
-  const map = useMap();
-  const markerRef = useRef<google.maps.Marker | null>(null);
+  if (!position) return null;
 
-  useEffect(() => {
-    // Create or update marker
-    if (map && position) {
-      if (!markerRef.current) {
-        markerRef.current = new google.maps.Marker({
-          map,
-          zIndex: Number(google.maps.Marker.MAX_ZINDEX + 2),
-          icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            fillColor: blue,
-            fillOpacity: 1,
-            strokeWeight: 0,
-            scale: 8,
-          },
-        });
-      }
-      markerRef.current.setPosition(position);
-    } else {
-      // Remove marker if there's no position or map
-      if (markerRef.current) {
-        markerRef.current.setMap(null);
-        markerRef.current = null;
-      }
-    }
-
-    // Cleanup function
-    return () => {
-      if (markerRef.current) {
-        markerRef.current.setMap(null);
-        markerRef.current = null;
-      }
-    };
-  }, [map, position]);
-
-  return null;
+  return (
+    <AdvancedMarker
+      position={position}
+      zIndex={Number(google.maps.Marker.MAX_ZINDEX + 2)}
+    >
+      <S.MarkerCircle />
+    </AdvancedMarker>
+  );
 };
 
 export default HoverMarker;
