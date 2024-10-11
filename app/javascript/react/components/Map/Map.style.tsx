d528d89593c9e3ff2da28a5627b9e902118e7918
bazelajoanna <bazela.joanna@gmail.com>
2024-10-11 12:37:18 +0200

    Fix NavList height to be fully displayed on android

--- app/javascript/react/components/Map/Map.style.tsx
+++ app/javascript/react/components/Map/Map.style.tsx
@@ -7,7 +7,7 @@ const ContainerStyle: React.CSSProperties = {
   width: "100%",
   height: "100%",
   overflow: "hidden",
-  position: "absolute", // Ensure it covers the entire viewport
+  position: "absolute",
   top: 0,
   left: 0,
   right: 0,
