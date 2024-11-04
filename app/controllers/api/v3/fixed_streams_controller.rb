64c0afcb92882d5f664a53d3a74ed7b146562ae7
Marta Połeć <polec.marta@gmail.com>
2024-11-04 12:50:20 +0100

    Remove unused feature flags

--- app/controllers/api/v3/fixed_streams_controller.rb
+++ app/controllers/api/v3/fixed_streams_controller.rb
@@ -2,17 +2,12 @@ module Api
   module V3
     class FixedStreamsController < BaseController
       def show
-        if Flipper.enabled?(:calendar)
-          result =
-            ::FixedStreams::ShowInteractor.new.call(stream_id: params[:id])
+        result = ::FixedStreams::ShowInteractor.new.call(stream_id: params[:id])
 
-          if result.success?
-            render json: result.value, status: :ok
-          else
-            render json: result.errors, status: :bad_request
-          end
+        if result.success?
+          render json: result.value, status: :ok
         else
-          head :not_found
+          render json: result.errors, status: :bad_request
         end
       end
     end
