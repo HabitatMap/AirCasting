0e5c1185aef0162107cb035cec6b9f1073fa9674
Marta Połeć <polec.marta@gmail.com>
2024-11-25 10:55:05 +0100

    Change logic for averages calculation for govt data (#902)

--- app/repositories/measurements_repository.rb
+++ app/repositories/measurements_repository.rb
@@ -14,20 +14,30 @@ class MeasurementsRepository
       .average(:value)
   end
 
-  def streams_averages_hourly_last_7_days(stream_ids:)
+  def streams_averages_hourly_last_7_days(stream_ids:, with_hour_shift:)
     end_date = Time.current.end_of_hour - 1.hour
     start_date = end_date.beginning_of_hour - 7.days
 
-    ActiveRecord::Base.connection.execute(
-      "
-        SELECT (DATE_TRUNC('hour', time_with_time_zone) + INTERVAL '1 hour') AS hour, AVG(value) AS average_value
+    hour_selection =
+      if with_hour_shift
+        "(DATE_TRUNC('hour', time_with_time_zone) + INTERVAL '1 hour')"
+      else
+        "DATE_TRUNC('hour', time_with_time_zone)"
+      end
+
+    ActiveRecord::Base
+      .connection
+      .execute(
+        "
+        SELECT #{hour_selection} AS hour, AVG(value) AS average_value
         FROM measurements
         WHERE stream_id IN (#{stream_ids.join(',')})
         AND time_with_time_zone >= '#{start_date}'
         AND time_with_time_zone < '#{end_date}'
         GROUP BY hour
         ORDER BY hour
-      "
-    ).map { |record| { time: record['hour'], value: record['average_value'] } }
+      ",
+      )
+      .map { |record| { time: record['hour'], value: record['average_value'] } }
   end
 end
