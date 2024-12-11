81436bedc9d2e17604754bc3e2787bf8eafd9e00
Marta Połeć <polec.marta@gmail.com>
2024-12-11 15:56:14 +0100

    Add sidekiq_averages_calculation_enabled to config

--- app/workers/update_stream_hourly_averages_worker.rb
+++ app/workers/update_stream_hourly_averages_worker.rb
@@ -4,6 +4,8 @@ class UpdateStreamHourlyAveragesWorker
   include Sidekiq::Worker
 
   def perform
+    return unless A9n.sidekiq_averages_calculation_enabled
+
     StreamHourlyAverages::Updater.new.call
   end
 end
