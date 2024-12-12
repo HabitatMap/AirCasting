7fc3d93eaa47bc917e89651698c05ba74775bd5f
Marta Połeć <polec.marta@gmail.com>
2024-12-12 15:40:56 +0100

    Add last_hourly_average_value to FixedSessionsSerializer
    Temp solution, needs to be changed after avg calculation is added
    for AirNow data

--- app/serializers/fixed_sessions_serializer.rb
+++ app/serializers/fixed_sessions_serializer.rb
@@ -3,35 +3,33 @@ class FixedSessionsSerializer
     sessions_array = []
 
     sessions.map do |session|
-      sessions_array <<
-        {
-          'id' => session["session_id"],
-          'uuid' => session["uuid"],
-          'end_time_local' => formatted_time(session["end_time_local"]),
-          'start_time_local' => formatted_time(session["start_time_local"]),
-          'last_measurement_value' => session["average_value"]&.round,
-          'is_indoor' => session["is_indoor"],
-          'latitude' => session["latitude"],
-          'longitude' => session["longitude"],
-          'title' => session["title"],
-          'username' => "username",
-          'is_active' => true,
-          'streams' => {
-            session["sensor_name"] => {
-              'measurement_short_type' => session["measurement_short_type"],
-              'sensor_name' => session["sensor_name"],
-              'unit_symbol' => session["unit_symbol"],
-              'id' => session["id"],
-              'stream_daily_average' => session["last_daily_average"]&.round || 'no data',
-            }
-          }
-        }
+      sessions_array << {
+        'id' => session['session_id'],
+        'uuid' => session['uuid'],
+        'end_time_local' => formatted_time(session['end_time_local']),
+        'start_time_local' => formatted_time(session['start_time_local']),
+        'last_measurement_value' => session['average_value']&.round,
+        'is_indoor' => session['is_indoor'],
+        'latitude' => session['latitude'],
+        'longitude' => session['longitude'],
+        'title' => session['title'],
+        'username' => 'username',
+        'is_active' => true,
+        'last_hourly_average_value' => session['average_value']&.round,
+        'streams' => {
+          session['sensor_name'] => {
+            'measurement_short_type' => session['measurement_short_type'],
+            'sensor_name' => session['sensor_name'],
+            'unit_symbol' => session['unit_symbol'],
+            'id' => session['id'],
+            'stream_daily_average' =>
+              session['last_daily_average']&.round || 'no data',
+          },
+        },
+      }
     end
 
-    {
-      'fetchableSessionsCount' => sessions.count,
-      'sessions' => sessions_array,
-    }
+    { 'fetchableSessionsCount' => sessions.count, 'sessions' => sessions_array }
   end
 
   private
