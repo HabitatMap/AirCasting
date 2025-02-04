1116d46866d8355f2bcdf754af010cab261a1629
Marta Połeć <polec.marta@gmail.com>
2025-02-04 10:50:13 +0100

    Improve logic for creating fixed streaming measurements

--- app/repositories/fixed_sessions_repository.rb
+++ app/repositories/fixed_sessions_repository.rb
@@ -1,7 +1,18 @@
 class FixedSessionsRepository
+  def find_by(user_id:, uuid:)
+    FixedSession.find_by(user_id: user_id, uuid: uuid)
+  end
+
   def active_with_streams
     FixedSession
       .includes(:streams, :user)
       .where('last_measurement_at > ?', Time.current - FixedSession::ACTIVE_FOR)
+  end
+
+  def update_end_timestamps!(session:, last_measurement:)
+    session.end_time_local = last_measurement.time
+    session.last_measurement_at = last_measurement.time_with_time_zone
+
+    session.save!
   end
 end
