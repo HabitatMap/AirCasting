d85ee98f87cd90c19e51919952d92650655ab999
Marta Połeć <polec.marta@gmail.com>
2025-02-20 12:26:20 +0100

    Drop flipper tables (#977)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_17_100537) do
+ActiveRecord::Schema.define(version: 2025_02_20_105049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,22 +51,6 @@ ActiveRecord::Schema.define(version: 2025_01_17_100537) do
     t.integer "user_id"
     t.index ["user_id"], name: "index_deleted_sessions_on_user_id"
     t.index ["uuid", "user_id"], name: "index_deleted_sessions_on_uuid_and_user_id"
-  end
-
-  create_table "flipper_features", force: :cascade do |t|
-    t.string "key", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["key"], name: "index_flipper_features_on_key", unique: true
-  end
-
-  create_table "flipper_gates", force: :cascade do |t|
-    t.string "feature_key", null: false
-    t.string "key", null: false
-    t.text "value"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "measurements", id: :serial, force: :cascade do |t|
