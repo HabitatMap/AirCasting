a5df50aa9a8b42d55f3703583f198da4d1d1a605
Marta Połeć <polec.marta@gmail.com>
2025-03-04 15:10:24 +0100

    Move GET api/mobile/sessions params validation to contract

--- app/controllers/api/mobile/sessions_controller.rb
+++ app/controllers/api/mobile/sessions_controller.rb
@@ -8,13 +8,8 @@ module Api
       q[:time_from] = Time.strptime(q[:time_from].to_s, '%s')
       q[:time_to] = Time.strptime(q[:time_to].to_s, '%s')
 
-      form =
-        Api::ParamsForm.new(
-          params: q,
-          schema: Api::MobileSessions::Schema,
-          struct: Api::MobileSessions::Struct
-        )
-      result = Api::ToMobileSessionsArray.new(form: form).call
+      contract = Api::MobileSessionsContract.new.call(q)
+      result = Api::ToMobileSessionsArray.new(contract: contract).call
 
       if result.success?
         render json: result.value, status: :ok
@@ -48,18 +43,25 @@ module Api
     private
 
     def stream
-      @stream ||= Stream
-        .includes(:threshold_set)
-        .joins(:session)
-        .find_by!(sensor_name: show_form.to_h.sensor_name, sessions: { id: show_form.to_h.id })
+      @stream ||=
+        Stream
+          .includes(:threshold_set)
+          .joins(:session)
+          .find_by!(
+            sensor_name: show_form.to_h.sensor_name,
+            sessions: {
+              id: show_form.to_h.id,
+            },
+          )
     end
 
     def show_form
-      @show_form ||= Api::ParamsForm.new(
-        params: params.to_unsafe_hash,
-        schema: Api::Session::Schema,
-        struct: Api::Session::Struct
-      )
+      @show_form ||=
+        Api::ParamsForm.new(
+          params: params.to_unsafe_hash,
+          schema: Api::Session::Schema,
+          struct: Api::Session::Struct,
+        )
     end
   end
 end
