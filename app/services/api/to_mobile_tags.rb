527eb8e8b3d1c6f549decf9da1e5791139f96636
Marta Połeć <polec.marta@gmail.com>
2025-03-04 13:44:53 +0100

    Move GET api/mobile/autocomplete/tags params validation to contract

--- app/services/api/to_mobile_tags.rb
+++ app/services/api/to_mobile_tags.rb
@@ -1,20 +1,20 @@
 class Api::ToMobileTags
-  def initialize(form:)
-    @form = form
+  def initialize(contract:)
+    @contract = contract
   end
 
   def call
-    return Failure.new(form.errors) if form.invalid?
+    return Failure.new(contract.errors.to_h) if contract.failure?
 
     sessions = MobileSession.filter_(data)
 
     session_ids = sessions.pluck(:id)
     return Success.new([]) if session_ids.empty?
 
-    tags = TagsRepository.new
-      .sessions_tags(
+    tags =
+      TagsRepository.new.sessions_tags(
         session_ids: session_ids,
-        input: data[:input]
+        input: data[:input],
       )
 
     Success.new(tags.rows.map { |row| row[0] })
@@ -22,9 +22,9 @@ class Api::ToMobileTags
 
   private
 
-  attr_reader :form
+  attr_reader :contract
 
   def data
-    form.to_h.to_h
+    contract.to_h
   end
 end
