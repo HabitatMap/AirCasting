106f2c363c3f77536c15a27a04a4c3c7ee9ce4b0
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2025-03-25 10:55:47 +0100

    Revert changes in apiClient

--- app/javascript/react/api/apiClient.ts
+++ app/javascript/react/api/apiClient.ts
@@ -1,53 +1,24 @@
 import axios, { AxiosResponse } from "axios";
 import { camelizeKeys } from "humps";
 
-const API_BASE_URL = process.env.API_BASE_URL;
-const API_STREAM_URL = process.env.API_STREAM_URL;
-
 const apiClient = axios.create({
-  baseURL: API_STREAM_URL,
+  baseURL: process.env.API_STREAM_URL,
 });
 
 const oldApiClient = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: process.env.API_BASE_URL,
 });
 
-apiClient.interceptors.request.use((config) => {
-  if (config.url && config.url.startsWith("http:")) {
-    config.url = config.url.replace("http:", "https:");
-  }
+apiClient.interceptors.response.use((response: AxiosResponse) => {
+  response.data = camelizeKeys(response.data);
 
-  return config;
+  return response;
 });
 
-oldApiClient.interceptors.request.use((config) => {
-  if (config.url && config.url.startsWith("http:")) {
-    config.url = config.url.replace("http:", "https:");
-  }
+oldApiClient.interceptors.response.use((response: AxiosResponse) => {
+  response.data = camelizeKeys(response.data);
 
-  return config;
+  return response;
 });
-
-apiClient.interceptors.response.use(
-  (response: AxiosResponse) => {
-    response.data = camelizeKeys(response.data);
-    return response;
-  },
-  (error) => {
-    console.error(`API Error (apiClient): ${error.message}`, error);
-    return Promise.reject(error);
-  }
-);
-
-oldApiClient.interceptors.response.use(
-  (response: AxiosResponse) => {
-    response.data = camelizeKeys(response.data);
-    return response;
-  },
-  (error) => {
-    console.error(`API Error (oldApiClient): ${error.message}`, error);
-    return Promise.reject(error);
-  }
-);
 
 export { apiClient, oldApiClient };
