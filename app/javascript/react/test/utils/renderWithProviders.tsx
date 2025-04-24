dedd710ca79aafde8bb5c02e763a95baaae6fabd
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2025-04-24 14:15:44 +0200

    Add first test

--- app/javascript/react/test/utils/renderWithProviders.tsx
+++ app/javascript/react/test/utils/renderWithProviders.tsx
@@ -2,10 +2,14 @@ import { render } from "@testing-library/react";
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { RootState } from "../../store";
 import { createMockStore } from "./mockStore";
 
-export const renderWithProviders = (ui: React.ReactElement) => {
-  const store = createMockStore();
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  options?: { initialState?: Partial<RootState> }
+) => {
+  const store = createMockStore(options?.initialState);
   return render(
     <Provider store={store}>
       <BrowserRouter>{ui}</BrowserRouter>
