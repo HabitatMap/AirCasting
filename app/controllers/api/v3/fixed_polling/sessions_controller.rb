5cc0074280e987bf954688eceab8ac56ab717ef3
Marta Połeć <polec.marta@gmail.com>
2025-08-05 15:10:16 +0200

    Add logging

--- app/controllers/api/v3/fixed_polling/sessions_controller.rb
+++ app/controllers/api/v3/fixed_polling/sessions_controller.rb
@@ -10,6 +10,9 @@ module Api
             ::FixedPolling::Interactor.new.call(params: params.to_unsafe_hash)
 
           if result.success?
+            Rails.logger.info(
+              "FixedPolling::Interactor response: #{result.value}",
+            )
             render json: result.value, status: :ok
           else
             render json: result.errors, status: :bad_request
