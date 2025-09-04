d703033417d81836cf531fe82c6608fc52a4d901
Marta Połeć <polec.marta@gmail.com>
2025-09-04 13:34:22 +0200

    Remove uniq validation for time and stream_id from FixedMeasurement

--- app/models/fixed_measurement.rb
+++ app/models/fixed_measurement.rb
@@ -2,6 +2,5 @@ class FixedMeasurement < ApplicationRecord
   belongs_to :stream
 
   validates :value, :time, :time_with_time_zone, presence: true
-  validates :time, uniqueness: { scope: :stream_id }
   validates :time_with_time_zone, uniqueness: { scope: :stream_id }
 end
