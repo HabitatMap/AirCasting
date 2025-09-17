cefa860447c2d7ab778f284cfe84e27941205635
Marta Połeć <polec.marta@gmail.com>
2025-09-17 13:23:04 +0200

    Use fixed_measurements in daily averages calculation script

--- app/services/stream_daily_averages/calculator.rb
+++ app/services/stream_daily_averages/calculator.rb
@@ -1,27 +1,18 @@
 module StreamDailyAverages
   class Calculator
-    def call(start_date:, end_date:)
-      start_date_time = Time.parse(start_date)
-      end_date_time = Time.parse(end_date) + 1.day
-
-      Rails.logger.info(
-        "STARTING: Recalculating averages from #{start_date_time} to #{end_date_time}",
-      )
+    def call
+      Rails.logger.info('STARTING: Recalculating stream daily averages')
 
       ActiveRecord::Base.transaction do
-        ActiveRecord::Base.connection.execute(
-          query(start_date_time, end_date_time),
-        )
+        ActiveRecord::Base.connection.execute(query)
       end
 
-      Rails.logger.info(
-        "FINISHED: Averages recalculated from #{start_date_time} to #{end_date_time}",
-      )
+      Rails.logger.info('FINISHED: Averages recalculated')
     end
 
     private
 
-    def query(start_date_time, end_date_time)
+    def query
       <<-SQL
       CREATE TEMP TABLE temp_daily_averages AS
       SELECT
@@ -31,13 +22,7 @@ module StreamDailyAverages
           WHEN time::time >= '00:00:01' THEN DATE_TRUNC('day', time)
           ELSE DATE_TRUNC('day', time) - INTERVAL '1 day'
         END AS date
-      FROM measurements
-      JOIN streams ON measurements.stream_id = streams.id
-      JOIN sessions ON streams.session_id = sessions.id
-      WHERE time > '#{start_date_time}'
-      AND time <= '#{end_date_time}'
-      AND sessions.last_measurement_at >= '#{start_date_time - 2.days}'
-      AND sessions.type = 'FixedSession'
+      FROM fixed_measurements
       GROUP BY stream_id, date;
 
       INSERT INTO stream_daily_averages (stream_id, value, date, created_at, updated_at)
--- spec/services/stream_daily_averages/calculator_spec.rb
+++ spec/services/stream_daily_averages/calculator_spec.rb
@@ -5,28 +5,26 @@ describe StreamDailyAverages::Calculator do
 
   describe '#call' do
     it 'calculates the daily averages' do
-      session_1 =
-        create(:fixed_session, last_measurement_at: '2025-01-18 00:00:00')
-      session_2 =
-        create(:fixed_session, last_measurement_at: '2025-01-18 00:00:00')
+      session_1 = create(:fixed_session)
+      session_2 = create(:fixed_session)
       stream_1 = create(:stream, session: session_1)
       stream_2 = create(:stream, session: session_2)
 
       # stream_1, for: 2025-01-16, average: 2
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_1,
         value: 1,
         time: '2025-01-16 00:00:01',
       )
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_1,
         value: 2,
         time: '2025-01-16 00:10:00',
       )
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_1,
         value: 4,
         time: '2025-01-17 00:00:00',
@@ -34,19 +32,19 @@ describe StreamDailyAverages::Calculator do
 
       # stream_1, for: 2025-01-17, average: 7
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_1,
         value: 8,
         time: '2025-01-17 00:00:01',
       )
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_1,
         value: 9,
         time: '2025-01-17 10:00:00',
       )
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_1,
         value: 3,
         time: '2025-01-18 00:00:00',
@@ -54,27 +52,25 @@ describe StreamDailyAverages::Calculator do
 
       # stream_2, for: 2025-01-17, average: 5
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_2,
         value: 4,
         time: '2025-01-17 00:30:01',
       )
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_2,
         value: 11,
         time: '2025-01-17 10:00:01',
       )
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream_2,
         value: 1,
         time: '2025-01-18 00:00:00',
       )
 
-      expect {
-        subject.call(start_date: '2025-01-16', end_date: '2025-01-17')
-      }.to change { StreamDailyAverage.count }.by(3)
+      expect { subject.call }.to change { StreamDailyAverage.count }.by(3)
 
       stream_daily_average_1_1 =
         StreamDailyAverage.find_by(
@@ -97,9 +93,8 @@ describe StreamDailyAverages::Calculator do
       expect(stream_daily_average_2_1.value).to eq(5)
     end
 
-    it 'does not update daily averages outside of the date range' do
-      session =
-        create(:fixed_session, last_measurement_at: '2025-01-18 00:09:00')
+    it 'updates values for existing records' do
+      session = create(:fixed_session)
       stream = create(:stream, session: session)
       stream_daily_average =
         create(
@@ -109,23 +104,15 @@ describe StreamDailyAverages::Calculator do
           value: 2,
         )
       create(
-        :measurement,
+        :fixed_measurement,
         stream: stream,
         value: 1,
-        time: '2025-01-15 00:09:00',
-      )
-      create(
-        :measurement,
-        stream: stream,
-        value: 1,
-        time: '2025-01-18 00:09:00',
+        time: '2025-01-16 00:09:00',
       )
 
-      expect {
-        subject.call(start_date: '2025-01-16', end_date: '2025-01-17')
-      }.to_not change { stream_daily_average.reload.updated_at }
+      expect { subject.call }.to_not change { StreamDailyAverage.count }
 
-      expect(StreamDailyAverage.count).to eq(1)
+      expect(stream_daily_average.reload.value).to eq(1)
     end
   end
 end
