d21d23732e4263deedd1cd67ee478a1bc369c085
Marta Połeć <polec.marta@gmail.com>
2025-09-18 13:16:35 +0200

    Remove OpenAq module

--- app/services/open_aq/filter_measurements.rb
+++
@@ -1,9 +0,0 @@
-class OpenAq::FilterMeasurements
-  SENSOR_NAMES = %w[pm25 o3]
-
-  def call(measurements:)
-    measurements.filter do |measurement|
-      SENSOR_NAMES.include?(measurement.sensor_name)
-    end
-  end
-end
