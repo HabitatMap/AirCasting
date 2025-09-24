3d78f46f7e61c034d0ee4a675a97f1b764bfe51a
Marta Połeć <polec.marta@gmail.com>
2025-09-24 17:29:06 +0200

    Extend sessions filtering endpoint

---
+++ app/services/sessions/contract.rb
@@ -0,0 +1,18 @@
+module Sessions
+  class Contract < Dry::Validation::Contract
+    params do
+      optional(:sensor_package_name).filled(:string)
+      optional(:start_datetime).filled(:date_time)
+      optional(:end_datetime).filled(:date_time)
+      optional(:tags).filled(:string)
+    end
+
+    rule(:end_datetime, :start_datetime) do
+      if values[:start_datetime] && values[:end_datetime]
+        if values[:end_datetime] <= values[:start_datetime]
+          key(:end_datetime).failure('must be greater than start_datetime')
+        end
+      end
+    end
+  end
+end
