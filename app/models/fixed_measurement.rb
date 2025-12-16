54aba2c15920f11b09c34b7eb6e1a82ad6d61dbe
Marta Połeć <polec.marta@gmail.com>
2025-12-16 17:43:16 +0100

    Revert "Adjust FixedMeasurement and FixedStream (#1091)"
    This reverts commit 1300aa3cea215fded2921b718a6f867c0f1c8649.

--- app/models/fixed_measurement.rb
+++ app/models/fixed_measurement.rb
@@ -1,6 +1,5 @@
 class FixedMeasurement < ApplicationRecord
   belongs_to :stream
-  belongs_to :fixed_stream, optional: true
 
   validates :value, :time, :time_with_time_zone, presence: true
   validates :time_with_time_zone, uniqueness: { scope: :stream_id }
