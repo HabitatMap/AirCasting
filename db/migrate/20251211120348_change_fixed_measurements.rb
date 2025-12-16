06845f811a9c2fe5d99b3159ad9c9b0a1fcbdf7f
Marta Połeć <polec.marta@gmail.com>
2025-12-16 17:43:59 +0100

    Revert "Adjust FixedMeasurement and FixedStream (#1091)" (#1092)
    This reverts commit 1300aa3cea215fded2921b718a6f867c0f1c8649.

--- db/migrate/20251211120348_change_fixed_measurements.rb
+++
@@ -1,8 +0,0 @@
-class ChangeFixedMeasurements < ActiveRecord::Migration[7.0]
-  def change
-    add_column :fixed_measurements, :fixed_stream_id, :bigint, foreign_key: true
-    add_column :fixed_measurements, :measured_at, :timestamptz, precision: 6
-
-    add_index :fixed_measurements, %i[fixed_stream_id measured_at], unique: true
-  end
-end
--- db/migrate/20251216114551_change_fixed_streams.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFixedStreams < ActiveRecord::Migration[7.0]
-  def change
-    add_column :fixed_streams, :title, :string, null: false
-    add_column :fixed_streams, :url_token, :string, null: false
-  end
-end
