3f9d47574e1bac438cd560bfe475e7b079eb6719
Marta Połeć <polec.marta@gmail.com>
2026-02-04 15:07:13 +0100

    Import data from all EEA countries

--- app/services/eea/ingest_orchestrator.rb
+++ app/services/eea/ingest_orchestrator.rb
@@ -1,7 +1,10 @@
 module Eea
   class IngestOrchestrator
-    COUNTRIES = %w[FR GB ES]
-    POLLUTANTS = %w[PM2.5 NO2 O3]
+    COUNTRIES = %w[
+      AD AL AT BA BE BG CH CY CZ DE DK EE ES FI FR GB GE GR HR HU
+      IE IS IT LT LU LV ME MK MT NL NO PL PT RO RS SE SI SK TR UA XK
+    ].freeze
+    POLLUTANTS = %w[PM2.5 NO2 O3].freeze
 
     def initialize(ingest_batch_enqueuer: IngestBatchEnqueuer.new)
       @ingest_batch_enqueuer = ingest_batch_enqueuer
