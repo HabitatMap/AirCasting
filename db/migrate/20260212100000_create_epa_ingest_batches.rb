61b2fafead079c18359dde8759572b27f61560eb
Marta Połeć <polec.marta@gmail.com>
2026-02-16 13:25:23 +0100

    Add tables for EPA ETL flow

---
+++ db/migrate/20260212100000_create_epa_ingest_batches.rb
@@ -0,0 +1,17 @@
+class CreateEpaIngestBatches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :epa_ingest_batches do |t|
+      t.timestamptz :window_starts_at, null: false
+      t.timestamptz :window_ends_at, null: false
+      t.string :status, null: false, default: 'queued'
+
+      t.timestamps
+    end
+
+    add_index :epa_ingest_batches, :status
+
+    add_check_constraint :epa_ingest_batches,
+                         'window_starts_at < window_ends_at',
+                         name: 'chk_epa_ingest_batches_window_bounds'
+  end
+end
