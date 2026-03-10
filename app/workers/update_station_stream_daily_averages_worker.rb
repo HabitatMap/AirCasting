1881bd835702ee8d6de1304aa2c1447ce36fea2b
Hanna Seweryn <hanka@lunarlogic.io>
2026-03-10 13:53:30 +0100

    fix: put UpdateStationStreamDailyAveragesWorker under feture flag

--- app/workers/update_station_stream_daily_averages_worker.rb
+++ app/workers/update_station_stream_daily_averages_worker.rb
@@ -4,6 +4,8 @@ class UpdateStationStreamDailyAveragesWorker
   include Sidekiq::Worker
 
   def perform
+    return unless A9n.sidekiq_averages_calculation_enabled
+
     GovernmentSources::StationStreamDailyAveragesCalculator.new.call
   end
 end
