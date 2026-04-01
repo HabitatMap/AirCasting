c549de4fc0763eb9742cbe140d5dedb657dc1ee7
Hanna Seweryn <hanka@lunarlogic.io>
2026-04-01 10:49:18 +0200

    refactor: consolidate db migrations

--- db/migrate/20260319115907_add_measurement_type_id_to_streams.rb
+++
@@ -1,9 +0,0 @@
-class AddMeasurementTypeIdToStreams < ActiveRecord::Migration[7.0]
-  def change
-    add_column :streams, :measurement_type_id, :integer, null: true
-    add_index :streams, %i[session_id measurement_type_id],
-              unique: true,
-              name: 'idx_streams_session_measurement_type_id',
-              where: 'measurement_type_id IS NOT NULL'
-  end
-end
